a469310e7ebaf9a23fbb77633a906cacb061eebc
Irene Volodko <ice7aye@gmail.com>
2024-07-03 17:37:52 +0300

    2974 Minimum number game

--- 2974-minimum-number-game.js
+++ 2974-minimum-number-game.js
@@ -12,14 +12,14 @@
 // Input = [2,5] Output = [5,2]
 //
 // ✅ SOLUTION:
-let numberGame = function(nums) {
-    nums.sort((a, b) => a - b)
+let numberGame = function ( nums ) {
+    nums.sort(( a , b ) => a - b)
     let arr = []
     for (let i = 0; i < nums.length; i += 2) {
-        arr.push(nums[i+1], nums[i])
+        arr.push(nums[i + 1] , nums[i])
     }
     return arr
 }
 
-console.log('Output: ' , numberGame([5,4,2,3]))
-console.log('Output: ' , numberGame([2,5]))
+console.log('Output: ' , numberGame([5 , 4 , 2 , 3]))
+console.log('Output: ' , numberGame([2 , 5]))
