babed5ca85a4242fd998a8f6673bcdf2ca6cf648
Irene Volodko <ice7aye@gmail.com>
2024-07-04 21:22:17 +0300

    1816 truncate sentence - tests

--- 1816-truncate-sentence.js
+++ 1816-truncate-sentence.js
@@ -1,3 +1,7 @@
 let truncateSentence = function ( s , k ) {
     return s.split(' ').slice(0 , k).join(' ')
 }
+
+console.log('Output: ' ,truncateSentence("Hello how are you Contestant", 4))
+console.log('Output: ' ,truncateSentence("What is the solution to this problem", 4))
+console.log('Output: ' ,truncateSentence("chopper is not a tanuki", 5))
