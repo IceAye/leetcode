f2262d0b02c186fe2bd57069aa316eb190167e03
Irene Volodko <ice7aye@gmail.com>
2024-07-04 21:56:36 +0300

    1816 truncate sentence

--- 1816-truncate-sentence.js
+++ 1816-truncate-sentence.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+// You want to truncate sentences such that it contains only the first k words.
+//
+// 📌 EXAMPLES:
+// Input: s = "Hello how are you Contestant", k = 4 --> Output: "Hello how are you"
+// Input: s = "What is the solution to this problem", k = 4 --> Output: "What is the solution"
+// Input: s = "chopper is not a tanuki", k = 5 --> Output: "chopper is not a tanuki"
+//
+// ✅ SOLUTION:
 let truncateSentence = function ( s , k ) {
     return s.split(' ').slice(0 , k).join(' ')
 }
