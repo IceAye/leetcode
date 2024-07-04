48ff99969eeb94eab516b6714bc2d828f3bf92cc
Irene Volodko <ice7aye@gmail.com>
2024-07-04 21:02:27 +0300

    1662 Check if two string arrays are equivalent - tests

--- 1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent.js
@@ -1,3 +1,6 @@
 let arrayStringsAreEqual = function (word1, word2) {
     return word1.join('') === word2.join('')
 }
+console.log('Output: ' ,arrayStringsAreEqual(["ab", "c"], ["a", "bc"]))
+console.log('Output: ' ,arrayStringsAreEqual(["a", "cb"], ["ab", "c"]))
+console.log('Output: ' ,arrayStringsAreEqual(["abc", "d", "defg"],["abcddefg"]))
