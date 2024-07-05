b82ee9c23a97d60b112149523603ef6c906c474c
Irene Volodko <ice7aye@gmail.com>
2024-07-05 20:12:20 +0300

    add 1528 shuffle string

---
+++ 1528-shuffle-string.js
@@ -0,0 +1,7 @@
+let restoreString = function (s, indices) {
+    let result = []
+    for (let i = 0; i < indices.length; i++) {
+        result[indices[i]] = s[i]
+    }
+    return result.join('')
+}
