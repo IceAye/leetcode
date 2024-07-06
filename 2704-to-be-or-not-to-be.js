20546d83082eb9b18811b345299093e788181582
Irene Volodko <ice7aye@gmail.com>
2024-07-06 21:48:05 +0300

    2704 to be or not to be

--- 2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Write a function expect that helps developers test their code.
+// It should take in any value val and return an object with the following two functions.
+//
+// toBe(val) accepts another value and returns true if the two values === each other.
+// If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other.
+// If they are equal, it should throw an error "Equal".
+//
+// ✅ SOLUTION:
 let expect = function ( val ) {
     function toBe(value) {
         if (val === value) {
@@ -6,7 +16,6 @@ let expect = function ( val ) {
             throw new Error('Not Equal')
         }
     }
-
     function notToBe(value) {
         if (val !== value) {
             return true
