30bd7b76af5be83320cb0fe51eea0d1717292bd6
Irene Volodko <ice7aye@gmail.com>
2024-07-07 21:53:12 +0300

    add 2236 root equals sum of children

---
+++ 2326-root-equals-sum-of-children.js
@@ -0,0 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+let checkTree = function(root) {
+    return root.val === root.left.val + root.right.val
+}
