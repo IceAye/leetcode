65337f0a139af6cf95dff1dfeeac032ab2400e58
Irene Volodko <ice7aye@gmail.com>
2024-07-16 23:47:45 +0300

    description check if the sentence is pangram

--- 1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given a string sentence containing only lowercase English letters,
+// return true if sentence is a pangram, or false otherwise.
+// sentence consists of lowercase English letters.
+//
+// ✅ SOLUTION:
 let checkIfPangram = function ( sentence ) {
     return new Set(sentence).size === 26
 }
