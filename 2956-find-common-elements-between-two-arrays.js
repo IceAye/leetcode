29ab0c854f475704939aacbfdf1ff9ccf535f3b3
Irene Volodko <ice7aye@gmail.com>
2024-07-16 23:04:14 +0300

    description find common elements between two arrays

--- 2956-find-common-elements-between-two-arrays.js
+++ 2956-find-common-elements-between-two-arrays.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given two integer arrays nums1 and nums2 of sizes n and m, respectively. Calculate the following values:
+// answer1 : the number of indices i such that nums1[i] exists in nums2.
+// answer2 : the number of indices i such that nums2[i] exists in nums1.
+// Return [answer1,answer2].
+//
+// ✅ SOLUTION:
+
 let findIntersectionValues = function(nums1, nums2) {
     let answer1 = nums1.filter(item => nums2.includes(item)).length
     let answer2 = nums2.filter(item => nums1.includes(item)).length
