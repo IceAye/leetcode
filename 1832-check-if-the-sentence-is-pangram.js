d81a2b22c920f935d4d2c624a6e6ee2481ee9b59
Irene Volodko <ice7aye@gmail.com>
2024-07-16 23:46:20 +0300

    test check if the sentence is pangram

--- 1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram.js
@@ -2,3 +2,6 @@ let checkIfPangram = function ( sentence ) {
     let result = new Set(sentence)
     return result.size === 26
 }
+
+console.log(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))
+console.log(checkIfPangram("leetcode"))
