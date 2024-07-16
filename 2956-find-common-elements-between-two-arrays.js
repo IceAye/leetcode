d3bb68dcc846c46faeaedef5e117e5cf0d095e7c
Irene Volodko <ice7aye@gmail.com>
2024-07-16 23:03:22 +0300

    test find common elements between two arrays

--- 2956-find-common-elements-between-two-arrays.js
+++ 2956-find-common-elements-between-two-arrays.js
@@ -3,3 +3,6 @@ let findIntersectionValues = function(nums1, nums2) {
     let answer2 = nums2.filter(item => nums1.includes(item)).length
     return [answer1, answer2]
 }
+
+console.log(findIntersectionValues([2,3,2], [1,2]))
+console.log(findIntersectionValues([4,3,2,3,1], [2,2,5,2,3,6]))
