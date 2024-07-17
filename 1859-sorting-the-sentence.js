1f351f89ddd8c1d29bf1669b7bbd15ef319b6382
Irene Volodko <ice7aye@gmail.com>
2024-07-17 17:38:55 +0300

    refactor sorting the sentence

--- 1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence.js
@@ -1,10 +1,5 @@
 let sortSentence = function (s) {
-    let array = s.split(' ')
-    let result = []
-    for (let i = 0; i < array.length; i++) {
-        result[array[i].at(-1) - 1] = array[i].slice(0, -1)
-    }
-    return result.join(' ')
+    return s.split(' ').sort((a, b) => a.at(-1) - b.at(-1)).map(item => item.slice(0, -1)).join(' ')
 }
 
 console.log(sortSentence("is2 sentence4 This1 a3"))
