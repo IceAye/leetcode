30c2ad81ec375d6c349d06b7919ab3b4b29acb0e
Irene Volodko <ice7aye@gmail.com>
2024-07-17 18:01:57 +0300

    description reverse words in a string

--- reverse-words-in-a-string.js
+++ reverse-words-in-a-string.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given a string s, reverse the order of characters in each word within a sentence
+// while still preserving whitespace and initial word order.
+//
+// ✅ SOLUTION:
 let reverseWords = function (s) {
     return s.split(' ').map(item => item.split('').reverse().join('')).join(' ')
 }
