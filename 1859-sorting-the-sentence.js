7265278d5df51a73c28e1278bc39cbb195c8361e
Irene Volodko <ice7aye@gmail.com>
2024-07-17 17:40:13 +0300

    description sorting the sentence

--- 1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// Each word consists of lowercase and uppercase English letters.
+// A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+// Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+//
+// ✅ SOLUTION:
 let sortSentence = function (s) {
     return s.split(' ').sort((a, b) => a.at(-1) - b.at(-1)).map(item => item.slice(0, -1)).join(' ')
 }
