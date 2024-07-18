870e866ec0a5d9c2a741de88c446562effc3e9fa
Irene Volodko <ice7aye@gmail.com>
2024-07-18 20:45:58 +0300

    description harshad number

--- 3099-harshad-number.js
+++ 3099-harshad-number.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// An integer divisible by the sum of its digits is said to be a Harshad number.
+// You are given an integer x. Return the sum of the digits of x if x is a Harshad number, otherwise, return -1.
+//
+// ✅ SOLUTION:
 let sumOfTheDigitsOfHarshadNumber = function(x) {
     let sumOfDigits = String(x).split('').map(item => Number(item)).reduce((sum, current) => sum + current)
     return x % sumOfDigits === 0 ? sumOfDigits : -1
