f26dd1b91893f4ed2db069e6461d72e485378acb
Irene Volodko <ice7aye@gmail.com>
2024-07-18 22:08:56 +0300

    description unique morse code words

--- 804-unique-morse-code-words.js
+++ 804-unique-morse-code-words.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+//
+// For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...".
+// We will call such a concatenation the transformation of a word.
+// Return the number of different transformations among all words we have.
+//
+// ✅ SOLUTION:
+
 const morseCode = [".-" , "-..." , "-.-." , "-.." , "." , "..-." , "--." , "...." , ".." , ".---" , "-.-" , ".-.." , "--" , "-." , "---" , ".--." , "--.-" , ".-." , "..." , "-" , "..-" , "...-" , ".--" , "-..-" , "-.--" , "--.."]
 const abc = "abcdefghijklmnopqrstuvwxyz"
 let uniqueMorseRepresentations = function ( words ) {
