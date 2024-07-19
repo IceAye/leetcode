1774dbc5249f8dbde360a7b29f3aa89186dd2010
Irene Volodko <ice7aye@gmail.com>
2024-07-19 12:38:49 +0300

    description maximum 69 number

--- 1323-maximum-69-number.js
+++ 1323-maximum-69-number.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// You are given a positive integer num consisting only of digits 6 and 9.
+// Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+//
+// ✅ SOLUTION:
 let maximum69Number  = function(num) {
     return +String(num).replace(/6/, "9")
 }
