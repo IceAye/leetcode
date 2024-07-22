63c11b9792f2e26f6102989235294bb178acfa50
Irene Volodko <ice7aye@gmail.com>
2024-07-22 10:20:29 +0300

    description sort the people (runtime 82.81%)

--- 2418-sort-the-people.js
+++ 2418-sort-the-people.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// You are given an array of strings names, and an array heights that consists of distinct positive integers.
+// Both arrays are of length n.
+//
+// For each index i, names[i] and heights[i] denote the name and height of the ith person.
+//
+// Return names sorted in descending order by the people's heights.
+//
+// ✅ SOLUTION:
+
 let sortPeople = function ( names , heights ) {
     let result = []
     let people = new Map()
