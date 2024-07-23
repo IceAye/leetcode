f6b2a41ff4d91adae3a7109ae99049926736191a
Irene Volodko <ice7aye@gmail.com>
2024-07-23 15:35:31 +0300

    description sum of squares of special elements

--- 2778-sum-of-squares-of-special-elements.js
+++ 2778-sum-of-squares-of-special-elements.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given a 1-indexed integer array nums of length n.
+//
+// An element nums[i] of nums is called special if i divides n, i.e. n % i == 0.
+//
+// Return the sum of the squares of all special elements of nums.
+//
+// ✅ SOLUTION:
 let sumOfSquares = function (nums) {
     let result = 0;
     for (let i = 1; i <= nums.length; i++) {
