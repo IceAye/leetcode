7678c4aa32a66de84fe051b6fcffe99fb3f0a625
Irene Volodko <ice7aye@gmail.com>
2024-07-24 15:26:20 +0300

    refactor number of strings that appear as substring in word

--- 1967-number-of-strings-that-appear-as-substrings-in-word.js
+++ 1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -1,15 +1,11 @@
-let numOfStrings = function(patterns, word) {
+let numOfStrings = function ( patterns , word ) {
     let sum = 0;
     for (const pattern of patterns) {
-        if (word.includes(pattern)) {
-            sum++
-        } else {
-            sum;
-        }
+        word.includes(pattern) ? sum++ : sum;
     }
     return sum;
 };
 
-console.log(numOfStrings(["a","abc","bc","d"], "abc"));
-console.log(numOfStrings(["a","b","c"], "aaaaabbbbb"));
-console.log(numOfStrings(["a","a","a"], "ab"));
+console.log(numOfStrings(["a" , "abc" , "bc" , "d"] , "abc"));
+console.log(numOfStrings(["a" , "b" , "c"] , "aaaaabbbbb"));
+console.log(numOfStrings(["a" , "a" , "a"] , "ab"));
