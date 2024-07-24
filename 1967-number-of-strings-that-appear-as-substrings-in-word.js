da7131df550c44634fecfd8c62891df936b75b99
Irene Volodko <ice7aye@gmail.com>
2024-07-24 15:27:04 +0300

    description number of strings that appear as substring in word

--- 1967-number-of-strings-that-appear-as-substrings-in-word.js
+++ 1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+//
+// A substring is a contiguous sequence of characters within a string.
+//
+// ✅ SOLUTION:
 let numOfStrings = function ( patterns , word ) {
     let sum = 0;
     for (const pattern of patterns) {
