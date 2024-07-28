89ee0a4ab9f6da0aff9d5a22d7ec3eac2163a706
Irene Volodko <ice7aye@gmail.com>
2024-07-28 10:56:25 +0300

    description count tested devices after test operations

--- 2960-count-tested-devices-after-test-operations.js
+++ 2960-count-tested-devices-after-test-operations.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array batteryPercentages having length n, denoting the battery percentages of n 0-indexed devices.
+// Your task is to test each device i in order from 0 to n - 1, by performing the following test operations:
+//
+// If batteryPercentages[i] is greater than 0:
+// Increment the count of tested devices.
+// Decrease the battery percentage of all devices with indices j in the range [i + 1, n - 1] by 1,
+// ensuring their battery percentage never goes below 0, i.e, batteryPercentages[j] = max(0, batteryPercentages[j] - 1).
+// Move to the next device.
+// Otherwise, move to the next device without performing any test.
+// Return an integer denoting the number of devices that will be tested after performing the test operations in order.
+//
+// ✅ SOLUTION:
 let countTestedDevices = function (batteryPercentages) {
     let result = 0;
     for (let i = 0; i < batteryPercentages.length; i++) {
