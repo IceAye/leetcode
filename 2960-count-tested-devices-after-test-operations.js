09204af85a86e43d1db0ae797a0530ecdcf182e1
Irene Volodko <ice7aye@gmail.com>
2024-07-28 10:46:24 +0300

    add count tested devices after test operations

---
+++ 2960-count-tested-devices-after-test-operations.js
@@ -0,0 +1,13 @@
+let countTestedDevices = function (batteryPercentages) {
+    let result = 0;
+    for (let i = 0; i < batteryPercentages.length; i++) {
+        if (batteryPercentages[i] > 0) {
+            result += 1;
+            batteryPercentages = batteryPercentages.map((item) => (item -= 1));
+        }
+    }
+    return result;
+};
+
+console.log(countTestedDevices([1, 1, 2, 1, 3]));
+console.log(countTestedDevices([0, 1, 2]));
