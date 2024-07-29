1e46ccae5f52c32749b518430fb436d9bacdc956
Irene Volodko <ice7aye@gmail.com>
2024-07-29 22:42:58 +0300

    description minimum number of chairs in a waiting room

--- 3168-minimum-number-of-chairs-in-a-waiting-room.js
+++ 3168-minimum-number-of-chairs-in-a-waiting-room.js
@@ -1,11 +1,21 @@
-let minimumChairs = function (s) {
+// ❓ DESCRIPTION:
+// You are given a string s. Simulate events at each second i:
+//
+// If s[i] == 'E', a person enters the waiting room and takes one of the chairs in it.
+// If s[i] == 'L', a person leaves the waiting room, freeing up a chair.
+// Return the minimum number of chairs needed so that a chair is available for every person who enters the waiting room
+// given that it is initially empty.
+//
+// ✅ SOLUTION:
+
+let minimumChairs = function ( s ) {
     let currentCount = 0;
     let result = 0;
     for (let i = 0; i < s.length; i++) {
         if (s[i] === "E") {
             currentCount++;
         } else currentCount--;
-        result = Math.max(currentCount, result);
+        result = Math.max(currentCount , result);
     }
     return result;
 };
