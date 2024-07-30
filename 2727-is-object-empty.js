6b76c6155c8bf7922a4cf1ce83a3ba0aca3f5efc
Irene Volodko <ice7aye@gmail.com>
2024-07-30 22:34:59 +0300

    refactor is object empty (runtime 89.04%)

--- 2727-is-object-empty.js
+++ 2727-is-object-empty.js
@@ -1,9 +1,12 @@
 let isEmpty = function (obj) {
-    return !Object.values(obj).length > 0;
+    for (const key in obj) {
+        return false;
+    }
+    return true;
 };
 
-console.log(isEmpty({ x: 5, y: 42 }));
+console.log(isEmpty({x: 5 , y: 42}));
 console.log(isEmpty({}));
-console.log(isEmpty([null, false, 0]));
+console.log(isEmpty([null , false , 0]));
 console.log(isEmpty([]));
 console.log(isEmpty({"length": 0}));
