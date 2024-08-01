58f061b2f1b08d6dd1b98a96d351fb9520a4c63e
Irene Volodko <ice7aye@gmail.com>
2024-08-01 13:16:25 +0300

    description number of senior citizens

--- 2678-number-of-senior-citizens.js
+++ 2678-number-of-senior-citizens.js
@@ -1,6 +1,14 @@
-let countSeniors = function(details) {
-    return details.filter(item => item.slice(-4, -2) > "60").length
+// ❓ DESCRIPTION:
+// You are given a 0-indexed array of strings details. Each element of details provides information about a given
+// passenger compressed into a string of length 15. The system is such that:  The first ten characters consist of the
+// phone number of passengers. The next character denotes the gender of the person. The following two characters are
+// used to indicate the age of the person. The last two characters determine the seat allotted to that person. Return
+// the number of passengers who are strictly more than 60 years old.
+//
+// ✅ SOLUTION:
+let countSeniors = function ( details ) {
+    return details.filter(item => item.slice(-4 , -2) > "60").length
 };
 
-console.log(countSeniors(["7868190130M7522","5303914400F9211","9273338290F4010"]))
-console.log(countSeniors(["1313579440F2036","2921522980M5644"]))
+console.log(countSeniors(["7868190130M7522" , "5303914400F9211" , "9273338290F4010"]))
+console.log(countSeniors(["1313579440F2036" , "2921522980M5644"]))
