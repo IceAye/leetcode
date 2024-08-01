2c701b3106756ae0709b2437e73b45b33671d282
Irene Volodko <ice7aye@gmail.com>
2024-08-01 13:44:45 +0300

    description count equal and divisible pairs in an array

--- 2776-count-equal-and-divisible-pairs-in-an-array.js
+++ 2776-count-equal-and-divisible-pairs-in-an-array.js
@@ -1,4 +1,9 @@
-let countPairs = function (nums, k) {
+// ❓ DESCRIPTION:
+// Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i <
+// j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.
+//
+// ✅ SOLUTION:
+let countPairs = function ( nums , k ) {
     let result = 0;
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
@@ -8,5 +13,5 @@ let countPairs = function (nums, k) {
     return result;
 };
 
-console.log(countPairs([3, 1, 2, 2, 2, 1, 3], 2));
-console.log(countPairs([1, 2, 3, 4], 1));
+console.log(countPairs([3 , 1 , 2 , 2 , 2 , 1 , 3] , 2));
+console.log(countPairs([1 , 2 , 3 , 4] , 1));
