b24e95595857eda55d49665cddafc58e480e5319
Irene Volodko <ice7aye@gmail.com>
2024-08-01 14:29:05 +0300

    refactor find if digits game can be won

--- 3232-find-if-digit-game-can-be-won.js
+++ 3232-find-if-digit-game-can-be-won.js
@@ -1,13 +1,12 @@
-let canAliceWin = function (nums) {
-    const alice = nums
-        .filter((item) => item < 10)
-        .reduce((sum, current) => sum + current);
-    const bob = nums
-        .filter((item) => item > 9)
-        .reduce((sum, current) => sum + current, 0);
-    return alice !== bob;
+let canAliceWin = function ( nums ) {
+    let single = 0;
+    let double = 0;
+    for (const num of nums) {
+        num < 10 ? single += num : double += num
+    }
+    return single !== double
 };
 
-console.log(canAliceWin([1, 2, 3, 4, 10]));
-console.log(canAliceWin([1, 2, 3, 4, 5, 14]));
-console.log(canAliceWin([5, 5, 5, 25]));
+console.log(canAliceWin([1 , 2 , 3 , 4 , 10]));
+console.log(canAliceWin([1 , 2 , 3 , 4 , 5 , 14]));
+console.log(canAliceWin([5 , 5 , 5 , 25]));
