87101aa606602c91efafe68f2d05a225abab9078
Irene Volodko <ice7aye@gmail.com>
2024-08-01 13:40:56 +0300

    refactor count equal and divisible pairs in an array

--- 2776-count-equal-and-divisible-pairs-in-an-array.js
+++ 2776-count-equal-and-divisible-pairs-in-an-array.js
@@ -2,7 +2,7 @@ let countPairs = function (nums, k) {
     let result = 0;
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j] && (i * j) % k === 0) result++;
+            nums[i] === nums[j] && (i * j) % k === 0 ? result++ : 0
         }
     }
     return result;
