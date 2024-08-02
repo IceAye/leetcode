6e989b17760d3332a12a18ad84444b2dd54a6502
Irene Volodko <ice7aye@gmail.com>
2024-08-02 23:50:45 +0300

    description return length of arguments passed

--- 2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a function argumentsLength that returns the count of arguments passed to it.
+//
+// ✅ SOLUTION:
 let argumentsLength = function(...args) {
     return [...args].length
 };
