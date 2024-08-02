27ebee629609606cf480bb4d0d883183f3e341f4
Irene Volodko <ice7aye@gmail.com>
2024-08-02 23:35:55 +0300

    description counter

--- 2620-counter.js
+++ 2620-counter.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more
+// than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+//
+// ✅ SOLUTION:
 let createCounter = function ( n ) {
     return () => n++
 };
