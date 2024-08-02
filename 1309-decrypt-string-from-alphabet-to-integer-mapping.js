61047aa5da704e89af6b00c75bc9a516f36f7fa1
Irene Volodko <ice7aye@gmail.com>
2024-08-02 22:18:42 +0300

    description decrypt string from alphabet to integer mapping

--- 1309-decrypt-string-from-alphabet-to-integer-mapping.js
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping.js
@@ -1,4 +1,12 @@
-let freqAlphabets = function(s) {
+// ❓ DESCRIPTION:
+// You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+//
+// Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+// Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+// Return the string formed after mapping.
+//
+// ✅ SOLUTION:
+let freqAlphabets = function ( s ) {
     let result = "";
     let i = 0;
     while (i < s.length) {
