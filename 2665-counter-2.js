c00f6107afd23cda457256c428637f9bfabe5183
Irene Volodko <ice7aye@gmail.com>
2024-08-02 23:53:30 +0300

    description counter 2

--- 2665-counter-2.js
+++ 2665-counter-2.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three
+// functions.  The three functions are:  increment() increases the current value by 1 and then returns it. decrement()
+// reduces the current value by 1 and then returns it. reset() sets the current value to init and then returns it.
+//
+// ✅ SOLUTION:
 let createCounter = function ( init ) {
     let count = init
 
