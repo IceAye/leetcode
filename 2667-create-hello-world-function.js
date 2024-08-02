4e3a566d856a11d82f9767e9ed43d91e69960f78
Irene Volodko <ice7aye@gmail.com>
2024-08-02 23:33:41 +0300

    description create hello world function

--- 2667-create-hello-world-function.js
+++ 2667-create-hello-world-function.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+//
+// ✅ SOLUTION:
 let createHelloWorld = function() {
     return function(...args) {
         return "Hello World"
