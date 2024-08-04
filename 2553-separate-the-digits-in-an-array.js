eff08ef0dc023be5f2c57c5ecb2819a74d1a4d7f
Irene Volodko <ice7aye@gmail.com>
2024-08-04 21:40:32 +0300

    description separate the digits in an array

--- 2553-separate-the-digits-in-an-array.js
+++ 2553-separate-the-digits-in-an-array.js
@@ -1,5 +1,14 @@
+// ❓ DESCRIPTION:
+// Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums
+// after separating them in the same order they appear in nums.
+// To separate the digits of an integer is to get all the digits it has in the same order.
+//
+// 📌 EXAMPLE:
+// 10921 --> [1,0,9,2,1]
+//
+// ✅ SOLUTION:
 let separateDigits = function ( nums ) {
-    return Array.from(nums.join(""), Number);
+    return Array.from(nums.join("") , Number);
 };
 
 console.log(separateDigits([13 , 25 , 83 , 77]));
