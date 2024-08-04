b28e6c875a4664eb939bf20728ecffac5832f832
Irene Volodko <ice7aye@gmail.com>
2024-08-04 23:42:38 +0300

    refactor find the difference of two arrays

--- 2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays.js
@@ -1,18 +1,15 @@
-let findDifference = function (nums1, nums2) {
-    let first = new Set();
-    let second = new Set();
-    for (const num of nums1) {
-        if (!nums2.includes(num)) first.add(num);
+let findDifference = function ( nums1 , nums2 ) {
+    let first = new Set(nums1);
+    let second = new Set(nums2);
+    for (const num of first) {
+        if (second.has(num)) {
+            first.delete(num);
+            second.delete(num);
+        }
     }
-    for (const num of nums2) {
-        if (!nums1.includes(num)) second.add(num);
-    }
-    let answer = [];
-    answer[0] = [...first];
-    answer[1] = [...second];
-    return answer;
+    return [Array.from(first) , Array.from(second)];
 };
 
-console.log(findDifference([1, 2, 3], [2, 4, 6]));
-console.log(findDifference([1, 2, 3, 3], [1, 1, 2, 2]));
-console.log(findDifference([-68, -80, -19, -94, 82, 21, -43], [-63]));
+console.log(findDifference([1 , 2 , 3] , [2 , 4 , 6]));
+console.log(findDifference([1 , 2 , 3 , 3] , [1 , 1 , 2 , 2]));
+console.log(findDifference([-68 , -80 , -19 , -94 , 82 , 21 , -43] , [-63]));
