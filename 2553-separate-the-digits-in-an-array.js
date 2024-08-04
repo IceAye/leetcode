cef2cb645858dce95cb7ab9a8af133d418049083
Irene Volodko <ice7aye@gmail.com>
2024-08-04 21:35:43 +0300

    add separate the digits in an array

---
+++ 2553-separate-the-digits-in-an-array.js
@@ -0,0 +1,9 @@
+let separateDigits = function (nums) {
+    return nums
+        .join("")
+        .split("")
+        .map((item) => +item);
+};
+
+console.log(separateDigits([13, 25, 83, 77]));
+console.log(separateDigits([7, 1, 3, 9]));
