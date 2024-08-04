8717968bf8a96480686482b03d66f5ebf4647c5a
Irene Volodko <ice7aye@gmail.com>
2024-08-04 23:05:04 +0300

    description destination city

--- 1436-destination-city.js
+++ 1436-destination-city.js
@@ -1,4 +1,11 @@
-let destCity = function (paths) {
+// ❓ DESCRIPTION:
+// You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from city Ai
+// to city Bi. Return the destination city, that is, the city without any path outgoing to another city.
+// It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination
+// city.
+//
+// ✅ SOLUTION:
+let destCity = function ( paths ) {
     let from = [];
     for (let i = 0; i < paths.length; i++) {
         from.push(paths[i][0]);
@@ -10,26 +17,26 @@ let destCity = function (paths) {
 
 console.log(
     destCity([
-        ["London", "New York"],
-        ["New York", "Lima"],
-        ["Lima", "Sao Paulo"],
-    ]),
+        ["London" , "New York"] ,
+        ["New York" , "Lima"] ,
+        ["Lima" , "Sao Paulo"] ,
+    ]) ,
 );
 
 console.log(
     destCity([
-        ["B", "C"],
-        ["D", "B"],
-        ["C", "A"],
-    ]),
+        ["B" , "C"] ,
+        ["D" , "B"] ,
+        ["C" , "A"] ,
+    ]) ,
 );
 
 console.log(
     destCity([
-        ["qMTSlfgZlC", "ePvzZaqLXj"],
-        ["xKhZXfuBeC", "TtnllZpKKg"],
-        ["ePvzZaqLXj", "sxrvXFcqgG"],
-        ["sxrvXFcqgG", "xKhZXfuBeC"],
-        ["TtnllZpKKg", "OAxMijOZgW"],
-    ]),
+        ["qMTSlfgZlC" , "ePvzZaqLXj"] ,
+        ["xKhZXfuBeC" , "TtnllZpKKg"] ,
+        ["ePvzZaqLXj" , "sxrvXFcqgG"] ,
+        ["sxrvXFcqgG" , "xKhZXfuBeC"] ,
+        ["TtnllZpKKg" , "OAxMijOZgW"] ,
+    ]) ,
 );
