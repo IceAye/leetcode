b9019184a801cd72ab05b161f89e4ed7e5ab778a
Irene Volodko <ice7aye@gmail.com>
2024-08-05 16:58:11 +0300

    description count pairs that form a complete day

--- 3184-count-pairs-that-form-a-complete-day-1.js
+++ 3184-count-pairs-that-form-a-complete-day-1.js
@@ -1,4 +1,10 @@
-let countCompleteDayPairs = function (hours) {
+// ❓ DESCRIPTION:
+// Given an integer array hours representing times in hours, return an integer denoting the number of pairs i, j where
+// i < j and hours[i] + hours[j] forms a complete day.  A complete day is defined as a time duration that is an exact
+// multiple of 24 hours.  For example, 1 day is 24 hours, 2 days is 48 hours, 3 days is 72 hours, and so on.
+//
+// ✅ SOLUTION:
+let countCompleteDayPairs = function ( hours ) {
     let result = 0;
     for (let i = 0; i < hours.length - 1; i++) {
         for (let j = 1; j < hours.length; j++) {
@@ -8,5 +14,5 @@ let countCompleteDayPairs = function (hours) {
     return result;
 };
 
-console.log(countCompleteDayPairs([12, 12, 30, 24, 24]));
-console.log(countCompleteDayPairs([72, 48, 24, 3]));
+console.log(countCompleteDayPairs([12 , 12 , 30 , 24 , 24]));
+console.log(countCompleteDayPairs([72 , 48 , 24 , 3]));
