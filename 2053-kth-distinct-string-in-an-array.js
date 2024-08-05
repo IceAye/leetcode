c83cf679c824e82c0c08ec075b3acc524df0c3e0
Irene Volodko <ice7aye@gmail.com>
2024-08-05 16:11:22 +0300

    description kth distinct string in an array

--- 2053-kth-distinct-string-in-an-array.js
+++ 2053-kth-distinct-string-in-an-array.js
@@ -1,4 +1,12 @@
-let kthDistinct = function (arr, k) {
+// ❓ DESCRIPTION:
+// A distinct string is a string that is present only once in an array.
+//
+// Given an array of strings arr, and an integer k, return the kth distinct string present in arr. If there are fewer
+// than k distinct strings, return an empty string "".  Note that the strings are considered in the order in which they
+// appear in the array.
+//
+// ✅ SOLUTION:
+let kthDistinct = function ( arr , k ) {
     let distinctArray = [];
     for (let item in arr) {
         if (arr.indexOf(arr[item]) === arr.lastIndexOf(arr[item]))
@@ -7,6 +15,6 @@ let kthDistinct = function (arr, k) {
     return distinctArray[k - 1] || "";
 };
 
-console.log(kthDistinct(["d", "b", "c", "b", "c", "a"], 2));
-console.log(kthDistinct(["aaa", "aa", "a"], 1));
-console.log(kthDistinct(["a", "b", "a"], 3));
+console.log(kthDistinct(["d" , "b" , "c" , "b" , "c" , "a"] , 2));
+console.log(kthDistinct(["aaa" , "aa" , "a"] , 1));
+console.log(kthDistinct(["a" , "b" , "a"] , 3));
