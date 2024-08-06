1a524e40e60bebf8203bf3a9e2640c17680c3522
Irene Volodko <ice7aye@gmail.com>
2024-08-06 22:44:59 +0300

    description number of common factors

--- 2427-number-of-common-factors.js
+++ 2427-number-of-common-factors.js
@@ -1,5 +1,11 @@
-let commonFactors = function (a, b) {
-    let min = Math.min(a, b);
+// ❓ DESCRIPTION:
+// Given two positive integers a and b, return the number of common factors of a and b.
+//
+// An integer x is a common factor of a and b if x divides both a and b.
+//
+// ✅ SOLUTION:
+let commonFactors = function ( a , b ) {
+    let min = Math.min(a , b);
     let factors = 0;
     for (let i = 1; i <= min; i++) {
         if (a % i === 0 && b % i === 0) factors++;
@@ -7,5 +13,5 @@ let commonFactors = function (a, b) {
     return factors;
 };
 
-console.log(commonFactors(12, 6));
-console.log(commonFactors(25, 30));
+console.log(commonFactors(12 , 6));
+console.log(commonFactors(25 , 30));
