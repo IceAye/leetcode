016f1de275c93dcc56ddd011ba813c0326c8d03f
Irene Volodko <ice7aye@gmail.com>
2024-08-07 20:50:16 +0300

    description remove trailing zeros from a string

--- 2710-remove-trailing-zeros from-a string.js
+++ 2710-remove-trailing-zeros from-a string.js
@@ -1,3 +1,7 @@
+// ❓ DEDSCRIPTION:
+// Given a positive integer num represented as a string, return the integer num without trailing zeros as a string.
+//
+// ✅ SOLUTION:
 let removeTrailingZeros = function (num) {
     return num.replace(/0+$/g, "");
 };
