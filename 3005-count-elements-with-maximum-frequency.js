28c94f457cdd1ca129223e30083c06f8f24b4583
Irene Volodko <ice7aye@gmail.com>
2024-08-08 22:42:00 +0300

    description count elements with maximum frequency

--- 3005-count-elements-with-maximum-frequency.js
+++ 3005-count-elements-with-maximum-frequency.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given an array nums consisting of positive integers.
+//
+// Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+//
+// The frequency of an element is the number of occurrences of that element in the array.
+//
+// ✅ SOLUTION:
 let maxFrequencyElements = function (nums) {
     let elements = new Map();
     for (const num of nums) {
