20bd3a61d03f61528231ca39fe32a36b234d4c3b
Irene Volodko <ice7aye@gmail.com>
2024-08-12 17:57:12 +0300

    description calculate delayed arrival time

--- 2651-calculate-delayed-arrival-time.js
+++ 2651-calculate-delayed-arrival-time.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given a positive integer arrivalTime denoting the arrival time of a train in hours, and another positive
+// integer delayedTime denoting the amount of delay in hours.  Return the time when the train will arrive at the
+// station.  Note that the time in this problem is in 24-hours format.
+//
+// ✅ SOLUTION:
 let findDelayedArrivalTime = function ( arrivalTime , delayedTime ) {
     let time = arrivalTime + delayedTime;
     return time % 24;
