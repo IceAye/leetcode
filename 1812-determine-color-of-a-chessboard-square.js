4748220800c30870ed052e2ba7940351662f43f8
Irene Volodko <ice7aye@gmail.com>
2024-08-12 17:41:05 +0300

    description determine color of a chessboard square

--- 1812-determine-color-of-a-chessboard-square.js
+++ 1812-determine-color-of-a-chessboard-square.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
+// Return true if the square is white, and false if the square is black.
+// The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+//
+// ✅ SOLUTION:
 let squareIsWhite = function (coordinates) {
     const abc = "abcdefgh";
     const digits = "12345678";
