77274813901697b34e7a6e838a2fc63078df08e0
Irene Volodko <ice7aye@gmail.com>
2024-08-14 16:45:51 +0300

    description number of students unable to eat lunch

--- 1700-numbers-of-students-unable-to-eat-lunch.js
+++ 1700-numbers-of-students-unable-to-eat-lunch.js
@@ -1,5 +1,16 @@
-let countStudents = function (students, sandwiches) {
-    const count = [0, 0];
+// ❓ DESCRIPTION:
+// The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1
+// respectively. All students stand in a queue. Each student either prefers square or circular sandwiches.
+// The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches are placed in a stack.
+// At each step:
+// If the student at the front of the queue prefers the sandwich on the top of the stack, they will take it and
+// leave the queue. Otherwise, they will leave it and go to the queue's end. This continues until none of the queue
+// students want to take the top sandwich and are thus unable to eat.
+// Return the number of students that are unable to eat.
+//
+// ✅ SOLUTION:
+let countStudents = function ( students , sandwiches ) {
+    const count = [0 , 0];
 
     for (const student of students) {
         count[student]++;
@@ -14,9 +25,9 @@ let countStudents = function (students, sandwiches) {
     return 0;
 };
 
-console.log(countStudents([1, 1, 0, 0], [0, 1, 0, 1]));
-console.log(countStudents([1, 1], [0, 1]));
-console.log(countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1]));
+console.log(countStudents([1 , 1 , 0 , 0] , [0 , 1 , 0 , 1]));
+console.log(countStudents([1 , 1] , [0 , 1]));
+console.log(countStudents([1 , 1 , 1 , 0 , 0 , 1] , [1 , 0 , 0 , 0 , 1 , 1]));
 console.log(
-    countStudents([0, 0, 0, 1, 1, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 1, 1, 0, 0, 0]),
+    countStudents([0 , 0 , 0 , 1 , 1 , 1 , 1 , 0 , 0 , 0] , [1 , 0 , 1 , 0 , 0 , 1 , 1 , 0 , 0 , 0]) ,
 );
