efc54cd323085d746cf8b5333ac2c9deffe321b0
Irene Volodko <ice7aye@gmail.com>
2024-08-17 18:56:39 +0300

    description find greatest common divisor of array

--- 1979-find-greatest-common-divisor-of-array.js
+++ 1979-find-greatest-common-divisor-of-array.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+// The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+//
+// ✅ SOLUTION:
 let findGCD = function (nums) {
     let min = Math.min(...nums);
     let max = Math.max(...nums);
