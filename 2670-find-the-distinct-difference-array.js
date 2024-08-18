c9df6f1056aa78919ebd04f4732dfb29715ef161
Irene Volodko <ice7aye@gmail.com>
2024-08-18 14:06:12 +0300

    description find the distinct difference array

--- 2670-find-the-distinct-difference-array.js
+++ 2670-find-the-distinct-difference-array.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed array nums of length n.
+// The distinct difference array of nums is an array diff of length n such that diff[i] is equal to the number of
+// distinct elements in the suffix nums[i + 1, ..., n - 1] subtracted from the number of distinct elements in the
+// prefix nums[0, ..., i].
+// Return the distinct difference array of nums.  Note that nums[i, ..., j] denotes the subarray of nums starting at index i
+// and ending at index j inclusive. Particularly, if i > j then nums[i, ..., j] denotes an empty subarray.
+//
+// ✅ SOLUTION:
 let distinctDifferenceArray = function (nums) {
     let result = [];
 
@@ -11,5 +20,5 @@ let distinctDifferenceArray = function (nums) {
     return result;
 };
 
-console.log(distinctDifferenceArray([1, 2, 3, 4, 5]));
-console.log(distinctDifferenceArray([3, 2, 3, 4, 2]));
+console.log(distinctDifferenceArray([1 , 2 , 3 , 4 , 5]));
+console.log(distinctDifferenceArray([3 , 2 , 3 , 4 , 2]));
