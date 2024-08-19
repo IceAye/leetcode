6f22a52bc8177c92235e77c980f84de8e2a547f0
Irene Volodko <ice7aye@gmail.com>
2024-08-19 22:41:17 +0300

    description sum of digits in base k

--- 1837-sum-of-digits-in-base-k.js
+++ 1837-sum-of-digits-in-base-k.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
+//
+// After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
+//
+// ✅ SOLUTION:
 let sumBase = function (n, k) {
     return Array.from(n.toString(k).split(""), Number).reduce(
         (sum, current) => sum + current,
