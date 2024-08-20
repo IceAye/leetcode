3189c2a5f73c840c43a0ab23518868da5188268b
Irene Volodko <ice7aye@gmail.com>
2024-08-20 10:45:48 +0300

    description unique number of occurrences

--- 1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+//
+// ✅ SOLUTION:
 let uniqueOccurrences = function (arr) {
     let map = new Map();
 
