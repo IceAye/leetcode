4165c0d3a8e40c6caa827b6859cf9387e57a873b
Irene Volodko <ice7aye@gmail.com>
2024-08-21 15:16:39 +0300

    add solution using binary search (count negative numbers in a sorted matrix)

--- 1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -15,3 +15,41 @@ let countNegatives = function (grid) {
 let countNegatives = function (grid) {
     return grid.flat().filter(item => item < 0).length;
 };
+
+// SOLUTION 3 (binary search), the most efficient:
+let countNegatives = function (grid) {
+    let count = 0;
+    let length = grid[0].length;
+
+    for (const row of grid) {
+        let start = 0;
+        let end = length - 1;
+
+        while (start <= end) {
+            let middle = Math.floor((start + end) / 2)
+
+            if (row[middle] >= 0) {
+                start = middle + 1
+            } else end = middle - 1
+        }
+
+        count += length - start;
+    }
+
+    return count;
+};
+
+console.log(
+    countNegatives([
+        [4, 3, 2, -1],
+        [3, 2, 1, -1],
+        [1, 1, -1, -2],
+        [-1, -1, -2, -3],
+    ]),
+);
+console.log(
+    countNegatives([
+        [3, 2],
+        [1, 0],
+    ]),
+);
