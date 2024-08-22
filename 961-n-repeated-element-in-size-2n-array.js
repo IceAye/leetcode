4512bcd8bfa49f4ac0bbf5b5f9c998f247beaf43
Irene Volodko <ice7aye@gmail.com>
2024-08-22 03:40:42 +0300

    description n repeated element in size 2n array

--- 961-n-repeated-element-in-size-2n-array.js
+++ 961-n-repeated-element-in-size-2n-array.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums with the following properties:
+//
+// nums.length == 2 * n.
+// nums contains n + 1 unique elements.
+// Exactly one element of nums is repeated n times.
+// Return the element that is repeated n times.
+//
+// ✅ SOLUTION:
 let repeatedNTimes = function (nums) {
     let set = new Set();
 
