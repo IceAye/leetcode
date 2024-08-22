88d65a70b4d68052b2b076e0aae3914edf01ac60
Irene Volodko <ice7aye@gmail.com>
2024-08-22 04:04:46 +0300

    add solution 3 for neither minimum nor maximum

--- 2733-neither-minimum-nor-maximum.js
+++ 2733-neither-minimum-nor-maximum.js
@@ -15,7 +15,17 @@ let findNonMinOrMax = function (nums) {
     return nums.sort((a, b) => a - b)[1];
 };
 
+// SOLUTION 3:
+let findNonMinOrMax = function (nums) {
+    if (nums.length <= 2) return -1;
 
+    let min = Math.min(nums[0], nums[1]);
+    let max = Math.max(nums[0], nums[1])
+
+    if (nums[2] > max) return max;
+    if (nums[2] < min) return min;
+    return nums[2];
+};
 
 console.log(findNonMinOrMax([3, 2, 1, 4]));
 console.log(findNonMinOrMax([1, 2]));
