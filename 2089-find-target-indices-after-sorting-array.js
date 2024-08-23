a8fd91a7a36b0c85e80c1cbe497461a4abe15205
Irene Volodko <ice7aye@gmail.com>
2024-08-23 16:10:16 +0300

    refactor find target indices after sorting array

--- 2089-find-target-indices-after-sorting-array.js
+++ 2089-find-target-indices-after-sorting-array.js
@@ -1,11 +1,13 @@
 let targetIndices = function (nums, target) {
-    let sorted = nums.sort((a, b) => a - b);
     let indices = [];
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i] === target) indices.push(i);
-    }
+    nums
+        .sort((a, b) => a - b)
+        .map((item, index) => {
+            if (item === target) indices.push(index);
+        });
     return indices;
 };
+
 console.log(targetIndices([1, 2, 5, 2, 3], 2));
 console.log(targetIndices([1, 2, 5, 2, 3], 3));
 console.log(targetIndices([1, 2, 5, 2, 3], 5));
