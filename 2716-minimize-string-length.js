4b0d9346e95b1eaea507175a8d4ac9d5b2e4236c
Irene Volodko <ice7aye@gmail.com>
2024-08-24 15:41:01 +0300

    description minimize string length

--- 2716-minimize-string-length.js
+++ 2716-minimize-string-length.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// Given a string s, you have two types of operation:
+//
+// Choose an index i in the string, and let c be the character in position i. Delete the closest occurrence of c to the left of i (if exists).
+// Choose an index i in the string, and let c be the character in position i. Delete the closest occurrence of c to the right of i (if exists).
+// Your task is to minimize the length of s by performing the above operations zero or more times.
+//
+// Return an integer denoting the length of the minimized string.
+//
+// ✅ SOLUTION:
+// Since the minimized string will not contain duplicate characters but will contain all distinct characters of the original string, we use Set.
 let minimizedStringLength = function (s) {
     return new Set(s).size;
 };
