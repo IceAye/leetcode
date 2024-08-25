f45115f4492085749257a265132b4b25ee6dc479
Irene Volodko <ice7aye@gmail.com>
2024-08-25 17:00:39 +0300

    description find n unique integers sum up to zero

--- 1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given an integer n, return any array containing n unique integers such that they add up to 0.
+//
+// ✅ SOLUTION:
 let sumZero = function (n) {
     let array = [];
     for (let i = 1; i <= n / 2; i++) {
