b5bc5d9a6c343de3819b0a9756ada8d6bb3c751e
Irene Volodko <ice7aye@gmail.com>
2024-08-25 16:40:47 +0300

    add solution 2 sort array by parity

--- 905-sort-array-by-parity.js
+++ 905-sort-array-by-parity.js
@@ -10,5 +10,19 @@ let sortArrayByParity = function (nums) {
     return [...even, ...odd];
 };
 
+// SOLUTION 2:
+let sortArrayByParity = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        while (left < right && nums[left] % 2 === 0) left++;
+        while (left < right && nums[right] % 2 !== 0) right--;
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+    }
+
+    return nums;
+};
+
 console.log(sortArrayByParity([3, 1, 2, 4]));
 console.log(sortArrayByParity([0]));
