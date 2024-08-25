8725e6040f6594d2f2226d1437282f0fb7906209
Irene Volodko <ice7aye@gmail.com>
2024-08-25 16:59:16 +0300

    add find n unique integers sum up to zero

---
+++ 1304-find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,13 @@
+let sumZero = function (n) {
+    let array = [];
+    for (let i = 1; i <= n / 2; i++) {
+        array.push(i, -i);
+    }
+    return n % 2 === 0 ? array : [0, ...array];
+};
+
+console.log(sumZero(5));
+console.log(sumZero(4));
+console.log(sumZero(3));
+console.log(sumZero(7));
+
