5f3cf54807b27f9d5e683c187f0683faeba0f369
Irene Volodko <ice7aye@gmail.com>
2024-08-27 18:43:55 +0300

    description make two arrays equal by reversing subarrays

--- 1460-make-two-arrays-equal-by-reversing-subarrays.js
+++ 1460-make-two-arrays-equal-by-reversing-subarrays.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty subarray of arr and reverse it. You are allowed to make any number of steps.
+//
+// Return true if you can make arr equal to target or false otherwise.
+//
+// ✅ SOLUTION:
 let canBeEqual = function (target, arr) {
     return target.sort((a, b) => a - b).toString() === arr.sort((a, b) => a - b).toString()
 };
