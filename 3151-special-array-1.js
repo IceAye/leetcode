631ea862e607d339cbd7e3fc611db899ec1c8eb0
Irene Volodko <ice7aye@gmail.com>
2024-08-29 22:02:07 +0300

    description special array 1

--- 3151-special-array-1.js
+++ 3151-special-array-1.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+//
+// You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+//
+// ✅ SOLUTION:
 let isArraySpecial = function (nums) {
     for (let i = 0; i < nums.length - 1; i++) {
         if ((nums[i] + nums[i + 1]) % 2 === 0) return false;
