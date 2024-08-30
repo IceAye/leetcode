6b189351ff71f5e520b7a3e2a789e0741352df3b
Irene Volodko <ice7aye@gmail.com>
2024-08-30 12:49:54 +0300

    description two out of three

--- 2032-two-out-of-three.js
+++ 2032-two-out-of-three.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given three integer arrays nums1, nums2, and nums3, return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
+//
+// ✅ SOLUTION:
 let twoOutOfThree = function (nums1, nums2, nums3) {
     let result = [];
     for (const num of nums1) {
