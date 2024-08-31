3d546426714bef1f146d729b38f1308d0e0ae74b
Irene Volodko <ice7aye@gmail.com>
2024-08-31 15:56:55 +0300

    description points that intersect with cars

--- 2848-points-that-intersect-with-cars.js
+++ 2848-points-that-intersect-with-cars.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed 2D integer array nums representing the coordinates of the cars parking on a number line. For any index i, nums[i] = [starti, endi] where starti is the starting point of the ith car and endi is the ending point of the ith car.
+//
+// Return the number of integer points on the line that are covered with any part of a car.
+//
+// ✅ SOLUTION:
 let numberOfPoints = function (nums) {
     nums.sort((a, b) => a[0] - b[0]);
     let mergedArray = [nums[0]];
