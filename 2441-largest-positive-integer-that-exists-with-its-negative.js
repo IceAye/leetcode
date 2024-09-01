05108ef6e9130cea86585ba417ba8e93d276a0bf
Irene Volodko <ice7aye@gmail.com>
2024-09-02 02:37:34 +0300

    add solution 2 for largest positive-integer-that-exists with its negative

--- 2441-largest-positive-integer-that-exists-with-its-negative.js
+++ 2441-largest-positive-integer-that-exists-with-its-negative.js
@@ -1,3 +1,4 @@
+// SOLUTION 1:
 let findMaxK = function (nums) {
     let array = [];
     for (const num of nums) {
@@ -5,3 +6,18 @@ let findMaxK = function (nums) {
     }
     return array.length > 0 ? Math.max(...array) : -1;
 };
+
+// SOLUTION 2 (with set):
+let findMaxK = function (nums) {
+    let result = -1;
+    let numSet = new Set(nums)
+    for (const num of numSet) {
+        if (numSet.has(-num)) result = Math.max(result, num);
+    }
+    return result;
+};
+
+console.log(findMaxK([-1, 2, -3, 3]));
+console.log(findMaxK([-1, 10, 6, 7, -7, 1]));
+console.log(findMaxK([-10, 8, 6, 7, -2, -3]));
+console.log(findMaxK([2, 8, 6, 7, -2, -3]));
