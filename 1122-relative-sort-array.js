b6652e2da147612ced2d68b79e0dc12255b6a444
Irene Volodko <ice7aye@gmail.com>
2024-09-04 23:14:41 +0300

    description relative sort array

--- 1122-relative-sort-array.js
+++ 1122-relative-sort-array.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+//
+// Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+//
+// ✅ SOLUTION:
 let relativeSortArray = function (arr1, arr2) {
     let start = arr1.sort((a, b) => arr2.indexOf(a) - arr2.indexOf(b));
     let end = start.splice(0, start.indexOf(arr2[0])).sort((a, b) => a - b);
