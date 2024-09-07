133dba174d5fd6cd2a5793c163086b60222371a3
Irene Volodko <ice7aye@gmail.com>
2024-09-07 16:50:50 +0300

    description find the sum of encrypted integers

--- 3079-find-the-sum-of-encrypted-integers.js
+++ 3079-find-the-sum-of-encrypted-integers.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums containing positive integers. We define a function encrypt such that encrypt(x) replaces every digit in x with the largest digit in x. For example, encrypt(523) = 555 and encrypt(213) = 333.
+//
+// Return the sum of encrypted elements.
+//
+// ✅ SOLUTION:
 let sumOfEncryptedInt = function (nums) {
     let result = 0;
     for (let num of nums) {
