29b9366b25bbc6c88e14f558bb4ca9cd41872e94
Irene Volodko <ice7aye@gmail.com>
2024-09-07 17:23:09 +0300

    refactor count operations to obtain zero

--- 2169-count-operations-to-obtain-zero.js
+++ 2169-count-operations-to-obtain-zero.js
@@ -2,13 +2,8 @@ let countOperations = function (num1, num2) {
     let count = 0;
 
     while (num1 > 0 && num2 > 0) {
-        if (num1 >= num2) {
-            count++;
-            num1 -= num2;
-        } else {
-            count++;
-            num2 -= num1;
-        }
+        num1 >= num2 ? (num1 -= num2) : (num2 -= num1);
+        count++;
     }
     return count;
 };
