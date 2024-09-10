b8f7b840af34bfb3ca8a21f02312fec4b763877b
Irene Volodko <ice7aye@gmail.com>
2024-09-10 22:02:42 +0300

    description count the number of vowel strings in range

--- 2586-count-the-number-of-vowel-strings-in-range.js
+++ 2586-count-the-number-of-vowel-strings-in-range.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed array of string words and two integers left and right.
+//
+// A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+//
+// Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+//
+// ✅ SOLUTION:
+
 let vowelStrings = function (words, left, right) {
     const vowels = ["a","e","i","o","u"];
     let count = 0;
