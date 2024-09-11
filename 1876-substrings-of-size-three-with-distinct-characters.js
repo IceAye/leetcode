29a77fb2200ce13903843d17375ad5c116872649
Irene Volodko <ice7aye@gmail.com>
2024-09-12 02:11:53 +0300

    add substrings of size three with distinct characters

---
+++ 1876-substrings-of-size-three-with-distinct-characters.js
@@ -0,0 +1,10 @@
+let countGoodSubstrings = function (s) {
+    let count = 0;
+    for (let i = 0; i < s.length - 2; i++) {
+        if (s[i] !== s[i + 1] && s[i + 1] !== s[i + 2] && s[i] !== s[i + 2] ) count++;
+    }
+    return count;
+};
+
+console.log(countGoodSubstrings("xyzzaz"));
+console.log(countGoodSubstrings("aababcabc"));
