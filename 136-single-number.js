e0c623230d459a42fe0bbf486ebcd77603474f40
Irene Volodko <ice7aye@gmail.com>
2024-09-12 02:56:15 +0300

    description single number

--- 136-single-number.js
+++ 136-single-number.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+//
+// ✅ SOLUTION:
 let singleNumber = function (nums) {
     let x = 0
     for (let num of nums) {
