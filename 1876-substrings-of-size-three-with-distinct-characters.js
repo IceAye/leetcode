9b24b17d4fbdecf7c1917b6ddcffecd84502dd4e
Irene Volodko <ice7aye@gmail.com>
2024-09-12 02:20:29 +0300

    add solution 2 substrings of size three with distinct characters

--- 1876-substrings-of-size-three-with-distinct-characters.js
+++ 1876-substrings-of-size-three-with-distinct-characters.js
@@ -1,7 +1,19 @@
+// SOLUTION 1:
 let countGoodSubstrings = function (s) {
     let count = 0;
     for (let i = 0; i < s.length - 2; i++) {
         if (s[i] !== s[i + 1] && s[i + 1] !== s[i + 2] && s[i] !== s[i + 2] ) count++;
+    }
+    return count;
+};
+
+// SOLUTION 2 (set):
+let countGoodSubstrings = function (s) {
+    let count = 0;
+    for (let i = 0; i < s.length - 2; i++) {
+        let word = [s[i], s[i + 1], s[i + 2]];
+        let set = new Set(word);
+        if (set.size === 3) count++;
     }
     return count;
 };
