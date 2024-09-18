cc4cdb414c72a5ddcb8f0c9545c293a627ca72ce
Irene Volodko <ice7aye@gmail.com>
2024-09-18 15:08:37 +0300

    description number of even and odd bits

--- 2595-number-of-even-and-odds-bits.js
+++ 2595-number-of-even-and-odds-bits.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given a positive integer n.
+// Let even denote the number of even indices in the binary representation of n with value 1.
+// Let odd denote the number of odd indices in the binary representation of n with value 1.
+// Note that bits are indexed from right to left in the binary representation of a number.
+// Return the array [even, odd].
+//
+// ✅ SOLUTION (runtime 91.51%, memory 95.28%):
 let evenOddBit = function (n) {
     let count = [0, 0];
     let array = [...n.toString(2)].reverse();
