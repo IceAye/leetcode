3677ca2d228694f2dd9312d560f92c12ebbe6580
Irene Volodko <ice7aye@gmail.com>
2024-09-19 12:46:09 +0300

    add smallest index with equal value

---
+++ 2057-smallest-index-with-equal-value.js
@@ -0,0 +1,10 @@
+let smallestEqual = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 10 === nums[i]) return i;
+    }
+    return -1;
+};
+
+console.log(smallestEqual([0,1,2]));
+console.log(smallestEqual([4,3,2,1]));
+console.log(smallestEqual([1,2,3,4,5,6,7,8,9,0]));
