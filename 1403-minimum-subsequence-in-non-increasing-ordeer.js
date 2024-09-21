7c63a94b48acb34033b95be9e845f767eda45ae5
Irene Volodko <ice7aye@gmail.com>
2024-09-21 23:37:48 +0300

    description minimum subsequence in non-increasing order

--- 1403-minimum-subsequence-in-non-increasing-ordeer.js
+++ 1403-minimum-subsequence-in-non-increasing-ordeer.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence.
+//
+// If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array.
+//
+// Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
+//
+// ✅ SOLUTION (runtime 99.20%):
 let minSubsequence = function (nums) {
     nums.sort((a, b) => b - a)
     let right = nums.reduce((a, b) => a + b);
