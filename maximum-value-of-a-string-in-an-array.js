00b5e7da4788c86dda073deea38e50e7d1fbdfdd
Irene Volodko <ice7aye@gmail.com>
2024-09-23 22:49:11 +0300

    description maximum value of a string in an array

--- maximum-value-of-a-string-in-an-array.js
+++ maximum-value-of-a-string-in-an-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// The value of an alphanumeric string can be defined as:
+// The numeric representation of the string in base 10, if it comprises of digits only.
+// The length of the string, otherwise.
+// Given an array strs of alphanumeric strings, return the maximum value of any string in strs.
+//
+// ✅ SOLUTION:
 let maximumValue = function (strs) {
     let result = 0;
     for (const str of strs) {
@@ -6,5 +13,6 @@ let maximumValue = function (strs) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(maximumValue(["alic3", "bob", "3", "4", "00000"]));
 console.log(maximumValue(["1", "01", "001", "0001"]));
