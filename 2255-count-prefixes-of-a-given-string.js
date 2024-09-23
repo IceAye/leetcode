73347318ceab3fc8ac5d0b1da096f1aeccd6bf4e
Irene Volodko <ice7aye@gmail.com>
2024-09-23 22:26:39 +0300

    description count prefixes of a given string

--- 2255-count-prefixes-of-a-given-string.js
+++ 2255-count-prefixes-of-a-given-string.js
@@ -1,8 +1,15 @@
+// ❓ DESCRIPTION:
+// You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+// Return the number of strings in words that are a prefix of s.
+// A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+//
+// ✅ SOLUTION:
 let countPrefixes = function (words, s) {
     let count = 0;
     words.map(item => s.startsWith(item) ? count++ : count);
     return count;
 };
 
+// 📌 TESTCASE:
 console.log(countPrefixes(["a", "b", "c", "ab", "bc", "abc"], "abc"));
 console.log(countPrefixes(["a", "a"], "aa"));
