c7100ac9125b4761b7ffa24ee1d0678791734046
Irene Volodko <ice7aye@gmail.com>
2024-09-24 13:39:48 +0300

    description make array zero by subtracting equal amounts

--- 2357-make-array-zero-by-subtracting-equal-amounts.js
+++ 2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -1,7 +1,15 @@
+// ❓ DESCRIPTION:
+// You are given a non-negative integer array nums. In one operation, you must:
+// Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+// Subtract x from every positive element in nums.
+// Return the minimum number of operations to make every element in nums equal to 0.
+//
+// ✅ SOLUTION:
 let minimumOperations = function(nums) {
     let set = new Set(nums);
     return set.has(0) ? set.size - 1 : set.size;
 };
 
+// 📌 TESTCASE:
 console.log(minimumOperations([1,5,0,3,5]))
 console.log(minimumOperations([0]))
