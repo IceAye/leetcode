5fccd72e134a71b3127ceeec04d17b34210756dd
Irene Volodko <ice7aye@gmail.com>
2024-09-29 20:17:23 +0300

    description squares of a sorted array

--- 977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+//
+// ✅ SOLUTION:
 let sortedSquares = function (nums) {
     return nums.map((item) => item ** 2).sort((a,b) => a - b);
 };
+// 📌 TESTCASE:
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
+console.log(sortedSquares([-7, -3, 2, 3, 11]));
