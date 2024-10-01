365eb760480da480fc0bc3eb171e553b6ceadf11
Irene Volodko <ice7aye@gmail.com>
2024-10-01 14:59:35 +0300

    description check if all s appears before all b

--- 2124-check-if-all-a-appears-before-all-b.js
+++ 2124-check-if-all-a-appears-before-all-b.js
@@ -1,8 +1,13 @@
+// ❓ DESCRIPTION:
+// Given a string s consisting of only the characters 'a' and 'b', return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
+//
+// ✅ SOLUTION (runtime 96.99%):
 let checkString = function (s) {
     let indexOfFirstB = s.indexOf("b");
     return indexOfFirstB === -1 || !s.slice(indexOfFirstB).includes("a");
 };
 
+// 📌 TESTCASE:
 console.log(checkString("aaabbb"));
 console.log(checkString("abab"));
 console.log(checkString("bbb"));
