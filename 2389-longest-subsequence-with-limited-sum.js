224e588381d0a7ee5045dd0bc1cd01ca3cf92ef5
Irene Volodko <ice7aye@gmail.com>
2024-10-01 21:48:17 +0300

    description longest subsequence with limited sum

--- 2389-longest-subsequence-with-limited-sum.js
+++ 2389-longest-subsequence-with-limited-sum.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums of length n, and an integer array queries of length m.
+// Return an array answer of length m where answer[i] is the maximum size of a subsequence that you can take from nums such that the sum of its elements is less than or equal to queries[i].
+// A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+//
+// ✅ SOLUTION:
 let answerQueries = function(nums, queries) {
     let answer = []
     nums.sort((a, b) => a - b);
@@ -15,5 +21,6 @@ let answerQueries = function(nums, queries) {
     return answer;
 };
 
+// 📌 TESTCASE:
 console.log(answerQueries([4,5,2,1], [3,10,21]));
 console.log(answerQueries([2,3,4,5], [1]));
