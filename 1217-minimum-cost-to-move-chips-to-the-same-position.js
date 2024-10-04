89e01980eafce3ce184c6bdfa72b4b2e2dc1046f
Irene Volodko <ice7aye@gmail.com>
2024-10-04 14:51:39 +0300

    description solution 2 minimum cost to move chips to the same position

--- 1217-minimum-cost-to-move-chips-to-the-same-position.js
+++ 1217-minimum-cost-to-move-chips-to-the-same-position.js
@@ -1,4 +1,11 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// We have n chips, where the position of the ith chip is position[i].
+// We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+// position[i] + 2 or position[i] - 2 with cost = 0.
+// position[i] + 1 or position[i] - 1 with cost = 1.
+// Return the minimum cost needed to move all the chips to the same position.
+//
+// ✅ SOLUTION 1:
 let minCostToMoveChips = function(position) {
     let even = 0;
     let odd = 0;
@@ -6,7 +13,7 @@ let minCostToMoveChips = function(position) {
     return Math.min(even, odd)
 };
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 let minCostToMoveChips = function(position) {
     return Math.min(...position.reduce(
         (acc, current) => {
