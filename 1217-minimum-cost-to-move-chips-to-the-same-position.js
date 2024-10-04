9d7eb3b3c6cf279fe58cce47aa0d46f0c56a4df5
Irene Volodko <ice7aye@gmail.com>
2024-10-04 14:50:42 +0300

    add solution 2 minimum cost to move chips to the same position

--- 1217-minimum-cost-to-move-chips-to-the-same-position.js
+++ 1217-minimum-cost-to-move-chips-to-the-same-position.js
@@ -1,8 +1,20 @@
+// SOLUTION 1:
 let minCostToMoveChips = function(position) {
     let even = 0;
     let odd = 0;
     position.map(item => item % 2 === 0 ? even++ : odd++)
     return Math.min(even, odd)
+};
+
+// SOLUTION 2:
+let minCostToMoveChips = function(position) {
+    return Math.min(...position.reduce(
+        (acc, current) => {
+            current % 2 ? acc[1]++ : acc[0]++;
+            return acc;
+        },
+        [0, 0],
+    ))
 };
 
 console.log(minCostToMoveChips([1,2,3]));
