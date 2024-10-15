31e85f928246b34969b303be94c798ab90c4e58f
Irene Volodko <ice7aye@gmail.com>
2024-10-15 22:17:18 +0300

    description crawler log function

--- crawler-log-folder.js
+++ crawler-log-folder.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// The Leetcode file system keeps a log each time some user performs a change folder operation.
+// The operations are described below:
+// "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+// "./" : Remain in the same folder.
+// "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+// You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+//
+// The file system starts in the main folder, then the operations in logs are performed.
+//
+// Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+//
+// ✅ SOLUTION:
 let minOperations = function (logs) {
     let stack = [];
     logs.map((item) =>
@@ -6,6 +19,7 @@ let minOperations = function (logs) {
     return stack.length;
 };
 
+// 📌 TESTCASE:
 console.log(minOperations(["d1/", "d2/", "../", "d21/", "./"]));
 console.log(minOperations(["d1/", "d2/", "./", "d3/", "../", "d31/"]));
 console.log(minOperations(["d1/", "../", "../", "../"]));
