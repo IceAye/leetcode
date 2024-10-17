f34f82a3893d5be33d03b4badf8aa9528cd7684e
Irene Volodko <ice7aye@gmail.com>
2024-10-17 12:28:38 +0300

    description count pairs of similar strings

--- 2506-count-pairs-of-similar-strings.js
+++ 2506-count-pairs-of-similar-strings.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed string array words.
+// Two strings are similar if they consist of the same characters.
+// For example, "abca" and "cba" are similar since both consist of characters 'a', 'b', and 'c'.
+// However, "abacba" and "bcfd" are not similar since they do not consist of the same characters.
+// Return the number of pairs (i, j) such that 0 <= i < j <= word.length - 1 and the two strings words[i] and words[j] are similar.
+//
+// ✅ SOLUTION:
 let similarPairs = function (words) {
     let sets = words.map(item => [...new Set([...item].sort())].join(""));
     let count = 0;
@@ -9,6 +17,7 @@ let similarPairs = function (words) {
     return count;
 };
 
+// 📌 TESTCASE:
 console.log(similarPairs(["aba","aabb","abcd","bac","aabc"]))
 console.log(similarPairs(["aabb","ab","ba"]))
 console.log(similarPairs(["nba","cba","dba"]))
