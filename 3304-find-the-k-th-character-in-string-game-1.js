f8d55e24cf8b775740783b9c152dafc452dad76f
Irene Volodko <ice7aye@gmail.com>
2024-10-22 23:23:35 +0300

    description find the k-th character in string game

--- 3304-find-the-k-th-character-in-string-game-1.js
+++ 3304-find-the-k-th-character-in-string-game-1.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+// You are given a positive integer k.
+// Now Bob will ask Alice to perform the following operation forever:
+// Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+// For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+// Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+// Note that the character 'z' can be changed to 'a' in the operation.
+//
+// ✅ SOLUTION (runtime 98.51%):
 let kthCharacter = function(k) {
     let a = "a";
     while (a.length - 1 < k) {
@@ -7,5 +17,6 @@ let kthCharacter = function(k) {
     return a.at(k - 1)
 };
 
+// 📌 TESTCASE:
 console.log(kthCharacter(5))
 console.log(kthCharacter(10))
