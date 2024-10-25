1d0c8174c172045184762ce87b6a66e22194e169
Irene Volodko <ice7aye@gmail.com>
2024-10-25 12:43:14 +0300

    description check distances between two letters

--- 2399-check-distances-between-same-letters.js
+++ 2399-check-distances-between-same-letters.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed string s consisting of only lowercase English letters, where each letter in s appears exactly twice. You are also given a 0-indexed integer array distance of length 26.
+// Each letter in the alphabet is numbered from 0 to 25 (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, ... , 'z' -> 25).
+// In a well-spaced string, the number of letters between the two occurrences of the ith letter is distance[i]. If the ith letter does not appear in s, then distance[i] can be ignored.
+// Return true if s is a well-spaced string, otherwise return false.
+//
+// ✅ SOLUTION (runtime 97.73%):
 let checkDistances = function(s, distance) {
     const abc = "abcdefghijklmnopqrstuvwxyz";
     for (const letter of s) {
@@ -6,5 +13,6 @@ let checkDistances = function(s, distance) {
     return true;
 };
 
+// 📌 TESTCASE:
 console.log(checkDistances("abaccb", [1,3,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])) // true
 console.log(checkDistances("aa", [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])) // false
