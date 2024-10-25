e39126c503e73113c6a986dfdf3886a73f2bbac4
Irene Volodko <ice7aye@gmail.com>
2024-10-25 13:24:54 +0300

    description sort array by parity

--- 922-sort-array-by-parity-2.js
+++ 922-sort-array-by-parity-2.js
@@ -1,4 +1,9 @@
-// SOLUTION 1:
+// ❓ DESCRIPTION:
+// Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+// Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+// Return any answer array that satisfies this condition.
+//
+// ✅ SOLUTION 1:
 let sortArrayByParityII = function(nums) {
     let result = [];
     let [evens, odds] = nums.reduce((acc, curr) => {
@@ -11,7 +16,7 @@ let sortArrayByParityII = function(nums) {
     return result
 };
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 let sortArrayByParityII = function(nums) {
     let result = [];
     let evenIndex = 0;
@@ -28,5 +33,6 @@ let sortArrayByParityII = function(nums) {
     return result
 };
 
+// 📌 TESTCASE:
 console.log(sortArrayByParityII([4,2,5,7])) // [4,5,2,7] || [4,7,2,5] || [2,5,4,7] || [2,7,4,5]
 console.log(sortArrayByParityII([2,3])) // [2,3]
