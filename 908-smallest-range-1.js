bc1ac9e56852c2d6a3d082d386835beb09fca46c
Irene Volodko <ice7aye@gmail.com>
2024-10-26 17:33:51 +0300

    description smallest range 1

--- 908-smallest-range-1.js
+++ 908-smallest-range-1.js
@@ -1,8 +1,16 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums and an integer k.
+// In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+// The score of nums is the difference between the maximum and minimum elements in nums.
+// Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+//
+// ✅ SOLUTION (runtime 100%):
 let smallestRangeI = function(nums, k) {
     let result = (Math.max(...nums) - k) - (Math.min(...nums) + k);
     return result >= 0 ? result : 0;
 };
 
+// 📌 TESTCASE:
 console.log(smallestRangeI([1], 0)) // 0
 console.log(smallestRangeI([0,10], 2)) // 6
 console.log(smallestRangeI([1,3,6], 3)) // 0
