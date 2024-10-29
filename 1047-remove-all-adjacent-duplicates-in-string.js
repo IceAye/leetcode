c5570afa46a22b95513b398c5a6dc769d5777e88
Irene Volodko <ice7aye@gmail.com>
2024-10-29 22:36:50 +0300

    description remove all adjacent duplicates in string

--- 1047-remove-all-adjacent-duplicates-in-string.js
+++ 1047-remove-all-adjacent-duplicates-in-string.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+// We repeatedly make duplicate removals on s until we no longer can.
+// Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+//
+// ✅ SOLUTION:
 let removeDuplicates = function(s) {
     let stack = [];
     for (const char of s) {
@@ -6,7 +12,7 @@ let removeDuplicates = function(s) {
     return stack.join("");
 };
 
-
+// 📌 TESTCASE:
 console.log(removeDuplicates("abbaca")) // "ca"
 console.log(removeDuplicates("azxxzy")) // "ay"
 console.log(removeDuplicates("abbbabaaa")) // "ababa"
