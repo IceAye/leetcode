4632025a82b6259c4f253a2fe7c8f4bc5751df8e
Irene Volodko <ice7aye@gmail.com>
2024-10-31 22:16:08 +0300

    description can make arithmetic progression from sequence

--- 1502-can-make-arithmetic-progression-from-sequence.js
+++ 1502-can-make-arithmetic-progression-from-sequence.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
+// Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
+//
+// ✅ SOLUTION:
 let canMakeArithmeticProgression = function(arr) {
     let sortedArr = arr.sort((a, b) => a - b);
     for (let i = 0; i < sortedArr.length - 2; i++) {
@@ -6,6 +11,7 @@ let canMakeArithmeticProgression = function(arr) {
     return true;
 };
 
+// 📌 TESTCASE:
 console.log(canMakeArithmeticProgression([3,5,1])) // true
 console.log(canMakeArithmeticProgression([1,2,4])) // false
 
