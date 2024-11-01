5a09996961330632c92f0dea107da3948ee54ea5
Irene Volodko <ice7aye@gmail.com>
2024-11-01 21:52:43 +0300

    add mean of array after removing some elements

---
+++ 1619-mean-of-array-after-removing-some-elements.js
@@ -0,0 +1,9 @@
+let trimMean = function(arr) {
+    let index = arr.length / 20;
+    let resulrArr = arr.sort((a, b) => a - b).slice(index, -index)
+    return +(resulrArr.reduce((acc, curr) => acc + curr, 0) / resulrArr.length).toFixed(5)
+};
+
+console.log(trimMean([1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3])) // 2.00000
+console.log(trimMean([6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0])) // 4.00000
+console.log(trimMean([6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4])) // 4.77778
