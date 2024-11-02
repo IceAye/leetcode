413bab68fdb38b75f17aabe9c5d8b656c828ad26
Irene Volodko <ice7aye@gmail.com>
2024-11-02 12:59:45 +0300

    description circular sentence

--- 2490-circular-sentence.js
+++ 2490-circular-sentence.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// For example, "Hello World", "HELLO", "hello world hello world" are all sentences.
+// Words consist of only uppercase and lowercase English letters. Uppercase and lowercase English letters are considered different.
+// A sentence is circular if:
+// The last character of a word is equal to the first character of the next word.
+// The last character of the last word is equal to the first character of the first word.
+// For example, "leetcode exercises sound delightful", "eetcode", "leetcode eats soul" are all circular sentences. However, "Leetcode is cool", "happy Leetcode", "Leetcode" and "I like Leetcode" are not circular sentences.
+// Given a string sentence, return true if it is circular. Otherwise, return false.
+//
+// ✅ SOLUTION (runtime 100%):
+
 let isCircularSentence = function(sentence) {
     if (sentence.at(0) !== sentence.at(-1)) return false
     let array = sentence.split(" ");
@@ -7,6 +19,7 @@ let isCircularSentence = function(sentence) {
     return true;
 };
 
+// 📌 TESTCASE:
 console.log(isCircularSentence("leetcode exercises sound delightful")) // true
 console.log(isCircularSentence("eetcode")) // true
 console.log(isCircularSentence("Leetcode is cool")) // false
