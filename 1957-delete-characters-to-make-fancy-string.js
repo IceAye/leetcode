c1314b509224d2acf38d7e5e2cdbd3a80a909a51
Irene Volodko <ice7aye@gmail.com>
2024-11-02 18:42:58 +0300

    description delete characters to make fancy string

--- 1957-delete-characters-to-make-fancy-string.js
+++ 1957-delete-characters-to-make-fancy-string.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// A fancy string is a string where no three consecutive characters are equal.
+// Given a string s, delete the minimum possible number of characters from s to make it fancy.
+// Return the final string after the deletion. It can be shown that the answer will always be unique.
+//
+// ✅ SOLUTION:
+
 let makeFancyString = function (s) {
     let result = s[0]
     for (let i = 1; i < s.length; i++) {
@@ -7,6 +14,7 @@ let makeFancyString = function (s) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(makeFancyString("leeetcode")); // "leetcode"
 console.log(makeFancyString("aaabaaaa")); // "aabaa"
 console.log(makeFancyString("aab")); // "aab"
