c7e85b0e9a237158a1718534554cb55162a96ec8
Irene Volodko <ice7aye@gmail.com>
2024-11-03 21:55:50 +0300

    description rotate string

--- 796-rotate-string.js
+++ 796-rotate-string.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+// For example, if s = "abcde", then it will be "bcdea" after one shift.
+//
+// ✅ SOLUTION (runtime 100%):
 let rotateString = function (s, goal) {
     for (let i = 0; i < s.length; i++) {
         let startIndex;
@@ -7,6 +13,7 @@ let rotateString = function (s, goal) {
     return false;
 };
 
+// 📌 TESTCASE:
 console.log(rotateString("abcde", "cdeab")); // true
 console.log(rotateString("abcde", "abced")); // false
 console.log(rotateString("dvadererrerdvaddf", "rerdvaddfdvaderer")); // true
