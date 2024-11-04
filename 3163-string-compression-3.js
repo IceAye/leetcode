1d438236ac18e9c8706c78dd8a739ae67ba227c0
Irene Volodko <ice7aye@gmail.com>
2024-11-04 19:41:04 +0300

    description string compression

--- 3163-string-compression-3.js
+++ 3163-string-compression-3.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Given a string word, compress it using the following algorithm:
+//
+// Begin with an empty string comp. While word is not empty, use the following operation:
+// Remove a maximum length prefix of word made of a single character c repeating at most 9 times.
+// Append the length of the prefix followed by c to comp.
+// Return the string comp.
+//
+// ✅ SOLUTION:
 let compressedString = function(word) {
     let comp = "";
     let count = 1;
@@ -14,6 +23,7 @@ let compressedString = function(word) {
     return comp;
 };
 
+// 📌 TESTCASE:
 console.log(compressedString("abcde")) // "1a1b1c1d1e"
 console.log(compressedString("aaaaaaaaaaaaaabb")) // "9a5a2b"
 console.log(compressedString("aa")) // "2a"
