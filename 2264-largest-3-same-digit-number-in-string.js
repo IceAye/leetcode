980703b71614dc42094766322d924bd0a594cae7
Irene Volodko <ice7aye@gmail.com>
2024-11-05 22:34:19 +0300

    description largest 3 same digit number in string

--- 2264-largest-3-same-digit-number-in-string.js
+++ 2264-largest-3-same-digit-number-in-string.js
@@ -1,4 +1,13 @@
-// SOLUTION 1 (runtime 78.8%):
+// ❓ DESCRIPTION:
+// You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+// It is a substring of num with length 3.
+// It consists of only one unique digit.
+// Return the maximum good integer as a string or an empty string "" if no such integer exists.
+// Note:
+// A substring is a contiguous sequence of characters within a string.
+// There may be leading zeroes in num or a good integer.
+//
+// ✅ SOLUTION 1 (runtime 78.8%):
 let largestGoodInteger = function(num) {
     let result = [];
     for (let i = 0; i < num.length - 2; i++) {
@@ -8,7 +17,7 @@ let largestGoodInteger = function(num) {
     return max < 0 ? "" : max.toString().repeat(3);
 };
 
-// SOLUTION 2:
+// ✅ SOLUTION 2:
 let largestGoodInteger = function(num) {
     let max = "";
     for (let i = 0; i < num.length - 2; i++) {
@@ -20,7 +29,7 @@ let largestGoodInteger = function(num) {
     return max;
 };
 
-
+// 📌 TESTCASE:
 console.log(largestGoodInteger("6777133339")) // "777"
 console.log(largestGoodInteger("2300019")) // "000"
 console.log(largestGoodInteger("42352338")) // ""
