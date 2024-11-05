20b39492147b51ddd8b4b9269ebeddbcd3a2b9ad
Irene Volodko <ice7aye@gmail.com>
2024-11-05 20:38:26 +0300

    description minimum number of changes to make binary string beautiful

--- 2914 -minimum-number-of-changes-to-make-binary-string-beautiful.js
+++ 2914 -minimum-number-of-changes-to-make-binary-string-beautiful.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed binary string s having an even length.
+// A string is beautiful if it's possible to partition it into one or more substrings such that:
+// Each substring has an even length.
+// Each substring contains only 1's or only 0's.
+// You can change any character in s to 0 or 1.
+// Return the minimum number of changes required to make the string s beautiful.
+//
+// ✅ SOLUTION (runtime 100%):
 let minChanges = function (s) {
     let count = 0;
     for (let i = 0; i < s.length; i += 2) {
@@ -6,6 +15,7 @@ let minChanges = function (s) {
     return count;
 };
 
+// 📌 TESTCASE:
 console.log(minChanges("1001")) // 2
 console.log(minChanges("10")) // 1
 console.log(minChanges("0000")) // 0
