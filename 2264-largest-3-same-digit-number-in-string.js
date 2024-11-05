576370ed3451ad3ed408b15b24ef267412bc650e
Irene Volodko <ice7aye@gmail.com>
2024-11-05 22:32:57 +0300

    add solution 2 largest 3 same digit number in string

--- 2264-largest-3-same-digit-number-in-string.js
+++ 2264-largest-3-same-digit-number-in-string.js
@@ -1,3 +1,4 @@
+// SOLUTION 1 (runtime 78.8%):
 let largestGoodInteger = function(num) {
     let result = [];
     for (let i = 0; i < num.length - 2; i++) {
@@ -6,6 +7,19 @@ let largestGoodInteger = function(num) {
     let max = Math.max(...result);
     return max < 0 ? "" : max.toString().repeat(3);
 };
+
+// SOLUTION 2:
+let largestGoodInteger = function(num) {
+    let max = "";
+    for (let i = 0; i < num.length - 2; i++) {
+        if (num[i] === num[i + 1] && num[i] === num[i + 2]) {
+            let string = num[i].repeat(3);
+            if (string > max) max = string;
+        }
+    }
+    return max;
+};
+
 
 console.log(largestGoodInteger("6777133339")) // "777"
 console.log(largestGoodInteger("2300019")) // "000"
