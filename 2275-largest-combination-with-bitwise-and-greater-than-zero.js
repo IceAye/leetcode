eb3fb51b09bc646f0c04b5f580c79dd83d0ded8e
Irene Volodko <ice7aye@gmail.com>
2024-11-07 20:31:04 +0300

    add solution 2 largest combination with bitwise and greater than zero

--- 2275-largest-combination-with-bitwise-and-greater-than-zero.js
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero.js
@@ -1,3 +1,5 @@
+
+// SOLUTION 1:
 const largestCombination = function(candidates) {
     let result = [];
     let binaries = candidates.map(item => item.toString(2));
@@ -10,6 +12,20 @@ const largestCombination = function(candidates) {
     }
     return Math.max(...result);
 };
+
+// SOLUTION 2:
+const largestCombination = function(candidates) {
+    let result = [];
+    for (let i = 1; i <= 24; i++) {
+        let count = 0;
+        for (let j = 0; j < candidates.length; j++) {
+            if (candidates[j] & (1 << i)) count++;
+        }
+        result.push(count);
+    }
+    return Math.max(...result);
+};
+
 
 console.log(largestCombination([16,17,71,62,12,24,14])) // 4
 console.log(largestCombination([8,8])) // 2
