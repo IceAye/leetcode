502ac5c5cf8e4863e995fbd2f3583cc4eab9ff56
Irene Volodko <ice7aye@gmail.com>
2024-11-08 13:59:51 +0300

    description find the encrypted string

--- 3210-find-the-encrypted-string.js
+++ 3210-find-the-encrypted-string.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given a string s and an integer k. Encrypt the string using the following algorithm:
+// For each character c in s, replace c with the kth character after c in the string (in a cyclic manner).
+// Return the encrypted string.
+//
+// ✅ SOLUTION:
+
 const getEncryptedString = function(s, k) {
     let result = "";
     for (let i = 0; i < s.length; i++) {
@@ -6,5 +13,6 @@ const getEncryptedString = function(s, k) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(getEncryptedString("dart", 3)) // "tdar"
 console.log(getEncryptedString("aaa", 1)) // "aaa"
