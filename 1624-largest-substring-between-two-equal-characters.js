9982c53f0578d828db3d473ebd01fae57bf08b07
Irene Volodko <ice7aye@gmail.com>
2024-11-11 18:24:20 +0300

    description largest substring between two equal characters

--- 1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
+// A substring is a contiguous sequence of characters within a string.
+//
+// ✅ SOLUTION:
 let maxLengthBetweenEqualCharacters = function(s) {
     let result = [];
     for (const char of s) {
@@ -6,6 +11,7 @@ let maxLengthBetweenEqualCharacters = function(s) {
     return Math.max(...result)
 };
 
+// 📌 TESTCASE:
 console.log(maxLengthBetweenEqualCharacters("aa")) // 0
 console.log(maxLengthBetweenEqualCharacters("abca")) // 2
 console.log(maxLengthBetweenEqualCharacters("cbzxy")) // -1
