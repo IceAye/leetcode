2c3c2b575f2fe4e3f25598dc2e5a2ce9c7f760eb
Irene Volodko <ice7aye@gmail.com>
2024-11-16 15:12:12 +0300

    description three consecutive odds

--- 1550-three-consecutive-odds.js
+++ 1550-three-consecutive-odds.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+//
+// ✅ SOLUTION (runtime 100%):
 const threeConsecutiveOdds = function(arr) {
     for (let i = 0; i < arr.length - 2; i++) {
         if (arr[i] % 2 === 1 && arr[i + 1] % 2 === 1 && arr[i + 2] % 2 === 1) return true;
@@ -5,5 +9,6 @@ const threeConsecutiveOdds = function(arr) {
     return false;
 };
 
+// 📌 TESTCASE:
 console.log(threeConsecutiveOdds([2,6,4,1])) // false
 console.log(threeConsecutiveOdds([1,2,34,3,4,5,7,23,12])) // true
