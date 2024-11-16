27b5ef5ab48aa1f121c694fdad602ce47296bdfa
Irene Volodko <ice7aye@gmail.com>
2024-11-16 15:02:39 +0300

    description intersection of multiple arrays

--- 2248-intersection-of-multiple-arrays.js
+++ 2248-intersection-of-multiple-arrays.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers, return the list of integers that are present in each array of nums sorted in ascending order.
+//
+// ✅ SOLUTION:
 const intersection = function(nums) {
     let result = [];
 
@@ -13,5 +17,6 @@ const intersection = function(nums) {
     return result.sort((a, b) => a - b);
 };
 
+// 📌 TESTCASE:
 console.log(intersection([[3,1,2,4,5],[1,2,3,4],[3,4,5,6]])) // [3,4]
 console.log(intersection([[1,2,3],[4,5,6]])) // []
