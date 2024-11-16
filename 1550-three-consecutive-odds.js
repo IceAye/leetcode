261587a4dfbf4dd333036cf1c3fb116580f0df5e
Irene Volodko <ice7aye@gmail.com>
2024-11-16 15:10:03 +0300

    add three consecutive odds

---
+++ 1550-three-consecutive-odds.js
@@ -0,0 +1,9 @@
+const threeConsecutiveOdds = function(arr) {
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr[i] % 2 === 1 && arr[i + 1] % 2 === 1 && arr[i + 2] % 2 === 1) return true;
+    }
+    return false;
+};
+
+console.log(threeConsecutiveOdds([2,6,4,1])) // false
+console.log(threeConsecutiveOdds([1,2,34,3,4,5,7,23,12])) // true
