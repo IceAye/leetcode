212123612a706c95eac38366032cb578cb0bc9a4
Irene Volodko <ice7aye@gmail.com>
2024-11-21 13:37:57 +0300

    add description

--- 1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+// Return the largest lucky integer in the array. If there is no lucky integer return -1.
+//
+// ✅ SOLUTION:
 const findLucky = function(arr) {
     let map = new Map();
     arr.map(item => map.set(item, map.get(item) + 1 || 1));
@@ -7,6 +12,7 @@ const findLucky = function(arr) {
     }, -1);
 };
 
+// 📌 TESTCASE:
 console.log(findLucky([2,2,3,4])) // 2
 console.log(findLucky([1,2,2,3,3,3])) // 3
 console.log(findLucky([2,2,2,3,3])) // -1
