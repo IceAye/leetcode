3d152ae41e86d4afb1f0b5dd9c395dcdc9f337a5
Irene Volodko <ice7aye@gmail.com>
2024-11-23 23:51:41 +0300

    add description

--- 2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+// If the length of the array is 0, the function should return init.
+// Please solve it without using the built-in Array.reduce method.
+//
+// ✅ SOLUTION:
 let reduce = function(nums, fn, init) {
     let result = init
 
@@ -7,6 +14,7 @@ let reduce = function(nums, fn, init) {
     return result
 }
 
+// 📌 TESTCASE:
 console.log(reduce(nums = [1,2,3,4], fn = function sum(accum, curr) { return accum + curr; }, init = 0)) // 10
 console.log(reduce(nums = [1,2,3,4], fn = function sum(accum, curr) { return accum + curr * curr; }, init = 100)) // 130
 console.log(reduce(nums = [], fn = function sum(accum, curr) { return 0; }, init = 25)) // 25
