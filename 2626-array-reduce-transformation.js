ce6635d521025abb77513a5d57ebfd5e2fc1f818
Irene Volodko <ice7aye@gmail.com>
2024-11-23 23:50:54 +0300

    add tests

--- 2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation.js
@@ -6,3 +6,7 @@ let reduce = function(nums, fn, init) {
     }
     return result
 }
+
+console.log(reduce(nums = [1,2,3,4], fn = function sum(accum, curr) { return accum + curr; }, init = 0)) // 10
+console.log(reduce(nums = [1,2,3,4], fn = function sum(accum, curr) { return accum + curr * curr; }, init = 100)) // 130
+console.log(reduce(nums = [], fn = function sum(accum, curr) { return 0; }, init = 25)) // 25
