0cc7109a45d6dd6cb540ffb10d3b7fe98956aa50
Irene Volodko <ice7aye@gmail.com>
2024-11-24 15:03:26 +0300

    add description

--- 1991-find-the-middle-index-in-array.js
+++ 1991-find-the-middle-index-in-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+// A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+// If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+// Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+//
+// ✅ SOLUTION:
 const findMiddleIndex = function(nums) {
     let leftSum = 0;
     let rightSum = nums.reduce((acc, curr) => acc + curr);
@@ -9,6 +16,7 @@ const findMiddleIndex = function(nums) {
     return -1;
 };
 
+// 📌 TESTCASE:
 console.log(findMiddleIndex([2,3,-1,8,4])) // 3
 console.log(findMiddleIndex([1,-1,4])) // 2
 console.log(findMiddleIndex([2,5])) // -1
