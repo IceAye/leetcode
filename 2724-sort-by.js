79f42254f5540636b6c945371a6b147af7576eb3
Irene Volodko <ice7aye@gmail.com>
2024-11-25 19:40:57 +0300

    add description

--- 2724-sort-by.js
+++ 2724-sort-by.js
@@ -1,8 +1,13 @@
+// ❓ DESCRIPTION:
+// Given an array arr and a function fn, return a sorted array sortedArr. You can assume fn only returns numbers and those numbers determine the sort order of sortedArr. sortedArr must be sorted in ascending order by fn output.
+// You may assume that fn will never duplicate numbers for a given array.
+//
+// ✅ SOLUTION:
 const sortBy = function(arr, fn) {
     return arr.sort((a, b) => fn(a) - fn(b));
 };
 
-
+// 📌 TESTCASE:
 console.log(sortBy([5, 4, 1, 2, 3], (x) => x)) // [1, 2, 3, 4, 5]
 console.log(sortBy([{"x": 1}, {"x": 0}, {"x": -1}], (d) => d.x)) // [{"x": -1}, {"x": 0}, {"x": 1}]
 console.log(sortBy([[3, 4], [5, 2], [10, 1]], (x) => x[1])) // [[10, 1], [5, 2], [3, 4]]
