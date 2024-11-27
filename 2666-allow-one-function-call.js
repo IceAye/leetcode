cb1fbcf40d5793170f8fdde62278137f6dee56cc
Irene Volodko <ice7aye@gmail.com>
2024-11-27 23:24:43 +0300

    add description

--- 2666-allow-one-function-call.js
+++ 2666-allow-one-function-call.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+//
+// ✅ SOLUTION:
 let once = function ( fn ) {
     let flag = 0
     return function ( ...args ) {
