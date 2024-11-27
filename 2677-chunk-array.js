01c608b1929b645c6632ffa9544256fc05719a5d
Irene Volodko <ice7aye@gmail.com>
2024-11-27 23:31:43 +0300

    add description

--- 2677-chunk-array.js
+++ 2677-chunk-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given an array arr and a chunk size size, return a chunked array.
+// A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
+// You may assume the array is the output of JSON.parse. In other words, it is valid JSON.
+// Please solve it without using lodash's _.chunk function.
+//
+// ✅ SOLUTION:
 let chunk = function ( arr , size ) {
     let resultArr = []
     let i = 0
@@ -10,3 +17,8 @@ let chunk = function ( arr , size ) {
     }
     return resultArr
 }
+
+// 📌 TESTCASE:
+console.log(chunk([1,2,3,4,5], 1)) // [[1],[2],[3],[4],[5]]
+console.log(chunk([1,9,6,3,2], 3)) // [[1,9,6],[3,2]]
+console.log(chunk([8,5,3,2,6], 6)) // [[8,5,3,2,6]]
