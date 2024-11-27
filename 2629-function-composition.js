d476b2d1ad1139f777d23b4c3d7c0b46e58ec009
Irene Volodko <ice7aye@gmail.com>
2024-11-27 23:20:54 +0300

    add description

--- 2629-function-composition.js
+++ 2629-function-composition.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+// The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+// The function composition of an empty list of functions is the identity function f(x) = x.
+// You may assume each function in the array accepts one integer as input and returns one integer as output.
+//
+// ✅ SOLUTION:
 let compose = function ( functions ) {
     return x => functions.reduceRight(( prev , acc ) => acc(prev) , x)
 }
