3dc5e6fe17dd75f189c13b037c8303bb0c692b79
Irene Volodko <ice7aye@gmail.com>
2024-12-02 17:32:20 +0300

    add solution 2

--- 258-add-digits.js
+++ 258-add-digits.js
@@ -4,6 +4,10 @@ const addDigits = function (num) {
     return num < 10 ? num : addDigits(num);
 };
 
+const addDigits = function (num) {
+    return 1 + ((num - 1) % 9);
+};
+
 console.log(addDigits(38)); // 2
 console.log(addDigits(0)); // 0
 console.log(addDigits(234)); // 9
