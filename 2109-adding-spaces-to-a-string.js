a85b0e5f4625b8be03959ebc921bbfadefc82ee0
Irene Volodko <ice7aye@gmail.com>
2024-12-03 04:18:29 +0300

    add description

--- 2109-adding-spaces-to-a-string.js
+++ 2109-adding-spaces-to-a-string.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. Each space should be inserted before the character at the given index.
+// For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
+// Return the modified string after the spaces have been added.
+//
+// ✅ SOLUTION:
 const addSpaces = function(s, spaces) {
     let string = '';
     let count = 0;
@@ -5,11 +11,12 @@ const addSpaces = function(s, spaces) {
         if (i === spaces[count]) {
             string += ' ' + s[i];
             count++;
-        } else string += s[i]
+        } else string += s[i];
     }
     return string;
 };
 
+// 📌 TESTCASE:
 console.log(addSpaces("LeetcodeHelpsMeLearn", [8,13,15])) // "Leetcode Helps Me Learn"
 console.log(addSpaces("icodeinpython", [1,5,7,9])) // "i code in py thon"
 console.log(addSpaces("spacing", [0,1,2,3,4,5,6])) //  " s p a c i n g"
