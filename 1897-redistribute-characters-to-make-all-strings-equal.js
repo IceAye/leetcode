a72199cc07dc22af5b53c5b5fc9aa9ec717b58c8
Irene Volodko <ice7aye@gmail.com>
2024-12-05 15:20:08 +0300

    add descsription

--- 1897-redistribute-characters-to-make-all-strings-equal.js
+++ 1897-redistribute-characters-to-make-all-strings-equal.js
@@ -1,8 +1,14 @@
+// ❓ DESCRIPTION:
+// In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+// Return true if you can make every string in words equal using any number of operations, and false otherwise.
+//
+// ✅ SOLUTION:
 const makeEqual = function (words) {
     let map = new Map();
     [...words.join("")].map((item) => map.set(item, map.get(item) + 1 || 1));
     return [...map.values()].every((item) => item % words.length === 0);
 };
 
+// 📌 TESTCASE:
 console.log(makeEqual(["abc", "aabc", "bc"])); // true
 console.log(makeEqual(["ab", "a"])); // false
