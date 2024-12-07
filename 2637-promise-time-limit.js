8f9b4f18c2f45842476badf899e0eb8ae6c25565
Irene Volodko <ice7aye@gmail.com>
2024-12-07 17:38:32 +0300

    add description

--- 2637-promise-time-limit.js
+++ 2637-promise-time-limit.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given an asynchronous function fn and a time t in milliseconds, return a new time limited version of the input function. fn takes arguments provided to the time limited function.
+// The time limited function should follow these rules:
+// If the fn completes within the time limit of t milliseconds, the time limited function should resolve with the result.
+// If the execution of the fn exceeds the time limit, the time limited function should reject with the string "Time Limit Exceeded".
+//
+// ✅ SOLUTION:
 const timeLimit = function (fn, t) {
     return function (...args) {
         const timeoutPromise = new Promise((resolve, reject) => {
@@ -8,5 +15,6 @@ const timeLimit = function (fn, t) {
     };
 };
 
+// 📌 TESTCASE:
 const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
 limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
