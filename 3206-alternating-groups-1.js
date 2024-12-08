2c7977403a9b63b28b4251b49996a8480799c01e
Irene Volodko <ice7aye@gmail.com>
2024-12-08 19:37:53 +0300

    add description

--- 3206-alternating-groups-1.js
+++ 3206-alternating-groups-1.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// There is a circle of red and blue tiles. You are given an array of integers colors. The color of tile i is represented by colors[i]:
+// colors[i] == 0 means that tile i is red.
+// colors[i] == 1 means that tile i is blue.
+// Every 3 contiguous tiles in the circle with alternating colors (the middle tile has a different color from its left and right tiles) is called an alternating group.
+// Return the number of alternating groups.
+// Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+//
+// ✅ SOLUTION:
 const numberOfAlternatingGroups = function(colors) {
     let length = colors.length;
     let count = 0;
@@ -8,5 +17,6 @@ const numberOfAlternatingGroups = function(colors) {
     return count;
 };
 
+// 📌 TESTCASE:
 console.log(numberOfAlternatingGroups([1,1,1])) // 0
 console.log(numberOfAlternatingGroups([0,1,0,0,1])) // 3
