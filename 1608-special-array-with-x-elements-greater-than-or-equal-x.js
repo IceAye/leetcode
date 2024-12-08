86e2dc4f1fe6a60311e3e5f92758390162f511f1
Irene Volodko <ice7aye@gmail.com>
2024-12-08 20:06:32 +0300

    add description

--- 1608-special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
+// Notice that x does not have to be an element in nums.
+// Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
+//
+// ✅ SOLUTION:
 const specialArray = function(nums) {
     let i = 0;
     while (i <= nums.length) {
@@ -14,6 +20,7 @@ const specialArray = function(nums) {
     return -1;
 };
 
+// 📌 TESTCASE:
 console.log(specialArray([3,5])) // 2
 console.log(specialArray([0,0])) // -1
 console.log(specialArray([0,4,3,0,4])) // 3
