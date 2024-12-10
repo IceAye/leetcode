4f6dfc066ac1db2e68c5c21d698d17f1c74b26cf
Irene Volodko <ice7aye@gmail.com>
2024-12-10 21:02:53 +0300

    add description

--- 1694-reformat-phone-number.js
+++ 1694-reformat-phone-number.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// You are given a phone number as a string number. number consists of digits, spaces ' ', and/or dashes '-'.
+// You would like to reformat the phone number in a certain manner. Firstly, remove all spaces and dashes. Then, group the digits from left to right into blocks of length 3 until there are 4 or fewer digits. The final digits are then grouped as follows:
+// 2 digits: A single block of length 2.
+// 3 digits: A single block of length 3.
+// 4 digits: Two blocks of length 2 each.
+// The blocks are then joined by dashes. Notice that the reformatting process should never produce any blocks of length 1 and produce at most two blocks of length 2.
+// Return the phone number after formatting.
+//
+// ✅ SOLUTION (runtime 100%):
 const reformatNumber = function (number) {
     let string = number.replace(/[-\s]/g, "");
     let n = string.length;
@@ -24,6 +34,7 @@ const reformatNumber = function (number) {
     return prefix;
 };
 
+// 📌 TESTCASE:
 console.log(reformatNumber("1-23-45 6")) // "123-456"
 console.log(reformatNumber("123 4-567")) // "123-45-67"
 console.log(reformatNumber("123 4-5678")) // "123-456-78"
