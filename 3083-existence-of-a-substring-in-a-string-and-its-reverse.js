0e6cf3e5846d957c6e9b12e4da0683964632d40b
Irene Volodko <ice7aye@gmail.com>
2024-12-13 22:26:04 +0300

    add description

--- 3083-existence-of-a-substring-in-a-string-and-its-reverse.js
+++ 3083-existence-of-a-substring-in-a-string-and-its-reverse.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given a string s, find any substring of length 2 which is also present in the reverse of s.
+// Return true if such a substring exists, and false otherwise.
+//
+// ✅ SOLUTION:
 const isSubstringPresent = function (s) {
     for (let i = 1; i < s.length; i++) {
         let str = `${s[i]}${s[i - 1]}`;
@@ -6,6 +11,7 @@ const isSubstringPresent = function (s) {
     return false;
 };
 
+// 📌 TESTCASE:
 console.log(isSubstringPresent("leetcode")); // true
 console.log(isSubstringPresent("abcba")); // true
 console.log(isSubstringPresent("abcd")); // false
