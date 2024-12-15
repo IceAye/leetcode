255ae9ce20130324f2a365415ec78b3d6c71a157
Irene Volodko <ice7aye@gmail.com>
2024-12-15 19:01:10 +0300

    add description

--- 2078-two-furthest-houses-with-different-colors.js
+++ 2078-two-furthest-houses-with-different-colors.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// There are n houses evenly lined up on the street, and each house is beautifully painted. You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
+// Return the maximum distance between two houses with different colors.
+// The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+//
+// ✅ SOLUTION:
 const maxDistance = function (colors) {
     let left = colors[0];
     let right = colors[colors.length - 1];
@@ -17,6 +23,7 @@ const maxDistance = function (colors) {
     return Math.max(differColorLeft, colors.length - 1 - differColorRight);
 };
 
+// 📌 TESTCASE:
 console.log(maxDistance([1, 1, 1, 6, 1, 1, 1])); // 3
 console.log(maxDistance([1, 8, 3, 8, 3])); // 4
 console.log(maxDistance([0, 1])); // 1
