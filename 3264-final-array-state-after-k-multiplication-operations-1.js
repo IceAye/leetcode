39af7bcd8b021bbf0491c957eee455b8e0a7d113
Irene Volodko <ice7aye@gmail.com>
2024-12-16 17:12:20 +0300

    add description

--- 3264-final-array-state-after-k-multiplication-operations-1.js
+++ 3264-final-array-state-after-k-multiplication-operations-1.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums, an integer k, and an integer multiplier.
+// You need to perform k operations on nums. In each operation:
+// Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+// Replace the selected minimum value x with x * multiplier.
+// Return an integer array denoting the final state of nums after performing all k operations.
+//
+// ✅ SOLUTION:
 const getFinalState = function (nums, k, multiplier) {
     while (k > 0) {
         let min = Math.min(...nums);
@@ -7,5 +15,6 @@ const getFinalState = function (nums, k, multiplier) {
     return nums;
 };
 
+// 📌 TESTCASE:
 console.log(getFinalState([2, 1, 3, 5, 6], 5, 2)); // [8,4,6,5,6]
 console.log(getFinalState([1, 2], 3, 4)); // [16,8]
