6a7a2ce6df333449afd2c72884deec04c5e4b72f
Irene Volodko <ice7aye@gmail.com>
2024-12-17 15:57:50 +0300

    add description

--- 2395-find-subarrays-with-equal-sum.js
+++ 2395-find-subarrays-with-equal-sum.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given a 0-indexed integer array nums, determine whether there exist two subarrays of length 2 with equal sum. Note that the two subarrays must begin at different indices.
+// Return true if these subarrays exist, and false otherwise.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+//
+// ✅ SOLUTION (runtime 100%):
 const findSubarrays = function (nums) {
     let map = new Map();
     for (let i = 0; i < nums.length - 1; i++) {
@@ -10,6 +16,7 @@ const findSubarrays = function (nums) {
     return false;
 };
 
+// 📌 TESTCASE:
 console.log(findSubarrays([4, 2, 4])); // true
 console.log(findSubarrays([1, 2, 3, 4, 5])); // false
 console.log(findSubarrays([0, 0, 0])); // true
