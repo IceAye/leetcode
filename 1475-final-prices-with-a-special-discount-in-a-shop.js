2785ab767a609be3298503f2691e68f93bf7478d
Irene Volodko <ice7aye@gmail.com>
2024-12-18 21:41:20 +0300

    add description

--- 1475-final-prices-with-a-special-discount-in-a-shop.js
+++ 1475-final-prices-with-a-special-discount-in-a-shop.js
@@ -1,3 +1,9 @@
+// 📌 DESCRIPTION:
+// You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+// There is a special discount for items in the shop. If you buy the ith item, then you will receive a discount equivalent to prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i]. Otherwise, you will not receive any discount at all.
+// Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, considering the special discount.
+//
+// ✅ SOLUTION:
 const finalPrices = function (prices) {
     let result = [];
     for (let i = 0; i < prices.length; i++) {
@@ -12,6 +18,7 @@ const finalPrices = function (prices) {
     return [...result];
 };
 
+// 📌 TESTCASE:
 console.log(finalPrices([8, 4, 6, 2, 3])); // [4,2,4,2,3]
 console.log(finalPrices([1, 2, 3, 4, 5])); // [1,2,3,4,5]
 console.log(finalPrices([10, 1, 1, 6])); // [9,0,1,6]
