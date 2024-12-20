c7bf45c84b632e721a6e7aa4e92907f47d5cf683
Irene Volodko <ice7aye@gmail.com>
2024-12-20 21:38:20 +0300

    add description

--- 2129-capitalize-the-title.js
+++ 2129-capitalize-the-title.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given a string title consisting of one or more words separated by a single space, where each word consists
+// of English letters. Capitalize the string by changing the capitalization of each word such that:  If the length of
+// the word is 1 or 2 letters, change all letters to lowercase. Otherwise, change the first letter to uppercase and the
+// remaining letters to lowercase. Return the capitalized title.
+//
+// ✅ SOLUTION:
 const capitalizeTitle = function (title) {
     let words = title.split(" ");
     let result = [];
@@ -11,6 +18,7 @@ const capitalizeTitle = function (title) {
     return result.join(" ");
 };
 
+// 📌 TESTCASE:
 console.log(capitalizeTitle("capiTalIze tHe titLe")); // "Capitalize The Title"
 console.log(capitalizeTitle("First leTTeR of EACH Word")); // "First Letter of Each Word"
 console.log(capitalizeTitle("i lOve leetcode")); // "i Love Leetcode"
