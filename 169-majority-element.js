5ebd46652db0bd4f678d8c7f655755693064a528
Irene Volodko <ice7aye@gmail.com>
2024-12-21 21:43:32 +0300

    add description

--- 169-majority-element.js
+++ 169-majority-element.js
@@ -1,10 +1,17 @@
+// ❓ DESCRIPTION:
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element
+// always exists in the array.
+//
+// ✅ SOLUTION:
 const majorityElement = function (nums) {
     let map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        map.set(nums[i], map.get(nums[i]) + 1 || 1);
+        map.set(nums[i] , map.get(nums[i]) + 1 || 1);
         if (map.get(nums[i]) > nums.length / 2) return nums[i];
     }
 };
 
-console.log(majorityElement([3, 2, 3])); // 3
-console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // 2
+// 📌 TESTCASE:
+console.log(majorityElement([3 , 2 , 3])); // 3
+console.log(majorityElement([2 , 2 , 1 , 1 , 1 , 2 , 2])); // 2
