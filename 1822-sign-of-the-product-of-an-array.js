c52cd68e6d072d2cb84abfb77dfa9782b7ffbca0
Irene Volodko <ice7aye@gmail.com>
2024-12-22 06:49:56 +0300

    add description

--- 1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array.js
@@ -1,8 +1,20 @@
+// ❓ DESCRIPTION:
+// Implement a function signFunc(x) that returns:
+//
+// 1 if x is positive.
+// -1 if x is negative.
+// 0 if x is equal to 0.
+// You are given an integer array nums. Let product be the product of all values in the array nums.
+//
+// Return signFunc(product).
+
+// ✅ SOLUTION:
 const arraySign = function (nums) {
     let product = nums.reduce((acc, curr) => acc * curr);
     return product > 0 ? 1 : product < 0 ? -1 : 0;
 };
 
+// 📌 TESTCASE:
 console.log(arraySign([-1, -2, -3, -4, 3, 2, 1])); // 1
 console.log(arraySign([1, 5, 0, 2, -3])); // 0
 console.log(arraySign([-1, 1, -1, 1, -1])); // -1
