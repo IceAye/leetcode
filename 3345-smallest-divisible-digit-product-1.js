b83a5533ac1f5a47ea34d1f54ab957268c25e584
Irene Volodko <ice7aye@gmail.com>
2024-12-22 07:11:57 +0300

    add description

--- 3345-smallest-divisible-digit-product-1.js
+++ 3345-smallest-divisible-digit-product-1.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// You are given two integers n and t. Return the smallest number greater than or equal to n such that the product of its digits is divisible by t.
+//
+// ✅ SOLUTION:
 const smallestNumber = function(n, t) {
     for (let i = n; ; i++) {
         let number = Array.from(i.toString(), Number).reduce((acc, curr) => acc * curr);
@@ -5,5 +9,6 @@ const smallestNumber = function(n, t) {
     }
 };
 
+// 📌 TESTCASE:
 console.log(smallestNumber(10, 2)) // 10
 console.log(smallestNumber(15, 3)) // 16
