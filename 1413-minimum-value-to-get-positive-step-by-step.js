67c20e2382134809afcb7120ef158a416d5a4e23
Irene Volodko <ice7aye@gmail.com>
2024-12-23 08:53:06 +0300

    add description

--- 1413-minimum-value-to-get-positive-step-by-step.js
+++ 1413-minimum-value-to-get-positive-step-by-step.js
@@ -1,4 +1,12 @@
-const minStartValue = function(nums) {
+// ❓ DESCRIPTION:
+// Given an array of integers nums, you start with an initial positive value startValue.
+//
+// In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+//
+// Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+//
+// ✅ SOLUTION (runtime 100%):
+const minStartValue = function (nums) {
     let start = 1;
     let sum = start;
     let i = 0
@@ -14,6 +22,7 @@ const minStartValue = function(nums) {
     return start;
 };
 
-console.log(minStartValue([-3,2,-3,4,2])) // 5
-console.log(minStartValue([1,2])) // 1
-console.log(minStartValue([1,-2,-3])) // 5
+// 📌 TESTCASE:
+console.log(minStartValue([-3 , 2 , -3 , 4 , 2])) // 5
+console.log(minStartValue([1 , 2])) // 1
+console.log(minStartValue([1 , -2 , -3])) // 5
