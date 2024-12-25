8af6999dfe7c339cb68bcd02fb1cc35181e210a1
Irene Volodko <ice7aye@gmail.com>
2024-12-25 22:14:30 +0300

    add solution

---
+++ 1408-string-matching-in-an-array.js
@@ -0,0 +1,18 @@
+const stringMatching = function (words) {
+    let result = [];
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if (words[j].match(words[i])) {
+                result.push(words[i]);
+            } else if (words[i].match(words[j])) {
+                result.push(words[j]);
+            }
+        }
+    }
+    return [...new Set(result)];
+};
+
+console.log(stringMatching(["mass", "as", "hero", "superhero"])); // ["as","hero"]
+console.log(stringMatching(["leetcode", "et", "code"])); // ["et","code"]
+console.log(stringMatching(["blue", "green", "bu"])); // []
+console.log(stringMatching(["leetcoder","leetcode","od","hamlet","am"])); // ["leetcode","od","am"]
