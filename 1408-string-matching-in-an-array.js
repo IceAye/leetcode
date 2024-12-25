e8fea2510346bff355c12d510a2e45444e221a08
Irene Volodko <ice7aye@gmail.com>
2024-12-25 22:18:48 +0300

    add solution 2

--- 1408-string-matching-in-an-array.js
+++ 1408-string-matching-in-an-array.js
@@ -12,6 +12,14 @@ const stringMatching = function (words) {
     return [...new Set(result)];
 };
 
+const stringMatching = function (words) {
+    let result = [];
+    for (const word of words) {
+        if (words.filter(el => el.includes(word)).length > 1) result.push(word);
+    }
+    return result;
+};
+
 console.log(stringMatching(["mass", "as", "hero", "superhero"])); // ["as","hero"]
 console.log(stringMatching(["leetcode", "et", "code"])); // ["et","code"]
 console.log(stringMatching(["blue", "green", "bu"])); // []
