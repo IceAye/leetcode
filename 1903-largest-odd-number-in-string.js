bb97f5a07580dbd25dbe9ed5651ae4d5fe4b16a1
Irene Volodko <ice7aye@gmail.com>
2024-12-26 04:33:47 +0300

    add description

--- 1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string.js
@@ -1,10 +1,17 @@
-const largestOddNumber = function(num) {
+// ❓ DESCRIPTION:
+// You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that
+// is a non-empty substring of num, or an empty string "" if no odd integer exists.
+// A substring is a contiguous sequence of characters within a string.
+//
+// ✅ SOLUTION:
+const largestOddNumber = function (num) {
     for (let i = num.length - 1; i >= 0; i--) {
-        if (+num[i] % 2 === 1) return num.slice(0, i + 1);
+        if (+num[i] % 2 === 1) return num.slice(0 , i + 1);
     }
     return "";
 };
 
+// 📌 TESTCASE:
 console.log(largestOddNumber("52")) // "5"
 console.log(largestOddNumber("4206")) // ""
 console.log(largestOddNumber("35427")) // "35427"
