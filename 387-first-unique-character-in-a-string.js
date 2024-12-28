bd3dc4f0b8e9f421d3c21e109efd1e168a8fe26f
Irene Volodko <ice7aye@gmail.com>
2024-12-28 06:28:27 +0300

    add description

--- 387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+//
+// ✅ SOLUTION:
 const firstUniqChar = function (s) {
     for (let i = 0; i < s.length; i++) {
         if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
@@ -5,6 +9,7 @@ const firstUniqChar = function (s) {
     return -1;
 };
 
+// 📌 TESTCASE:
 console.log(firstUniqChar("leetcode")); // 0
 console.log(firstUniqChar("loveleetcode")); // 2
 console.log(firstUniqChar("aabb")); // -1
