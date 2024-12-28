8d255de340dd17e30e663e1a40649551687c41b9
Irene Volodko <ice7aye@gmail.com>
2024-12-28 06:18:16 +0300

    add description

--- 383-ransom-note.js
+++ 383-ransom-note.js
@@ -1,28 +1,34 @@
-const canConstruct = function (ransomNote, magazine) {
+// ❓ DESCRIPTION:
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from
+// magazine and false otherwise.  Each letter in magazine can only be used once in ransomNote.
+//
+// ✅ SOLUTION 1:
+const canConstruct = function (ransomNote , magazine) {
     let map = new Map();
 
     for (const el of magazine) {
-        map.set(el, map.get(el) + 1 || 1);
+        map.set(el , map.get(el) + 1 || 1);
     }
 
     for (const el of ransomNote) {
         if (map.get(el) === undefined) return false;
-        map.set(el, map.get(el) - 1);
+        map.set(el , map.get(el) - 1);
         if (map.get(el) < 0) return false;
     }
 
     return true;
 };
 
-const canConstruct = function (ransomNote, magazine) {
+// ✅ SOLUTION 2:
+const canConstruct = function (ransomNote , magazine) {
     for (const char of magazine) {
-        ransomNote = ransomNote.replace(char, '');
+        ransomNote = ransomNote.replace(char , '');
     }
     return ransomNote.length === 0;
 };
 
-
-console.log(canConstruct("a", "b")); // false
-console.log(canConstruct("aa", "ab")); // false
-console.log(canConstruct("aa", "aab")); // true
-console.log(canConstruct("aab", "baa")); // true
+// 📌 TESTCASE:
+console.log(canConstruct("a" , "b")); // false
+console.log(canConstruct("aa" , "ab")); // false
+console.log(canConstruct("aa" , "aab")); // true
+console.log(canConstruct("aab" , "baa")); // true
