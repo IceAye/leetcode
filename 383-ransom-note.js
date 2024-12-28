84fc7ec72502a9cf20dae03fbd5825041fe94282
Irene Volodko <ice7aye@gmail.com>
2024-12-28 06:13:43 +0300

    add solution

---
+++ 383-ransom-note.js
@@ -0,0 +1,20 @@
+const canConstruct = function (ransomNote, magazine) {
+    let map = new Map();
+
+    for (const el of magazine) {
+        map.set(el, map.get(el) + 1 || 1);
+    }
+
+    for (const el of ransomNote) {
+        if (map.get(el) === undefined) return false;
+        map.set(el, map.get(el) - 1);
+        if (map.get(el) < 0) return false;
+    }
+
+    return true;
+};
+
+console.log(canConstruct("a", "b")); // false
+console.log(canConstruct("aa", "ab")); // false
+console.log(canConstruct("aa", "aab")); // true
+console.log(canConstruct("aab", "baa")); // true
