5ae0214ff504d8a6960387a5517b8e3fdd9c9dd6
Irene Volodko <ice7aye@gmail.com>
2024-12-28 06:27:28 +0300

    add solution

---
+++ 387-first-unique-character-in-a-string.js
@@ -0,0 +1,10 @@
+const firstUniqChar = function (s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+    }
+    return -1;
+};
+
+console.log(firstUniqChar("leetcode")); // 0
+console.log(firstUniqChar("loveleetcode")); // 2
+console.log(firstUniqChar("aabb")); // -1
