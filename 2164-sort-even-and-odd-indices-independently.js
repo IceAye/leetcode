1b745794b3a6f35f7404584607fabc6be200be0c
Irene Volodko <ice7aye@gmail.com>
2024-12-30 22:46:00 +0300

    add description

--- 2164-sort-even-and-odd-indices-independently.js
+++ 2164-sort-even-and-odd-indices-independently.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array nums. Rearrange the values of nums according to the following rules:
+//
+// Sort the values at odd indices of nums in non-increasing order.
+// For example, if nums = [4,1,2,3] before this step, it becomes [4,3,2,1] after. The values at odd indices 1 and 3 are sorted in non-increasing order.
+// Sort the values at even indices of nums in non-decreasing order.
+// For example, if nums = [4,1,2,3] before this step, it becomes [2,1,4,3] after. The values at even indices 0 and 2 are sorted in non-decreasing order.
+// Return the array formed after rearranging the values of nums.
+//
+// ✅ SOLUTION:
 const sortEvenOdd = function (nums) {
     let odds = [];
     let evens = [];
@@ -14,6 +24,7 @@ const sortEvenOdd = function (nums) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(sortEvenOdd([4, 1, 2, 3])); // [2,3,4,1]
 console.log(sortEvenOdd([2, 1])); // [2,1]
 console.log(sortEvenOdd([4, 1, 2, 3, 10])); // [2,3,4,1,10]
