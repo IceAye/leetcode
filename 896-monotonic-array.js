936ec2706079904090c81c7bb0f193f70df688cb
Irene Volodko <ice7aye@gmail.com>
2025-01-08 20:41:35 +0300

    add description

--- 896-monotonic-array.js
+++ 896-monotonic-array.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// An array is monotonic if it is either monotone increasing or monotone decreasing.
+// An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if
+// for all i <= j, nums[i] >= nums[j].  Given an integer array nums, return true if the given array is monotonic, or
+// false otherwise.
+//
+// ✅ SOLUTION (95% runtime):
 const isMonotonic = function (nums) {
     let start = nums.find((num) => nums[0] !== num);
     if (start === undefined) return true;
@@ -13,7 +20,8 @@ const isMonotonic = function (nums) {
     return true;
 };
 
-console.log(isMonotonic([1, 2, 2, 3])); // true
-console.log(isMonotonic([6, 5, 4, 4])); // true
-console.log(isMonotonic([1, 3, 2])); // false
-console.log(isMonotonic([1, 1, 1])); // true
+// 📌 TESTCASE:
+console.log(isMonotonic([1 , 2 , 2 , 3])); // true
+console.log(isMonotonic([6 , 5 , 4 , 4])); // true
+console.log(isMonotonic([1 , 3 , 2])); // false
+console.log(isMonotonic([1 , 1 , 1])); // true
