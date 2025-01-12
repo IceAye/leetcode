f5c74cd63249fcd88f64f1054f22fbbdb4ed4592
Irene Volodko <ice7aye@gmail.com>
2025-01-12 03:33:30 +0300

    add description

--- max-consecutive-ones.js
+++ max-consecutive-ones.js
@@ -1,7 +1,12 @@
+// ❓ DESCRIPTION:
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+//
+// ✅ SOLUTION:
 const findMaxConsecutiveOnes = function(nums) {
     return Math.max(...nums.join("").split("0").map(num => num.length));
 };
 
+// 📌 TESTCASE:
 console.log(findMaxConsecutiveOnes([1,1,0,1,1,1])) // 3
 console.log(findMaxConsecutiveOnes([1,0,1,1,0,1])) // 2
 console.log(findMaxConsecutiveOnes([1,0,1,1,0,0,0,1])) // 2
