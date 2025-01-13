5c6b4ca05f4927bd8084b083642e0c853500126f
Irene Volodko <ice7aye@gmail.com>
2025-01-13 15:18:15 +0300

    add description

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an integer array nums of positive integers, return the average value of all even integers that are divisible by 3.
+// Note that the average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+//
+// ✅ SOLUTION 1:
 const averageValue = function (nums) {
     let array = [];
     for (const num of nums) {
@@ -13,6 +18,7 @@ const averageValue = function (nums) {
     return 0;
 };
 
+// ✅ SOLUTION 2:
 const averageValue = function (nums) {
     let sum = 0;
     let count = 0;
@@ -25,6 +31,7 @@ const averageValue = function (nums) {
     return sum ? Math.floor(sum / count) : 0;
 };
 
+// 📌 TESTCASE:
 console.log(averageValue([1, 3, 6, 10, 12, 15])); // 9
 console.log(averageValue([1, 2, 4, 7, 10])); // 0
 console.log(averageValue([43,9,75,76,25,96,46,85,19,29,88,2,5,24,60,26,76,24,96,82,97,97,72,35,21,77,82,30,94,55,76,94,51])); // 57
