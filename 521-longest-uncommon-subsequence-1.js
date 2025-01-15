a5764c3f0c62b38ee3871553a84d0226b2fc3bea
Irene Volodko <ice7aye@gmail.com>
2025-01-15 13:03:23 +0300

    add description

--- 521-longest-uncommon-subsequence-1.js
+++ 521-longest-uncommon-subsequence-1.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If no such uncommon subsequence exists, return -1.
+// An uncommon subsequence between two strings is a string that is a
+// subsequence of exactly one of them.
+//
+// ✅ SOLUTION (100% runtime):
 const findLUSlength = function(a, b) {
     return a === b ? - 1 : Math.max(a.length, b.length);
 };
 
+// 📌 TESTCASE:
 console.log(findLUSlength("aba","cdc")) // 3
 console.log(findLUSlength("aaa","bbb")) // 3
 console.log(findLUSlength("aaa","aaa")) // -1
