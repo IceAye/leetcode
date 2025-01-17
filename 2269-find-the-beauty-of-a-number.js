2139c059b4c8630ea0a219731c1d2808d2d174eb
Irene Volodko <ice7aye@gmail.com>
2025-01-17 06:37:35 +0300

    add description

--- 2269-find-the-beauty-of-a-number.js
+++ 2269-find-the-beauty-of-a-number.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// The k-beauty of an integer num is defined as the number of substrings of num when it is read as a string that meet the following conditions:
+// It has a length of k.
+// It is a divisor of num.
+// Given integers num and k, return the k-beauty of num.
+//
+// Note:
+// Leading zeros are allowed.
+// 0 is not a divisor of any value.
+// A substring is a contiguous sequence of characters in a string.
+//
+// ✅ SOLUTION (100% runtime):
 const divisorSubstrings = function(num, k) {
     let str = num.toString();
     let result = 0;
@@ -11,6 +23,7 @@ const divisorSubstrings = function(num, k) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(divisorSubstrings(240, 2)) // 2
 console.log(divisorSubstrings(430043, 2)) // 2
 console.log(divisorSubstrings(30003, 3)) // 1
