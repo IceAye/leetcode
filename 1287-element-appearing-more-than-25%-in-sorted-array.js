53df147481e91789e49d38ecfcd7ab1cec5eb996
Irene Volodko <ice7aye@gmail.com>
2025-01-18 12:01:05 +0300

    add solution 2

--- 1287-element-appearing-more-than-25%-in-sorted-array.js
+++ 1287-element-appearing-more-than-25%-in-sorted-array.js
@@ -8,5 +8,12 @@ const findSpecialInteger = function (arr) {
     }
 };
 
+const findSpecialInteger = function (arr) {
+    let sliding = Math.floor(0.25 * arr.length);
+    for (let i = 0; i < arr.length - sliding; i++) {
+        if (arr[i] === arr[i + sliding]) return arr[i];
+    }
+};
+
 console.log(findSpecialInteger([1,2,2,6,6,6,6,7,10])) // 6
 console.log(findSpecialInteger([1, 1])) // 1
