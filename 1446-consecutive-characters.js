e1a3bd7994f43b88bf0507db10a90845f5797e90
Irene Volodko <ice7aye@gmail.com>
2025-01-22 21:44:32 +0300

    add description

--- 1446-consecutive-characters.js
+++ 1446-consecutive-characters.js
@@ -2,7 +2,7 @@
 // The power of the string is the maximum length of a non-empty substring that contains only one unique character.
 // Given a string s, return the power of s.
 //
-// ✅ SOLUTION:
+// ✅ SOLUTION (100% runtime):
 const maxPower = function (s) {
     let count = 1;
 
