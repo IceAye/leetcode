e28d2b5592c55cc22edd71d6fc3bae3700d2e9e4
Irene Volodko <ice7aye@gmail.com>
2025-01-27 21:57:20 +0300

    add description

--- 2190-most-frequent-number-following-key-in-an-array.js
+++ 2190-most-frequent-number-following-key-in-an-array.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array nums. You are also given an integer key, which is present in nums.
+//
+// For every unique integer target in nums, count the number of times target immediately follows an occurrence of key in nums. In other words, count the number of indices i such that:
+//
+// 0 <= i <= nums.length - 2,
+// nums[i] == key and,
+// nums[i + 1] == target.
+// Return the target with the maximum count. The test cases will be generated such that the target with maximum count is unique.
+//
+// ✅ SOLUTION:
 const mostFrequent = function(nums, key) {
     const map = new Map();
     for (let i = 0; i < nums.length - 1; i++) {
@@ -8,5 +19,6 @@ const mostFrequent = function(nums, key) {
     return [...map.keys()].filter(key => map.get(key) === Math.max(...[...map.values()]))[0]
 };
 
+// 📌 TESTCASE:
 console.log(mostFrequent([1,100,200,1,100], 1)) // 100
 console.log(mostFrequent([2,2,2,2,3], 2)) // 2
