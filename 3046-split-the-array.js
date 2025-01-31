62a9a7a723504aecfb76ee83ba622127359cc642
Irene Volodko <ice7aye@gmail.com>
2025-01-31 12:51:57 +0300

    add description

--- 3046-split-the-array.js
+++ 3046-split-the-array.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums of even length. You have to split the array into two parts nums1 and nums2 such that:
+//
+// nums1.length == nums2.length == nums.length / 2.
+// nums1 should contain distinct elements.
+// nums2 should also contain distinct elements.
+// Return true if it is possible to split the array, and false otherwise.
+//
+// ✅ SOLUTION:
 const isPossibleToSplit = function(nums) {
     const count = new Map ();
     for (const num of nums) {
@@ -7,5 +16,6 @@ const isPossibleToSplit = function(nums) {
     return true;
 };
 
+// 📌 TESTCASE:
 console.log(isPossibleToSplit([1,1,2,2,3,4])) // true
 console.log(isPossibleToSplit([1,1,1,1])) // false
