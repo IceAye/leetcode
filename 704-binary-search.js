ab3729547904db02f761f63bd8313dd120b7a45d
Irene Volodko <ice7aye@gmail.com>
2025-02-01 14:51:07 +0300

    add solution 2

--- 704-binary-search.js
+++ 704-binary-search.js
@@ -1,3 +1,20 @@
 const search = function(nums, target) {
+    let start= 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        let middleElement = Math.floor((start + end) / 2);
+        if (target === nums[middleElement]) {
+            return middleElement;
+        } else if (target < nums[middleElement]) {
+            end = middleElement - 1;
+        } else if (target > nums[middleElement]) {
+            start = middleElement + 1;
+        }
+    }
+    return -1;
+};
+
+const search = function(nums, target) {
     return nums.indexOf(target)
 };
