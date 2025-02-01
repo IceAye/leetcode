98f994383ebc30096881731e1a4a651eee34e7c5
Irene Volodko <ice7aye@gmail.com>
2025-02-01 14:52:22 +0300

    add description

--- 704-binary-search.js
+++ 704-binary-search.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+//
+// ✅ SOLUTION 1 (100% runtime):
 const search = function(nums, target) {
     let start= 0;
     let end = nums.length - 1;
@@ -15,6 +20,11 @@ const search = function(nums, target) {
     return -1;
 };
 
+// ✅ SOLUTION 2 (100% runtime):
 const search = function(nums, target) {
     return nums.indexOf(target)
 };
+
+// 📌 TESTCASE:
+console.log(search([-1,0,3,5,9,12], 9)) // 4
+console.log(search([-1,0,3,5,9,12], 2)) // -1
