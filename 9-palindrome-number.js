84a91df1edc756ef770f03bfd5eed8dd7c5aff3f
Irene Volodko <ice7aye@gmail.com>
2025-02-06 16:00:31 +0300

    add description

--- 9-palindrome-number.js
+++ 9-palindrome-number.js
@@ -1,8 +1,13 @@
+// ❓ DESCRIPTION:
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+//
+// ✅ SOLUTION:
 const isPalindrome = function (x) {
     if (x < 0) return false;
     return +Array.from(x.toString()).reverse().join("") === x;
 };
 
+// 📌 TESTCASE:
 console.log(isPalindrome(121)); // true
 console.log(isPalindrome(-121)); // false
 console.log(isPalindrome(10)); // false
