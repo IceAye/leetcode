4676b886d7d399eefffbfc0bf3f6456f176c781e
Irene Volodko <ice7aye@gmail.com>
2025-02-07 16:35:46 +0300

    add description

--- 2465-number-of-distinct-averages.js
+++ 2465-number-of-distinct-averages.js
@@ -1,3 +1,19 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array nums of even length.
+//
+// As long as nums is not empty, you must repetitively:
+//
+// Find the minimum number in nums and remove it.
+// Find the maximum number in nums and remove it.
+// Calculate the average of the two removed numbers.
+// The average of two numbers a and b is (a + b) / 2.
+//
+// For example, the average of 2 and 3 is (2 + 3) / 2 = 2.5.
+// Return the number of distinct averages calculated using the above process.
+//
+// Note that when there is a tie for a minimum or maximum number, any can be removed
+//
+// ✅ SOLUTION:
 const distinctAverages = function(nums) {
     let averages = new Set();
     nums.sort((a, b) => a - b);
@@ -8,5 +24,6 @@ const distinctAverages = function(nums) {
     return averages.size;
 };
 
+// 📌 TESTCASE:
 console.log(distinctAverages([4,1,4,0,3,5])) // 2
 console.log(distinctAverages([1,100])) // 1
