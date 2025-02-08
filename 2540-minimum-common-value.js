04ac645ac3aada80d27415787e339e948a8a5b4e
Irene Volodko <ice7aye@gmail.com>
2025-02-08 06:17:43 +0300

    add description

--- 2540-minimum-common-value.js
+++ 2540-minimum-common-value.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+// Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+//
+// ✅ SOLUTION:
 const getCommon = function (nums1, nums2) {
     let i = 0;
     let j = 0;
@@ -14,5 +19,6 @@ const getCommon = function (nums1, nums2) {
     return -1;
 };
 
+// 📌 TESTCASE:
 console.log(getCommon([1, 2, 3], [2, 4])); // 2
 console.log(getCommon([1, 2, 3, 6], [2, 3, 4, 5])); // 2
