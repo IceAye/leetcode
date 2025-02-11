d7ff375ccfed5069cc541172017c52048d15f821
Irene Volodko <ice7aye@gmail.com>
2025-02-11 19:47:09 +0300

    add description

--- 1910-remove-all-occurences-of-a-substring.js
+++ 1910-remove-all-occurences-of-a-substring.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+// Find the leftmost occurrence of the substring part and remove it from s.
+// Return s after removing all occurrences of part.
+// A substring is a contiguous sequence of characters in a string.
+//
+// ✅ SOLUTION:
 const removeOccurrences = function (s, part) {
     while (new RegExp(`${part}`).test(s)) {
         s = s.replace(new RegExp(`${part}`), "");
@@ -5,5 +12,6 @@ const removeOccurrences = function (s, part) {
     return s;
 };
 
+// 📌 TESTCASE:
 console.log(removeOccurrences("daabcbaabcbc", "abc")) // "dab"
 console.log(removeOccurrences("axxxxyyyyb", "xy")) // "ab"
