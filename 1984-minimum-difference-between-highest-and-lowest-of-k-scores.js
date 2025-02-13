50d37af53d698731253d8df8c6e83a26261efd23
Irene Volodko <ice7aye@gmail.com>
2025-02-13 21:12:37 +0300

    add description

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
+// Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+// Return the minimum possible difference.
+//
+// ✅ SOLUTION:
 const minimumDifference = function (nums, k) {
     nums.sort((a, b) => a - b);
     let result = [];
@@ -8,5 +14,6 @@ const minimumDifference = function (nums, k) {
     return Math.min(...result);
 };
 
+// 📌 TESTCASE:
 console.log(minimumDifference([90], 1)); // 0
 console.log(minimumDifference([9, 4, 1, 7], 2)); // 2
