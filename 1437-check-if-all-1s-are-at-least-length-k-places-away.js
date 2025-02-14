f946bfa7257d21f5813cf57cd496e9f9260f464a
Irene Volodko <ice7aye@gmail.com>
2025-02-14 20:51:10 +0300

    add description

--- 1437-check-if-all-1s-are-at-least-length-k-places-away.js
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+//
+// ✅ SOLUTION (100% runtime):
 const kLengthApart = function (nums, k) {
     let prevIndex = -1;
     for (let i = 0; i < nums.length; i++) {
@@ -11,6 +15,7 @@ const kLengthApart = function (nums, k) {
     return true;
 };
 
+// 📌 TESTCASE:
 console.log(kLengthApart([1, 0, 0, 0, 1, 0, 0, 1], 2)); // true
 console.log(kLengthApart([1, 0, 0, 1, 0, 1], 2)); // false
 console.log(kLengthApart([1, 1, 1, 1, 1], 0)); // true
