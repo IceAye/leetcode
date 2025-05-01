afb96c0482363a6037032f0983494e749d33531a
Irene Volodko <ice7aye@gmail.com>
2025-05-01 18:51:38 +0300

    add solution 2: transform array by parity

--- 3467-transform-array-by-parity.js
+++ 3467-transform-array-by-parity.js
@@ -2,5 +2,15 @@ const transformArray = function(nums) {
     return nums.map(item => item % 2 === 0 ? 0 : 1).sort();
 };
 
+const transformArray = function(nums) {
+    let zeros = [];
+    let ones = [];
+
+    for (const num of nums) {
+        (num % 2 === 0) ? zeros.push(0) : ones.push(1)
+    }
+    return [...zeros, ...ones];
+};
+
 console.log(transformArray([4,3,2,1])) // [0,0,1,1]
 console.log(transformArray([1,5,1,4,2])) // [0,0,1,1,1]
