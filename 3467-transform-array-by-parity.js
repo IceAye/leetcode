e4f8f290662c676cad27611bf2f1d7331d8f461e
Irene Volodko <ice7aye@gmail.com>
2025-05-01 18:58:24 +0300

    description: transform array by parity

--- 3467-transform-array-by-parity.js
+++ 3467-transform-array-by-parity.js
@@ -1,7 +1,16 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums. Transform nums by performing the following operations in the exact order specified:
+// Replace each even number with 0.
+// Replace each odd numbers with 1.
+// Sort the modified array in non-decreasing order.
+// Return the resulting array after performing these operations.
+//
+// ✅ SOLUTION 1 (71.17% runtime, 62.80% memory):
 const transformArray = function(nums) {
     return nums.map(item => item % 2 === 0 ? 0 : 1).sort();
 };
 
+// ✅ SOLUTION 2 (38.13% runtime, 18.03% memory):
 const transformArray = function(nums) {
     let zeros = [];
     let ones = [];
@@ -12,6 +21,7 @@ const transformArray = function(nums) {
     return [...zeros, ...ones];
 };
 
+// ✅ SOLUTION 3 (7.22% runtime, 51.93% memory):
 const transformArray = function(nums) {
     let countZeros = 0;
 
@@ -21,5 +31,6 @@ const transformArray = function(nums) {
     return new Array(countZeros).fill(0).concat(new Array(nums.length - countZeros).fill(1));
 };
 
+// 📌 TESTCASE:
 console.log(transformArray([4,3,2,1])) // [0,0,1,1]
 console.log(transformArray([1,5,1,4,2])) // [0,0,1,1,1]
