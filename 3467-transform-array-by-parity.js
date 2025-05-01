384101fcfed44c92a402da6aa265003de1e29358
Irene Volodko <ice7aye@gmail.com>
2025-05-01 18:54:29 +0300

    add solution 3: transform array by parity

--- 3467-transform-array-by-parity.js
+++ 3467-transform-array-by-parity.js
@@ -12,5 +12,14 @@ const transformArray = function(nums) {
     return [...zeros, ...ones];
 };
 
+const transformArray = function(nums) {
+    let countZeros = 0;
+
+    for (const num of nums) {
+        if (num % 2 === 0) countZeros++;
+    }
+    return new Array(countZeros).fill(0).concat(new Array(nums.length - countZeros).fill(1));
+};
+
 console.log(transformArray([4,3,2,1])) // [0,0,1,1]
 console.log(transformArray([1,5,1,4,2])) // [0,0,1,1,1]
