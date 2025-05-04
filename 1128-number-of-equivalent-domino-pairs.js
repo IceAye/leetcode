75cf3ca419a2bf97f00067d6793a982599b624ed
Irene Volodko <ice7aye@gmail.com>
2025-05-04 14:50:58 +0300

    description: number of equivalent domino pairs

--- 1128-number-of-equivalent-domino-pairs.js
+++ 1128-number-of-equivalent-domino-pairs.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+// Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+// 📅 (daily question 2025 May, 4)
+//
+// ✅ SOLUTION:
 const numEquivDominoPairs = function(dominoes) {
     let map = new Map();
 
@@ -10,5 +16,6 @@ const numEquivDominoPairs = function(dominoes) {
     return [...map.values()].reduce((acc, curr) => acc + (curr * (curr - 1)) / 2, 0);
 };
 
+// 📌 TESTCASE:
 console.log(numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6]])) // 1
 console.log(numEquivDominoPairs([[1,2],[1,2],[1,1],[1,2],[2,2]])) // 3
