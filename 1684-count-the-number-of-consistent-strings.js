a6f19f02d1ef833d13b9517d0b2d329316d3eb1b
Irene Volodko <ice7aye@gmail.com>
2025-05-05 17:11:41 +0300

    description: count the number of consistent strings

--- 1684-count-the-number-of-consistent-strings.js
+++ 1684-count-the-number-of-consistent-strings.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+// Return the number of consistent strings in the array words.
+//
+// ✅ SOLUTION:
+
 const countConsistentStrings = function(allowed, words) {
     const allowedSet = new Set(allowed);
 
@@ -11,6 +17,7 @@ const countConsistentStrings = function(allowed, words) {
     }, 0);
 };
 
+// 📌 TESTCASE:
 console.log(countConsistentStrings("ab",["ad","bd","aaab","baa","badab"])) // 2
 console.log(countConsistentStrings("abc",["a","b","c","ab","ac","bc","abc"])) // 7
 console.log(countConsistentStrings("cad",["cc","acd","b","ba","bac","bad","ac","d"])) // 4
