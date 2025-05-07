7eba64f0c8aeaa59732b1f2f52cc0006883988bb
Irene Volodko <ice7aye@gmail.com>
2025-05-07 16:50:06 +0300

    add solution: the two sneaky numbers of Digitville - map

---
+++ 3289-the-two-sneaky-numbers-of-digitville.js
@@ -0,0 +1,11 @@
+const getSneakyNumbers = function (nums) {
+    let map = new Map();
+    for (const num of nums) {
+        map.set(num, map.get(num) + 1 || 1);
+    }
+    return [...map.keys()].filter((number) => map.get(number) >= 2);
+};
+
+console.log(getSneakyNumbers([0,1,1,0])) // [0,1]
+console.log(getSneakyNumbers([0,3,2,1,3,2])) // [2,3]
+console.log(getSneakyNumbers([7,1,5,4,3,4,6,0,9,5,8,2])) // [4,5]
