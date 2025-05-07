3188dd632f16ad8041a32af5af3da02f7815613f
Irene Volodko <ice7aye@gmail.com>
2025-05-07 16:53:39 +0300

    add solution 2: the two sneaky numbers of Digitville - set

--- 3289-the-two-sneaky-numbers-of-digitville.js
+++ 3289-the-two-sneaky-numbers-of-digitville.js
@@ -6,6 +6,15 @@ const getSneakyNumbers = function (nums) {
     return [...map.keys()].filter((number) => map.get(number) >= 2);
 };
 
+const getSneakyNumbers = function (nums) {
+    let set = new Set();
+    let result = [];
+    for (const num of nums) {
+        (set.has(num)) ? result.push(num) : set.add(num);
+    }
+    return result;
+};
+
 console.log(getSneakyNumbers([0,1,1,0])) // [0,1]
 console.log(getSneakyNumbers([0,3,2,1,3,2])) // [2,3]
 console.log(getSneakyNumbers([7,1,5,4,3,4,6,0,9,5,8,2])) // [4,5]
