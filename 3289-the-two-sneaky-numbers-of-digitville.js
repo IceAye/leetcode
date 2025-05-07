c798e3656ad95148b18e07583464e3aa07784562
Irene Volodko <ice7aye@gmail.com>
2025-05-07 16:55:13 +0300

    description: the two sneaky numbers of Digitville - set

--- 3289-the-two-sneaky-numbers-of-digitville.js
+++ 3289-the-two-sneaky-numbers-of-digitville.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+//
+// As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+//
+// ✅ SOLUTION 1:
 const getSneakyNumbers = function (nums) {
     let map = new Map();
     for (const num of nums) {
@@ -6,6 +12,7 @@ const getSneakyNumbers = function (nums) {
     return [...map.keys()].filter((number) => map.get(number) >= 2);
 };
 
+// ✅ SOLUTION 2:
 const getSneakyNumbers = function (nums) {
     let set = new Set();
     let result = [];
@@ -15,6 +22,7 @@ const getSneakyNumbers = function (nums) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(getSneakyNumbers([0,1,1,0])) // [0,1]
 console.log(getSneakyNumbers([0,3,2,1,3,2])) // [2,3]
 console.log(getSneakyNumbers([7,1,5,4,3,4,6,0,9,5,8,2])) // [4,5]
