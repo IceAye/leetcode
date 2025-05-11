fc6ffbe2732c25771e8ea206d9dbbe48e3bca1a7
Irene Volodko <ice7aye@gmail.com>
2025-05-11 12:28:04 +0300

    description: minimum equal sum of two arrays after replacing zeros

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeroes.js
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeroes.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given two arrays nums1 and nums2 consisting of positive integers.
+// You have to replace all the 0's in both arrays with strictly positive integers such that the sum of elements of both arrays becomes equal.
+// Return the minimum equal sum you can obtain, or -1 if it is impossible.
+// 📅 (daily question 2025, May 10th)
+//
+// ✅ SOLUTION:
 const minSum = function(nums1, nums2) {
 
     function mapped (array) {
@@ -18,6 +25,7 @@ const minSum = function(nums1, nums2) {
     return (counts1.total > counts2.total) ? counts1.total : counts2.total;
 };
 
+// 📌 TESTCASE:
 console.log(minSum([3,2,0,1,0], [6,5,0])) // 12
 console.log(minSum([2,0,2,0], [1,4])) // -1
 console.log(minSum([8,13,15,18,0,18,0,0,5,20,12,27,3,14,22,0], [29,1,6,0,10,24,27,17,14,13,2,19,2,11])) // 179
