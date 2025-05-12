c31c32f35b03dced64a9e3312b0d667a7b7ee6e6
Irene Volodko <ice7aye@gmail.com>
2025-05-12 16:18:29 +0300

    description: finding 3-digit even numbers

--- 2094-finding-3-digit-even-numbers.js
+++ 2094-finding-3-digit-even-numbers.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// You are given an integer array digits, where each element is a digit. The array may contain duplicates.
+//
+// You need to find all the unique integers that follow the given requirements:
+//
+// The integer consists of the concatenation of three elements from digits in any arbitrary order.
+// The integer does not have leading zeros.
+// The integer is even.
+// For example, if the given digits were [1, 2, 3], integers 132 and 312 follow the requirements.
+//
+// Return a sorted array of the unique integers.
+// 📅 (daily question 2025, May 12th):
+//
+// ✅ SOLUTION (81.03% runtime, 31.09% memory):
 const findEvenNumbers = function(digits) {
     let result = [];
 
@@ -32,6 +46,7 @@ const findEvenNumbers = function(digits) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(findEvenNumbers([2,1,3,0])) // [102,120,130,132,210,230,302,310,312,320]
 console.log(findEvenNumbers([2,2,8,8,2])) // [222,228,282,288,822,828,882]
 console.log(findEvenNumbers([3,7,5])) // []
