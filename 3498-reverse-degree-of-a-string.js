cd4dd04dc8bf6a51ec352ec640104690bd79afde
Irene Volodko <ice7aye@gmail.com>
2025-05-13 15:20:40 +0300

    description: reverse degree of a string

--- 3498-reverse-degree-of-a-string.js
+++ 3498-reverse-degree-of-a-string.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// Given a string s, calculate its reverse degree.
+//
+// The reverse degree is calculated as follows:
+//
+// For each character, multiply its position in the reversed alphabet ('a' = 26, 'b' = 25, ..., 'z' = 1) with its position in the string (1-indexed).
+// Sum these products for all characters in the string.
+// Return the reverse degree of s.
+//
+// ✅ SOLUTION:
 const reverseDegree = function(s) {
     return [...s].reduce((acc, curr, ind) => {
         acc += (curr.charCodeAt(0) - (71 + 2 * (curr.charCodeAt(0) - 97))) * (ind + 1);
@@ -5,5 +15,6 @@ const reverseDegree = function(s) {
     }, 0)
 };
 
+// 📌 TESTCASE:
 console.log(reverseDegree("abc")) // 148
 console.log(reverseDegree("zaza")) // 160
