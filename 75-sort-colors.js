88db13482402e671cfac318407361096882f16cd
Irene Volodko <ice7aye@gmail.com>
2025-05-17 22:49:05 +0300

    add solution: sort colors

---
+++ 75-sort-colors.js
@@ -0,0 +1,13 @@
+const sortColors = function (nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = 0; j < nums.length - 1 - i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+            }
+        }
+    }
+    return nums;
+};
+
+console.log(sortColors([2, 0, 2, 1, 1, 0])); // [0,0,1,1,2,2]
+console.log(sortColors([2, 0, 1])); // [0,1,2]
