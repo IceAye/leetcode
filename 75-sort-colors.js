2e43cdd0d66cd09e507122f948758d4e3b81c54f
Irene Volodko <ice7aye@gmail.com>
2025-05-17 22:49:57 +0300

    description: sort colors

--- 75-sort-colors.js
+++ 75-sort-colors.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+// You must solve this problem without using the library's sort function.
+//
+// ✅ SOLUTION:
 const sortColors = function (nums) {
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = 0; j < nums.length - 1 - i; j++) {
@@ -9,5 +15,6 @@ const sortColors = function (nums) {
     return nums;
 };
 
+// 📌 TESTCASE:
 console.log(sortColors([2, 0, 2, 1, 1, 0])); // [0,0,1,1,2,2]
 console.log(sortColors([2, 0, 1])); // [0,1,2]
