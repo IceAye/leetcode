e966c84aa1c1095a0ab86e8cd0028e8f4e2b54ad
Irene Volodko <ice7aye@gmail.com>
2025-05-20 16:08:16 +0300

    description: find indices of stable mountains

--- 3285-find-indices-of-stable-mountains.js
+++ 3285-find-indices-of-stable-mountains.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// There are n mountains in a row, and each mountain has a height. You are given an integer array height where height[i] represents the height of mountain i, and an integer threshold.
+//
+// A mountain is called stable if the mountain just before it (if it exists) has a height strictly greater than threshold. Note that mountain 0 is not stable.
+//
+// Return an array containing the indices of all stable mountains in any order.
+//
+// ✅ SOLUTION (100% runtime, 93.70% memory):
 const stableMountains = function(height, threshold) {
     let result = [];
     for (let i = 1; i < height.length; i++) {
@@ -6,6 +14,7 @@ const stableMountains = function(height, threshold) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(stableMountains([1,2,3,4,5], 2)) // [3,4]
 console.log(stableMountains([10,1,10,1,10], 3)) // [1,3]
 console.log(stableMountains([10,1,10,1,10], 10)) // []
