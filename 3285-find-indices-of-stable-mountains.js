e2a2533ba30cc6b9736be60553f6e2cce41aea20
Irene Volodko <ice7aye@gmail.com>
2025-05-20 16:06:54 +0300

    add solution: find indices of stable mountains

---
+++ 3285-find-indices-of-stable-mountains.js
@@ -0,0 +1,11 @@
+const stableMountains = function(height, threshold) {
+    let result = [];
+    for (let i = 1; i < height.length; i++) {
+        if (height[i - 1] > threshold) result.push(i);
+    }
+    return result;
+};
+
+console.log(stableMountains([1,2,3,4,5], 2)) // [3,4]
+console.log(stableMountains([10,1,10,1,10], 3)) // [1,3]
+console.log(stableMountains([10,1,10,1,10], 10)) // []
