4add9283660c89336d00d8c8e9952c35c733ceef
Irene Volodko <ice7aye@gmail.com>
2025-05-22 16:40:20 +0300

    description: sum of variable length subarrays

--- 3427-sum-of-variable-length-subarrays.js
+++ 3427-sum-of-variable-length-subarrays.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums of size n. For each index i where 0 <= i < n, define a subarray nums[start ... i] where start = max(0, i - nums[i]).
+//
+// Return the total sum of all elements from the subarray defined for each index in the array.
+//
+// ✅ SOLUTION:
 const subarraySum = function(nums) {
     let result = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -7,5 +13,6 @@ const subarraySum = function(nums) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(subarraySum([2,3,1])) // 11
 console.log(subarraySum([3,1,1,2])) // 13
