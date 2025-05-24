225c750ea2fa4e31bfbef79670b6664102b3948c
Irene Volodko <ice7aye@gmail.com>
2025-05-24 15:04:14 +0300

    description: remove outermost parentheses

--- 1021-remove-outermost-parentheses.js
+++ 1021-remove-outermost-parentheses.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+//
+// For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+// A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+//
+// Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+//
+// Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+//
+// ✅ SOLUTION (20.3% runtime, 96.31% memory):
 const removeOuterParentheses = function (s) {
     const parentheses = {
         '(': 1,
@@ -19,6 +30,7 @@ const removeOuterParentheses = function (s) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(removeOuterParentheses('(()())(())')) // '()()()'
 console.log(removeOuterParentheses('(()())(())(()(()))')) // '()()()()(())'
 console.log(removeOuterParentheses('()()')) // ''
