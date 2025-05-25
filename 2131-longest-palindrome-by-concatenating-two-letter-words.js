16a7529461931176666ca8088e7f9d6ec5d6c333
Irene Volodko <ice7aye@gmail.com>
2025-05-25 16:22:31 +0300

    description: longest palindrome by concatenating two letter words

--- 2131-longest-palindrome-by-concatenating-two-letter-words.js
+++ 2131-longest-palindrome-by-concatenating-two-letter-words.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given an array of strings words. Each element of words consists of two lowercase English letters.
+// Create the longest possible palindrome by selecting some elements from words and concatenating them in any order. Each element can be selected at most once.
+// Return the length of the longest palindrome that you can create. If it is impossible to create any palindrome, return 0.
+// A palindrome is a string that reads the same forward and backward.
+// (daily question 2025 May, 25th):
+//
+// ✅ SOLUTION:
 const longestPalindrome = function(words) {
     const hash = new Map();
     let result = 0;
@@ -33,6 +41,7 @@ const longestPalindrome = function(words) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(longestPalindrome(["lc","cl","gg"])) // 6
 console.log(longestPalindrome(["ab","ty","yt","lc","cl","ab"])) // 8
 console.log(longestPalindrome(["cc","ll","xx"])) // 2
