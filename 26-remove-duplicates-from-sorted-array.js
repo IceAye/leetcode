43bc1a8843b77e957b4443c1d624cb97d024e292
Irene Volodko <ice7aye@gmail.com>
2025-05-27 17:13:26 +0300

    description: remove duplicates from sorted array

--- 26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+//
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+//
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+//
+// ✅ SOLUTION 1 (100% runtime, 14.06% memory):
 const removeDuplicates = function (nums) {
     let l = 0, r = 1;
     while (r < nums.length) {
@@ -11,6 +20,7 @@ const removeDuplicates = function (nums) {
     return l || nums.length;
 };
 
+// ✅ SOLUTION 2:
 const removeDuplicates = function (nums) {
     let l = 1;
     for (let r = 1; r < nums.length; r++) {
@@ -22,6 +32,7 @@ const removeDuplicates = function (nums) {
     return l;
 };
 
+// ❓ TESTCASE:
 console.log(removeDuplicates([1,1,2])) // 2
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])) // 5
 console.log(removeDuplicates([1])) // 1
