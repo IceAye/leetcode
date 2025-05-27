595e255cb8e5548cf4cc77ea2672ea72fcd26405
Irene Volodko <ice7aye@gmail.com>
2025-05-27 17:10:44 +0300

    add solution 2: remove duplicates from sorted array

--- 26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array.js
@@ -11,6 +11,17 @@ const removeDuplicates = function (nums) {
     return l || nums.length;
 };
 
+const removeDuplicates = function (nums) {
+    let l = 1;
+    for (let r = 1; r < nums.length; r++) {
+        if (nums[l - 1] !== nums[r]) {
+            nums[l] = nums[r];
+            l++;
+        }
+    }
+    return l;
+};
+
 console.log(removeDuplicates([1,1,2])) // 2
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])) // 5
 console.log(removeDuplicates([1])) // 1
