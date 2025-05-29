a41104c520f650c4e3b48fc9a4c919365d158669
Irene Volodko <ice7aye@gmail.com>
2025-05-29 16:28:43 +0300

    description: find the index of the first occurence in a string

--- 28-find-the-index-of-the-first-occurence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurence-in-a-string.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//
+// ✅ SOLUTION (100% runtime, 54.26% memory):
 const strStr = function(haystack, needle) {
     return haystack.indexOf(needle);
 };
 
+// 📌 TESTCASE:
 console.log(strStr("sadbutsad", "sad")) // 0
 console.log(strStr("leetcode", "leeto")) // -1
