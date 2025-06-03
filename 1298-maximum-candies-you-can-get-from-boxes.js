a64774a4cb83c96e082641475b6365f171fa346d
Irene Volodko <ice7aye@gmail.com>
2025-06-03 16:50:54 +0300

    description: maximum candies you can get from boxes

--- 1298-maximum-candies-you-can-get-from-boxes.js
+++ 1298-maximum-candies-you-can-get-from-boxes.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// You have n boxes labeled from 0 to n - 1. You are given four arrays: status, candies, keys, and containedBoxes where:
+//
+// status[i] is 1 if the ith box is open and 0 if the ith box is closed,
+// candies[i] is the number of candies in the ith box,
+// keys[i] is a list of the labels of the boxes you can open after opening the ith box.
+// containedBoxes[i] is a list of the boxes you found inside the ith box.
+// You are given an integer array initialBoxes that contains the labels of the boxes you initially have. You can take all the candies in any open box and you can use the keys in it to open new boxes and you also can use the boxes you find in it.
+//
+// Return the maximum number of candies you can get following the rules above.
+// 📅 (daily question 2025, June, 3rd)
+//
+// ✅ SOLUTION:
 const maxCandies = function(status, candies, keys, containedBoxes, initialBoxes) {
     let totalCandies = 0;
     const ownedBoxes = new Set(initialBoxes);
@@ -37,5 +50,6 @@ const maxCandies = function(status, candies, keys, containedBoxes, initialBoxes)
     return totalCandies;
 };
 
+// 📌 TESTCASE:
 console.log(maxCandies(  [1,0,1,0], [7,5,4,100],  [[],[],[1],[]], [[1,2],[3],[],[]], [0])) // 16
 console.log(maxCandies( [1,0,0,0,0,0], [1,1,1,1,1,1], [[1,2,3,4,5],[],[],[],[],[]], [[1,2,3,4,5],[],[],[],[],[]], [0])) // 6
