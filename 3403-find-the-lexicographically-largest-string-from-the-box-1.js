992ea3afd45273503888f2608ca76073262ba892
Irene Volodko <ice7aye@gmail.com>
2025-06-04 15:54:27 +0300

    description: find the lexicographically largest string from the box 1

--- 3403-find-the-lexicographically-largest-string-from-the-box-1.js
+++ 3403-find-the-lexicographically-largest-string-from-the-box-1.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given a string word, and an integer numFriends.
+//
+// Alice is organizing a game for her numFriends friends. There are multiple rounds in the game, where in each round:
+//
+// word is split into numFriends non-empty strings, such that no previous round has had the exact same split.
+// All the split words are put into a box.
+// Find the lexicographically largest string from the box after all the rounds are finished.
+// 📅 (daily question 2025, June, 4th):
+//
+// ✅ SOLUTION:
 const answerString = function(word, numFriends) {
     if (numFriends === 1) return word;
 
@@ -16,6 +27,7 @@ const answerString = function(word, numFriends) {
     return max.sort().at(-1);
 };
 
+// 📌 TESTCASE:
 console.log(answerString( "dbca",  2)) // "dbc"
 console.log(answerString("gggg",  4)) // "g"
 console.log(answerString("aann", 2)) // "nn"
