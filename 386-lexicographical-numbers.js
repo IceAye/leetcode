8e11035919a8eafeb1f0be3158e5630a0d47f9a4
Irene Volodko <ice7aye@gmail.com>
2025-06-08 20:31:42 +0300

    description: lexicographical numbers

--- 386-lexicographical-numbers.js
+++ 386-lexicographical-numbers.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+// You must write an algorithm that runs in O(n) time and uses O(1) extra space.
+//
+// ✅ SOLUTION:
 const lexicalOrder = function(n) {
     let array = [];
     for (let i = 1; i <= n; i++) {
@@ -6,5 +11,6 @@ const lexicalOrder = function(n) {
     return array.sort();
 };
 
+// 📌 TESTCASE:
 console.log(lexicalOrder(13)) // [1,10,11,12,13,2,3,4,5,6,7,8,9]
 console.log(lexicalOrder(2)) // [1,2]
