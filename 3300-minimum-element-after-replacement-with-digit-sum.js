0b650124b1071dca0732f206820932173f964ade
Irene Volodko <ice7aye@gmail.com>
2025-06-13 13:08:04 +0300

    description: minimum element after replacement with digit sum

--- 3300-minimum-element-after-replacement-with-digit-sum.js
+++ 3300-minimum-element-after-replacement-with-digit-sum.js
@@ -1,7 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums.
+// You replace each element in nums with the sum of its digits.
+// Return the minimum element in nums after all replacements.
+//
+// ✅ SOLUTION:
 const minElement = function(nums) {
     return Math.min(...nums.map(num => [...num.toString()].reduce((acc, curr) => acc + +curr, 0)));
 };
 
+// 📌 TESTCASE:
 console.log(minElement([10,12,13,14])) // 1
 console.log(minElement([1,2,3,4])) // 1
 console.log(minElement([999,19,199])) // 10
