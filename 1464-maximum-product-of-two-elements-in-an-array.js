a30b1e33545022c7bfe75b535ac2c106da9978f1
Irene Volodko <ice7aye@gmail.com>
2025-06-20 07:43:41 +0300

    add solution: maximum product of two elements in an array

---
+++ 1464-maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,8 @@
+const maxProduct = function(nums) {
+    nums.sort((a, b) => b - a);
+    return (nums[0] - 1) * (nums[1] - 1);
+};
+
+console.log(maxProduct([3,4,5,2])) // 12
+console.log(maxProduct([1,5,4,5])) // 16
+console.log(maxProduct([3,7])) // 12
