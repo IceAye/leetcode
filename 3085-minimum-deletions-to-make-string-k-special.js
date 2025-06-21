aa504824045d27510981465a0d2cd439ba724914
Irene Volodko <ice7aye@gmail.com>
2025-06-21 14:24:55 +0300

    description: minimum deletions to make string k special

--- 3085-minimum-deletions-to-make-string-k-special.js
+++ 3085-minimum-deletions-to-make-string-k-special.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given a string word and an integer k.
+//
+// We consider word to be k-special if |freq(word[i]) - freq(word[j])| <= k for all indices i and j in the string.
+//
+// Here, freq(x) denotes the frequency of the character x in word, and |y| denotes the absolute value of y.
+//
+// Return the minimum number of characters you need to delete to make word k-special.
+// 📅 (daily question 2025, June 21st)
+//
+// ✅ SOLUTION (66.67% runtime, 90.48% memory):
 const minimumDeletions = function (word, k) {
     const map = new Map();
     for (const char of word) {
@@ -21,6 +32,7 @@ const minimumDeletions = function (word, k) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(minimumDeletions("aabcaba", 0)) // 3
 console.log(minimumDeletions("dabdcbdcdcd", 2)) // 2
 console.log(minimumDeletions("aaabaaa", 2)) // 1
