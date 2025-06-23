3fb3542e2b2823bb1f46c9a791fc6302ba43482d
Irene Volodko <ice7aye@gmail.com>
2025-06-23 12:14:36 +0300

    description: maximum product difference between two pairs

--- 1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs.js
@@ -1,7 +1,16 @@
+// ❓ DESCRIPTION:
+// The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+// For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+// Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+//
+// Return the maximum such product difference.
+//
+// ✅ SOLUTION:
 const maxProductDifference = function(nums) {
     nums.sort((a, b) => a - b);
     return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
 };
 
+// 📌 TESTCASE:
 console.log(maxProductDifference([5,6,2,7,4])) // 34
 console.log(maxProductDifference([4,2,5,9,7,4,8])) // 64
