8cc8795ded05e0383d714efe6d787b0a776955d9
Irene Volodko <ice7aye@gmail.com>
2025-06-24 23:06:11 +0300

    description: find all k-distant indices in an array

--- 2200-find-all-k-distant-indices-in-an-array.js
+++ 2200-find-all-k-distant-indices-in-an-array.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array nums and two integers key and k. A k-distant index is an index i of nums for which there exists at least one index j such that |i - j| <= k and nums[j] == key.
+// Return a list of all k-distant indices sorted in increasing order.
+// (daily question 2025, June 24th)
+//
+// ✅ SOLUTION:
 const findKDistantIndices = function(nums, key, k) {
     let set = new Set();
     for (let i = 0; i < nums.length; i++) {
@@ -8,5 +14,6 @@ const findKDistantIndices = function(nums, key, k) {
     return [...set.values()];
 };
 
+// 📌 TESTCASE:
 console.log(findKDistantIndices([3,4,9,1,3,9,5],  9, 1)) // [1,2,3,4,5,6]
 console.log(findKDistantIndices([2,2,2,2,2],  2, 2)) // [0,1,2,3,4]
