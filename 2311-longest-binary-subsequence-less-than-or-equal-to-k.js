2cb6f61a35638bdc1670520f8658a257861f604c
Irene Volodko <ice7aye@gmail.com>
2025-06-26 15:33:46 +0300

    description: longest binary subsequence less than or equal to k

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k.js
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given a binary string s and a positive integer k.
+// Return the length of the longest subsequence of s that makes up a binary number less than or equal to k.
+//
+// Note:
+// The subsequence can contain leading zeroes.
+// The empty string is considered to be equal to 0.
+// A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+// 📅 (daily question 2025, June 26th)
+//
+// ✅ SOLUTION:
 const longestSubsequence = function(s, k) {
     while (parseInt(s, 2) > k) {
         s = s.replace(/1/, '');
@@ -5,5 +16,6 @@ const longestSubsequence = function(s, k) {
     return s.length;
 };
 
+// 📌 TESTCASE:
 console.log(longestSubsequence("1001010",  5)) // 5
 console.log(longestSubsequence("00101001",  1)) // 6
