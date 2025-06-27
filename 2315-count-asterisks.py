52339fa87e2a5c4e6dbf47c94613f5abb4fa0530
Irene Volodko <ice7aye@gmail.com>
2025-06-27 16:57:45 +0300

    description: count asterisks

--- 2315-count-asterisks.py
+++ 2315-count-asterisks.py
@@ -1,3 +1,12 @@
+# ❓ DESCRIPTI0N:
+# You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+
+# Return the number of '*' in s, excluding the '*' between each pair of '|'.
+
+# Note that each '|' will belong to exactly one pair.
+
+# ✅ SOLUTION (100% runtime):
+
 class Solution(object):
     def countAsterisks(self, s):
         count = 0
@@ -10,7 +19,8 @@ class Solution(object):
                         count +=1
 
         return count
-    
+
+# 📌 TESTCASE: 
 s = Solution()
 print(s.countAsterisks("l|*e*et|c**o|*de|"))
 print(s.countAsterisks("iamprogrammer"))
