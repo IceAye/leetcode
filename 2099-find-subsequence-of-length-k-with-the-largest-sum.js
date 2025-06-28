0ce7afda54b742d3b47d75fe70d1a949ac367059
Irene Volodko <ice7aye@gmail.com>
2025-06-28 14:24:36 +0300

    add solution 2: find subsequence of length k with the largest sum

--- 2099-find-subsequence-of-length-k-with-the-largest-sum.js
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum.js
@@ -20,6 +20,12 @@ const maxSubsequence = function (nums, k) {
     return result;
 };
 
+const maxSubsequence = function (nums, k) {
+    const indexedNums = nums.map((num, ind) => [num, ind]);
+    const sortedNums = indexedNums.sort((a, b) => b[0] - a[0]);
+    return sortedNums.slice(0, k).sort((a, b) => a[1] - b[1]).map(num => num[0]);
+};
+
 console.log(maxSubsequence([2, 1, 3, 3], 2)); // [3,3]
 console.log(maxSubsequence([-1, -2, 3, 4], 3)); // [-1,3,4]
 console.log(maxSubsequence([3, 4, 3, 3], 2)); // [3,4]
