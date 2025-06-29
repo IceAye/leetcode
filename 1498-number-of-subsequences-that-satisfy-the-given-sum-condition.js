cfc22f20a2b1baa0e78fe1f2855789341cb77b72
Irene Volodko <ice7aye@gmail.com>
2025-06-29 22:30:35 +0300

    description: number of consequences that satisfy the given sum condition

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given an array of integers nums and an integer target.
+//
+// Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+// 📅 (daily question 2025 June, 29th)
+//
+// ✅ SOLUTION:
 const numSubseq = function(nums, target) {
     const MOD = 1e9 + 7;
     nums.sort((a, b) => a - b);
@@ -23,6 +30,7 @@ const numSubseq = function(nums, target) {
     return res;
 };
 
+// 📌 TESTCASE:
 console.log(numSubseq([3,5,6,7], 9)) // 4
 console.log(numSubseq([3,3,6,8], 10)) // 6
 console.log(numSubseq([2,3,3,4,6,7], 12)) // 61
