b35b3431e3e5805b9bb74cac776f4bfeef1f4e8f
Irene Volodko <ice7aye@gmail.com>
2025-06-30 17:49:57 +0300

    description: longest harmonious subsequent

--- 594-longest-harmonious-subsequence.js
+++ 594-longest-harmonious-subsequence.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+// Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+// 📅 (daily question 2025 June< 30th):
+//
+// ✅ SOLUTION (96.73% runtime):
 const findLHS = function(nums) {
     const freq = new Map();
     let maxLength = 0;
@@ -15,6 +21,7 @@ const findLHS = function(nums) {
     return maxLength;
 };
 
+// 📌 TESTCASE:
 console.log(findLHS([1,3,2,2,5,2,3,7])) // 5
 console.log(findLHS([1,2,3,4])) // 2
 console.log(findLHS([1,1,1,1])) // 0
