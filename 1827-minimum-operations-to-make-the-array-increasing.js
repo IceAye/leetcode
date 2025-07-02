21af1fad0e477d90f8ab24adba1dc2dfffe969a7
Irene Volodko <ice7aye@gmail.com>
2025-07-02 14:52:50 +0300

    description: minimum operations to make the array increasing

--- 1827-minimum-operations-to-make-the-array-increasing.js
+++ 1827-minimum-operations-to-make-the-array-increasing.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+//
+// For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+// Return the minimum number of operations needed to make nums strictly increasing.
+//
+// An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1. An array of length 1 is trivially strictly increasing.
+//
+// ✅ SOLUTION:
 const minOperations = function(nums) {
     let result = 0;
     for (let i = 1; i < nums.length; i++) {
@@ -11,6 +20,7 @@ const minOperations = function(nums) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(minOperations([1,1,1])) // 3
 console.log(minOperations([1,5,2,4,1])) // 14
 console.log(minOperations([8])) // 0;
