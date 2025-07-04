e7b110de5bc7e7fa79958e73dac44d8546d3aa4e
Irene Volodko <ice7aye@gmail.com>
2025-07-04 14:06:49 +0300

    description: array partition

--- 561-array-position.js
+++ 561-array-position.js
@@ -1,6 +1,11 @@
+// ❓ DESCRIPTION:
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+// ✅ SOLUTION:
 const arrayPairSum = function(nums) {
     return nums.sort((a, b) => a - b).reduce((acc, curr, ind) => acc + (ind % 2 === 0 ? curr : 0), 0);
 };
 
+// 📌 TESTCASE:
 console.log(arrayPairSum([1,4,3,2])) // 4
 console.log(arrayPairSum([6,2,6,5,1,2])) // 9
