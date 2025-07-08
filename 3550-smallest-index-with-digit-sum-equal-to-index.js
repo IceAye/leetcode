37b2dfcfb7ac464b3e6b281397cac1befa39feed
Irene Volodko <ice7aye@gmail.com>
2025-07-08 14:59:35 +0300

    add solution 2: smallest index with digit sum equal to index

--- 3550-smallest-index-with-digit-sum-equal-to-index.js
+++ 3550-smallest-index-with-digit-sum-equal-to-index.js
@@ -8,6 +8,22 @@ const smallestIndex = function (nums) {
     return -1;
 };
 
+const smallestIndex = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let number = nums[i];
+        let sumOfDigits = 0;
+
+        while (number > 0) {
+            sumOfDigits += number % 10;
+            number = Math.floor(number / 10);
+        }
+
+        if (i === sumOfDigits) return i;
+    }
+
+    return -1;
+};
+
 console.log(smallestIndex([1, 3, 2])); // 2
 console.log(smallestIndex([1, 10, 11])); // 1
 console.log(smallestIndex([1, 2, 3])); // -1
