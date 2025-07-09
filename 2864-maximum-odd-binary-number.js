7aeddab290d7622d21be0d1c474de77f5ed76d61
Irene Volodko <ice7aye@gmail.com>
2025-07-09 16:04:26 +0300

    description: maximum odd binary number

--- 2864-maximum-odd-binary-number.js
+++ 2864-maximum-odd-binary-number.js
@@ -1,7 +1,18 @@
+// ❓ DESCRIPTION:
+// You are given a binary string s that contains at least one '1'.
+//
+// You have to rearrange the bits in such a way that the resulting binary number is the maximum odd binary number that can be created from this combination.
+//
+// Return a string representing the maximum odd binary number that can be created from the given combination.
+//
+// Note that the resulting string can have leading zeros.
+
+// ✅ SOLUTION:
 const maximumOddBinaryNumber = function(s) {
     const onesLength = s.match(/1/g).length;
     return '1'.repeat(onesLength - 1) + '0'.repeat(s.length - onesLength) + '1';
 };
 
+// 📌 TESTCASE:
 console.log(maximumOddBinaryNumber("010")) // "001"
 console.log(maximumOddBinaryNumber("0101")) // "1001"
