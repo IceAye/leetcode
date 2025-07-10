f0d4e07c2c2a3fc2bfd905279c83997b9696893e
Irene Volodko <ice7aye@gmail.com>
2025-07-10 15:10:15 +0300

    description: subarrays distinct element sum of squares 1

--- 2913-subarrays-distinct-element-sum-of-squares-1.js
+++ 2913-subarrays-distinct-element-sum-of-squares-1.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array nums.
+//
+// The distinct count of a subarray of nums is defined as:
+//
+// Let nums[i..j] be a subarray of nums consisting of all the indices from i to j such that 0 <= i <= j < nums.length. Then the number of distinct values in nums[i..j] is called the distinct count of nums[i..j].
+// Return the sum of the squares of distinct counts of all subarrays of nums.
+//
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// ✅ SOLUTION 1:
 const sumCounts = function(nums) {
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -9,6 +20,7 @@ const sumCounts = function(nums) {
     return sum;
 };
 
+// ✅ SOLUTION 2:
 const sumCounts = function(nums) {
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -21,5 +33,6 @@ const sumCounts = function(nums) {
     return sum;
 };
 
+// 📌 TESTCASE:
 console.log(sumCounts([1,2,1])) // 15
 console.log(sumCounts([1,1])) // 3
