0a9a0e814473605a1c61126bc6760bbb5be86c2d
Irene Volodko <ice7aye@gmail.com>
2025-07-10 15:08:45 +0300

    add solution 2: subarrays distinct element sum of squares 1

--- 2913-subarrays-distinct-element-sum-of-squares-1.js
+++ 2913-subarrays-distinct-element-sum-of-squares-1.js
@@ -9,5 +9,17 @@ const sumCounts = function(nums) {
     return sum;
 };
 
+const sumCounts = function(nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let set = new Set();
+        for (let j = i; j < nums.length; j++) {
+           set.add(nums[j]);
+           sum += set.size**2;
+        }
+    }
+    return sum;
+};
+
 console.log(sumCounts([1,2,1])) // 15
 console.log(sumCounts([1,1])) // 3
