b4715ea9ec721be003f19f5a3144b59b01f44454
Irene Volodko <ice7aye@gmail.com>
2025-07-13 18:47:24 +0300

    description: maximum matching of players with trainers

--- 2410-maximum-matching-of-players-with-trainers.js
+++ 2410-maximum-matching-of-players-with-trainers.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array players, where players[i] represents the ability of the ith player. You are also given a 0-indexed integer array trainers, where trainers[j] represents the training capacity of the jth trainer.
+// The ith player can match with the jth trainer if the player's ability is less than or equal to the trainer's training capacity. Additionally, the ith player can be matched with at most one trainer, and the jth trainer can be matched with at most one player.
+// Return the maximum number of matchings between players and trainers that satisfy these conditions.
+// (daily question 2025 July, 13th)
+
+// ✅ SOLUTION:
 const matchPlayersAndTrainers = function(players, trainers) {
     players.sort((a, b) => a - b);
     trainers.sort((a, b) => a - b);
@@ -18,5 +25,6 @@ const matchPlayersAndTrainers = function(players, trainers) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(matchPlayersAndTrainers([4,7,9],  [8,2,5,8])) // 2
 console.log(matchPlayersAndTrainers([1,1,1],  [10])) // 1
