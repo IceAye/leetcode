316b4e7f92f060dbaff5efbd830f0fa275764dfd
Irene Volodko <ice7aye@gmail.com>
2025-07-15 20:22:45 +0300

    description: valid word

--- 3136-valid-word.js
+++ 3136-valid-word.js
@@ -1,3 +1,21 @@
+// ❓ DESCRIPTION:
+// A word is considered valid if:
+//
+// It contains a minimum of 3 characters.
+// It contains only digits (0-9), and English letters (uppercase and lowercase).
+// It includes at least one vowel.
+// It includes at least one consonant.
+// You are given a string word.
+//
+// Return true if word is valid, otherwise, return false.
+//
+// Notes:
+// 'a', 'e', 'i', 'o', 'u', and their uppercases are vowels.
+// A consonant is an English letter that is not a vowel.
+// (daily question 2025 July, 15):
+
+// ✅ SOLUTION:
+
 /**
  * @param {string} word
  * @return {boolean}
@@ -6,6 +24,7 @@ const isValid = function(word) {
     return /^(?=.*[aeiou])(?=.*[bcdfghjklmnpqrstvwyxz])[a-z0-9]{3,}$/i.test(word);
 };
 
+// 📌 TESTCASE:
 console.log(isValid("234Adas")) // true
 console.log(isValid("b3")) // false
 console.log(isValid("a3$e")) // false
