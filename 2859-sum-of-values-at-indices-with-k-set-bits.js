0e7e7dbdbb82dc3a4a165e8d5595ca1eeb8818c7
Irene Volodko <ice7aye@gmail.com>
2025-07-20 19:16:24 +0300

    description: sum of values at indices with k set bits

--- 2859-sum-of-values-at-indices-with-k-set-bits.js
+++ 2859-sum-of-values-at-indices-with-k-set-bits.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed integer array nums and an integer k.
+// Return an integer that denotes the sum of elements in nums whose corresponding indices have exactly k set bits in their binary representation.
+// The set bits in an integer are the 1's present when it is written in binary.
+// For example, the binary representation of 21 is 10101, which has 3 set bits.
+
+// ✅ SOLUTION:
+
 /**
  * @param {number[]} nums
  * @param {number} k
@@ -15,5 +23,6 @@ const sumIndicesWithKSetBits = function(nums, k) {
     return nums.reduce((acc, curr, ind) => acc + (checkOnes(ind) === k ? curr : 0), 0);
 };
 
+// 📌 TESTCASE:
 console.log(sumIndicesWithKSetBits([5,10,1,5,2], 1)) // 13
 console.log(sumIndicesWithKSetBits([4,3,2,1], 2)) // 1
