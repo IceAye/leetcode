adaa86ce644b306c29a8e34da0c2d2d9a6652752
Irene Volodko <ice7aye@gmail.com>
2025-07-22 21:51:16 +0300

    description: maximum erasure value

--- 1695-maximum-erasure-value.js
+++ 1695-maximum-erasure-value.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+// Return the maximum score you can get by erasing exactly one subarray.
+// An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+// 📅 (daily question 2025 July, 22nd)
+
+// ✅ SOLUTION (77.42% runtime, 66.93% memory):
 /**
  * @param {number[]} nums
  * @return {number}
@@ -21,5 +28,6 @@ const maximumUniqueSubarray = function(nums) {
     return maxSum;
 };
 
+// 📌 TESTCASE:
 console.log(maximumUniqueSubarray([4 , 2 , 4 , 5 , 6])); // 17
 console.log(maximumUniqueSubarray([5 , 2 , 1 , 2 , 5 , 2 , 1 , 2 , 5])); // 8
