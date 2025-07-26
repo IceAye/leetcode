52ec142fe76ed333b37cd151cc871c930349c809
Irene Volodko <ice7aye@gmail.com>
2025-07-26 22:35:07 +0300

    description: counting bits

--- 338-counting-bits.js
+++ 338-counting-bits.js
@@ -1,11 +1,15 @@
+// ❓ DESCRIPTION:
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+// ✅ SOLUTION:
 /**
  * @param {number} n
  * @return {number[]}
  */
 const countBits = function(n) {
-    const ans = [];
+    const ans = [0];
 
-    for (let i = 0; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         const binaryDigit = i.toString(2);
         const numberOfOnes = [...binaryDigit].reduce((acc, curr) => acc + (curr === '1' ? +curr : 0), 0);
         ans.push(numberOfOnes);
@@ -14,5 +18,6 @@ const countBits = function(n) {
     return ans;
 };
 
+// 📌 TESTCASE:
 console.log(countBits(2)) // [0,1,1]
 console.log(countBits(5)) // [0,1,1,2,1,2]
