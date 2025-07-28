850740645ad921423ce9c4491d2d2ae7ff5c5003
Irene Volodko <ice7aye@gmail.com>
2025-07-28 20:16:57 +0300

    description: find a corresponding node of a binary tree in a clone of that tree

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone.js
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone.js
@@ -1,4 +1,10 @@
+// ❓ DESCRIPTION:
+// Given two binary trees original and cloned and given a reference to a node target in the original tree.
+// The cloned tree is a copy of the original tree.
+// Return a reference to the same node in the cloned tree.
+// Note that you are not allowed to change any of the two trees or the target node and the answer must be a reference to a node in the cloned tree.
 
+// ✅ SOLUTION:
 // Definition for a binary tree node.
 // function TreeNode(val) {
 //      this.val = val;
@@ -12,6 +18,7 @@
  * @return {TreeNode}
  */
 
+// ✅ TESTCASE:
 const getTargetCopy = function(original, cloned, target) {
     if (!original) return null;
     if (original === target) return cloned;
