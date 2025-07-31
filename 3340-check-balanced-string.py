eb80903edfe59ed24e384dc5587473b01377a6e2
Irene Volodko <ice7aye@gmail.com>
2025-07-31 19:14:38 +0300

    description: 3340

--- 3340-check-balanced-string.py
+++ 3340-check-balanced-string.py
@@ -1,3 +1,8 @@
+# ❓ DESCRIPTION:
+# You are given a string num consisting of only digits. A string of digits is called balanced if the sum of the digits at even indices is equal to the sum of digits at odd indices.
+# Return true if num is balanced, otherwise return false.
+
+# ✅ SOLUTION (92.75% runtime, 99.52% memory):
 class Solution(object):
     def isBalanced(self, num):
         sum_evens = 0
@@ -8,7 +13,8 @@ class Solution(object):
             else:
                 sum_odds += int(num[i])
         return sum_evens == sum_odds
-    
+
+# 📌 TESTCASE: 
 s = Solution()
 print(s.isBalanced('1234'))
 print(s.isBalanced('24123'))
