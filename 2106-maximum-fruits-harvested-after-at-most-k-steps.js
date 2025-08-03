06caaa50a312b87a094f69a24db644efaecb640f
Irene Volodko <ice7aye@gmail.com>
2025-08-03 16:03:28 +0300

    description: maximum fruits harvested after at most k steps

--- 2106-maximum-fruits-harvested-after-at-most-k-steps.js
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps.js
@@ -1,3 +1,10 @@
+// ❓ DESCRIPTION:
+// Fruits are available at some positions on an infinite x-axis. You are given a 2D integer array fruits where fruits[i] = [positioni, amounti] depicts amounti fruits at the position positioni. fruits is already sorted by positioni in ascending order, and each positioni is unique.
+// You are also given an integer startPos and an integer k. Initially, you are at the position startPos. From any position, you can either walk to the left or right. It takes one step to move one unit on the x-axis, and you can walk at most k steps in total. For every position you reach, you harvest all the fruits at that position, and the fruits will disappear from that position.
+// Return the maximum total number of fruits you can harvest.
+// (daily question 2025 August, 3rd)
+
+// ✅ SOLUTION:
 /**
  * @param {number[][]} fruits
  * @param {number} startPos
@@ -33,6 +40,7 @@ const maxTotalFruits = function(fruits, startPos, k) {
     return max;
 };
 
+// 📌 TESTCASE:
 console.log(maxTotalFruits([[2,8],[6,3],[8,6]], 5, 4)) // 9
 console.log(maxTotalFruits([[0,9],[4,1],[5,7],[6,2],[7,4],[10,9]], 5, 4)) // 14
 console.log(maxTotalFruits([[0,3],[6,4],[8,5]], 3, 2)) // 0
