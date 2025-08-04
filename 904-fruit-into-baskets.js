a206b5b1ed7877e91d2b1cf4a7a4a7ba9aa38e84
Irene Volodko <ice7aye@gmail.com>
2025-08-04 20:49:48 +0300

    description: fruit into baskets

--- 904-fruit-into-baskets.js
+++ 904-fruit-into-baskets.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+// You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+// 📅(daily question 2025 August, 4th)
+
+// ✅ SOLUTION (83.07% runtime, 70.84% memory):
 /**
  * @param {number[]} fruits
  * @return {number}
@@ -22,6 +32,7 @@ const totalFruit = function(fruits) {
     return max;
 }
 
+// 📌 TESTCASE:
 console.log(totalFruit([1,2,1])) // 3
 console.log(totalFruit([0,1,2,2])) // 3
 console.log(totalFruit([1,2,3,2,2])) // 4
