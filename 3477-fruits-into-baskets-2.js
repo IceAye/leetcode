f1002b3a75910850a8462e41443b27d40b7fe5de
Irene Volodko <ice7aye@gmail.com>
2025-08-05 14:45:40 +0300

    description: fruits into baskets 2

--- 3477-fruits-into-baskets-2.js
+++ 3477-fruits-into-baskets-2.js
@@ -1,3 +1,13 @@
+// ❓ DESCRIPTION:
+// You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+// From left to right, place the fruits according to these rules:
+// Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+// Each basket can hold only one type of fruit.
+// If a fruit type cannot be placed in any basket, it remains unplaced.
+// Return the number of fruit types that remain unplaced after all possible allocations are made.
+// (daily question 2025 August, 5th)
+
+// ✅ SOLUTION:
 /**
  * @param {number[]} fruits
  * @param {number[]} baskets
@@ -18,5 +28,6 @@ const numOfUnplacedFruits = function(fruits, baskets) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(numOfUnplacedFruits([4,2,5], [3,5,4])) // 1
 console.log(numOfUnplacedFruits([3,6,1], [6,4,7])) // 0
