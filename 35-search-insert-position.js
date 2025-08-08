95130c24137275713751309f3ca1c22f62e9c858
Irene Volodko <ice7aye@gmail.com>
2025-08-08 19:24:38 +0300

    add solution: search insert position

---
+++ 35-search-insert-position.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function(nums, target) {
+    const index = nums.findIndex((num, ind) => num === target || num > target);
+    return index === -1 ? nums.length : index;
+};
+
+console.log(searchInsert([1,3,5,6], 5)) // 2
+console.log(searchInsert([1,3,5,6], 2)) // 1
+console.log(searchInsert([1,3,5,6], 7)) // 4
