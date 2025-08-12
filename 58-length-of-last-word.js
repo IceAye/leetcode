42e04143217ad4f6adb89068b2c92aec8ac45208
Irene Volodko <ice7aye@gmail.com>
2025-08-12 22:57:28 +0300

    description: length of last word

--- 58-length-of-last-word.js
+++ 58-length-of-last-word.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+// A word is a maximal substring consisting of non-space characters only.
+
+// ✅ SOLUTION (100% runtime):
 /**
  * @param {string} s
  * @return {number}
@@ -6,6 +11,7 @@ const lengthOfLastWord = function(s) {
     return  s.trim().split(' ', ).at(-1).length;
 };
 
+// 📌 TESTCASE:
 console.log(lengthOfLastWord("Hello World")) // 5
 console.log(lengthOfLastWord("   fly me   to   the moon  ")) // 4
 console.log(lengthOfLastWord("luffy is still joyboy")) // 6
