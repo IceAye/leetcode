f1961bd0b7b1638580f0ee0b132afa0c14fa1750
Irene Volodko <ice7aye@gmail.com>
2025-08-15 22:41:33 +0300

    description: power of four

--- 342-power-of-four.js
+++ 342-power-of-four.js
@@ -1,3 +1,9 @@
+// ❓ DESCRIPTION:
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+// 📅 (daily question 2025 August, 15th)
+
+// ✅ SOLUTION:
 /**
  * @param {number} n
  * @return {boolean}
@@ -6,6 +12,7 @@ const isPowerOfFour = function(n) {
     return Number.isInteger(Math.log(n) / Math.log(4));
 };
 
+// 📌 TESTCASE:
 console.log(isPowerOfFour(16)) // true
 console.log(isPowerOfFour(5)) // false
 console.log(isPowerOfFour(1)) // true
