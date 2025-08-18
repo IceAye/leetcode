195fc4f7959e7a97d4d038c2acadc49f9b73b6b8
Irene Volodko <ice7aye@gmail.com>
2025-08-18 15:00:00 +0300

    description: plus one

--- 66-plus-one.js
+++ 66-plus-one.js
@@ -1,3 +1,8 @@
+// ❓ DESCRIPTION:
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+
+// ✅ SOLUTION (100% runtime):
 /**
  * @param {number[]} digits
  * @return {number[]}
@@ -14,6 +19,7 @@ const plusOne = function(digits) {
     return digits;
 }
 
+// 📌 TESTCASE:
 console.log(plusOne([1,2,3])) // [1,2,4]
 console.log(plusOne([4,3,2,1])) // [4,3,2,2]
 console.log(plusOne([9])) // [1,0]
