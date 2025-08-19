97b6a4d4832ad42e8b53ea88d75465e7ed35e0cf
Irene Volodko <ice7aye@gmail.com>
2025-08-19 15:39:31 +0300

    add solution: sqrt(x)

---
+++ 69-sqrt-x.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const mySqrt = function (x) {
+    return Math.floor(Math.sqrt(x));
+};
+
+console.log(mySqrt(4)) // 2
+console.log(mySqrt(8)) // 2
