32a4313c9b15a510f4da55e9e113651bbbedb1a0
Irene Volodko <ice7aye@gmail.com>
2025-08-22 16:53:27 +0300

    description: number of 1 bits

--- 191-number-of-1-bits.js
+++ 191-number-of-1-bits.js
@@ -1,3 +1,7 @@
+// ❓ DESCRIPTION:
+// Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+// ✅ SOLUTION (100% runtime):
 /**
  * @param {number} n
  * @return {number}
@@ -6,6 +10,7 @@ const hammingWeight = function(n) {
     return n.toString(2).match(/1/g).length;
 };
 
+// 📌 TESTCASE:
 console.log(hammingWeight(11)) // 3
 console.log(hammingWeight(128)) // 1
 console.log(hammingWeight(2147483645)) // 30
