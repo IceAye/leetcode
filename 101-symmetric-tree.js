dbebd65c9be2bbc6d44bf9ed17b4e06184df62d6
Irene Volodko <ice7aye@gmail.com>
2025-08-28 16:51:10 +0300

    add solution: symmetric tree

---
+++ 101-symmetric-tree.js
@@ -0,0 +1,32 @@
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSymmetric = function (root) {
+    if (!root) return true;
+    const check = (left, right) => {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        else {
+            return left.val === right.val && check(left.left, right.right) && check(left.right, right.left)
+        }
+    }
+    return check(root.left, root.right);
+};
+
+console.log(
+    isSymmetric(
+        new TreeNode(
+            1,
+            new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+            new TreeNode(2, new TreeNode(4), new TreeNode(3)),
+        ),
+    ),
+);
