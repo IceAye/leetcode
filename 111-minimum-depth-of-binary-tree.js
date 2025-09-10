d35248b76f8691ead0d9f441695d727a537b53c0
Irene Volodko <ice7aye@gmail.com>
2025-09-10 19:24:34 +0300

    add solution 2: 111 - minimum depth of binary tree

--- 111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree.js
@@ -1,5 +1,5 @@
 // Definition for a binary tree node.
-function TreeNode(val, left, right) {
+function TreeNode(val , left , right) {
     this.val = val === undefined ? 0 : val;
     this.left = left === undefined ? null : left;
     this.right = right === undefined ? null : right;
@@ -31,26 +31,37 @@ const minDepth = function (root) {
     }
 };
 
+const minDepth = function (root) {
+    if (root == null) return 0;
+    if (root.left == null && root.right == null)
+        return 1;
+    if (root.left == null)
+        return 1 + minDepth(root.right);
+    if (root.right == null)
+        return 1 + minDepth(root.left);
+    return Math.min(minDepth(root.left) , minDepth(root.right)) + 1;
+};
+
 console.log(minDepth([]));
 console.log(
     minDepth(
         new TreeNode(
-            3,
-            new TreeNode(9),
-            new TreeNode(20, new TreeNode(15), new TreeNode(7)),
-        ),
-    ),
+            3 ,
+            new TreeNode(9) ,
+            new TreeNode(20 , new TreeNode(15) , new TreeNode(7)) ,
+        ) ,
+    ) ,
 );
 console.log(
     minDepth(
         new TreeNode(
-            2,
-            null,
+            2 ,
+            null ,
             new TreeNode(
-                3,
-                null,
-                new TreeNode(4, null, new TreeNode(5, null, new TreeNode(6))),
-            ),
-        ),
-    ),
+                3 ,
+                null ,
+                new TreeNode(4 , null , new TreeNode(5 , null , new TreeNode(6))) ,
+            ) ,
+        ) ,
+    ) ,
 );
