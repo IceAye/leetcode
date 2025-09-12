12f42a66f1d1aded4a856b71d1693662a8ae0f70
Irene Volodko <ice7aye@gmail.com>
2025-09-12 17:36:15 +0300

    description: 3227 - vowels game in a string

--- 3227-vowels-game-in-a-string.js
+++ 3227-vowels-game-in-a-string.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// Alice and Bob are playing a game on a string.
+// You are given a string s, Alice and Bob will take turns playing the following game where Alice starts first:
+// On Alice's turn, she has to remove any non-empty substring from s that contains an odd number of vowels.
+// On Bob's turn, he has to remove any non-empty substring from s that contains an even number of vowels.
+// The first player who cannot make a move on their turn loses the game. We assume that both Alice and Bob play optimally.
+// Return true if Alice wins the game, and false otherwise.
+// The English vowels are: a, e, i, o, and u.
+// 📅 (daily question 2025 September, 12th):
+
+// ✅ SOLUTION:
 /**
  * @param {string} s
  * @return {boolean}
@@ -7,5 +18,6 @@ const doesAliceWin = function(s) {
     return !!vowels;
 };
 
+// 📌 TESTCASE:
 console.log(doesAliceWin("leetcoder")) // true
 console.log(doesAliceWin("bbcd")) // false
