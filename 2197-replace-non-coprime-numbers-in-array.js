0ce88f97fa9cb448d9ccaf38a50028fbb3764ba8
Irene Volodko <ice7aye@gmail.com>
2025-09-16 18:17:04 +0300

    description: 2197 - replace non-coprime numbers in array

--- 2197-replace-non-coprime-numbers-in-array.js
+++ 2197-replace-non-coprime-numbers-in-array.js
@@ -1,3 +1,23 @@
+// ❓ DESCRIPTION:
+// You are given an array of integers nums. Perform the following steps:
+//
+// Find any two adjacent numbers in nums that are non-coprime.
+// If no such numbers are found, stop the process.
+// Otherwise, delete the two numbers and replace them with their LCM (Least Common Multiple).
+// Repeat this process as long as you keep finding two adjacent non-coprime numbers.
+// Return the final modified array. It can be shown that replacing adjacent non-coprime numbers in any arbitrary order will lead to the same result.
+//
+// The test cases are generated such that the values in the final array are less than or equal to 108.
+//
+// Two values x and y are non-coprime if GCD(x, y) > 1 where GCD(x, y) is the Greatest Common Divisor of x and y.
+// (daily question 2025 September, 16th).
+//
+// ❗ Constraints:
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 105
+// The test cases are generated such that the values in the final array are less than or equal to 108.
+
+// ✅ SOLUTION:
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -33,5 +53,6 @@ const replaceNonCoprimes = function(nums) {
     return nums;
 };
 
+// 📌 TESTCASE:
 console.log(replaceNonCoprimes([6,4,3,2,7,6,2])) // [12,7,6]
 console.log(replaceNonCoprimes([2,2,1,1,3,3,3])) // [2,1,1,3]
