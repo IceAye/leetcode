7617b68911cab9bdd2853c64268456665574e0a1
Irene Volodko <ice7aye@gmail.com>
2025-09-22 17:46:06 +0300

    167: add solution in python

---
+++ 167-two-sum-2-input-array-is-sorted.py
@@ -0,0 +1,36 @@
+# ❓ DESCRIPTION:
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+# Your solution must use only constant extra space.
+
+# ❗ Constraints:
+# 2 <= numbers.length <= 3 * 104
+# -1000 <= numbers[i] <= 1000
+# numbers is sorted in non-decreasing order.
+# -1000 <= target <= 1000
+# The tests are generated such that there is exactly one solution.
+
+# ✅ SOLUTION:
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            sum = numbers[left] + numbers[right]
+            if sum == target:
+                return [left + 1, right + 1]
+            elif sum < target:
+                left += 1
+            else: 
+                right -= 1
+
+# 📌 TESTCASE:
+s = Solution()
+print(s.twoSum([2,7,11,15], 9))
