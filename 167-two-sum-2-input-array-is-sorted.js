5c447aea67b777cc67333d849f02552de3cfa40f
Irene Volodko <ice7aye@gmail.com>
2025-09-22 17:26:25 +0300

    description: 167 - two sum 2 - input array is sorted

--- 167-two-sum-2-input-array-is-sorted.js
+++ 167-two-sum-2-input-array-is-sorted.js
@@ -1,3 +1,17 @@
+// ❓ DESCRIPTION:
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+// Your solution must use only constant extra space.
+
+// ❗ Constraints:
+// 2 <= numbers.length <= 3 * 104
+// -1000 <= numbers[i] <= 1000
+// numbers is sorted in non-decreasing order.
+// -1000 <= target <= 1000
+// The tests are generated such that there is exactly one solution.
+
+// ✅ SOLUTION (100% runtime> 26.89% memory):
 /**
  * @param {number[]} numbers
  * @param {number} target
@@ -19,6 +33,7 @@ const twoSum = function(numbers, target) {
     }
 };
 
+// 📌 TESTCASE:
 console.log(twoSum([2,7,11,15],  9)) // [1,2]
 console.log(twoSum([2,3,4], 6)) // [1,3]
 console.log(twoSum([-1,0],  -1)) // [1,2]
