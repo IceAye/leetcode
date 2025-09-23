f8906252b0938bc477621bdac2f19d18c3abf082
Irene Volodko <ice7aye@gmail.com>
2025-09-23 17:03:56 +0300

    description: 18 - 4sum

--- 18-4sum.js
+++ 18-4sum.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+// 0 <= a, b, c, d < n
+// a, b, c, and d are distinct.
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+// You may return the answer in any order.
+
+// ❗ CONSTRAINTS:
+// 1 <= nums.length <= 200
+// -10^9 <= nums[i] <= 10^9
+// -10*9 <= target <= 10^9
+
+// ✅ SOLUTION (66.19% runtime, 17.10% memory):
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -31,5 +44,6 @@ const fourSum = function(nums, target) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(fourSum([1,0,-1,0,-2,2], 0)); // [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 console.log(fourSum([2,2,2,2,2], 8)); // [[2,2,2,2]]
