a1060faab2e23815fedf6fc611e744fe920d9d4d
Irene Volodko <ice7aye@gmail.com>
2025-09-24 16:48:03 +0300

    description: 856 - score of parentheses

--- 856-score-of-parentheses.js
+++ 856-score-of-parentheses.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// Given a balanced parentheses string s, return the score of the string.
+// The score of a balanced parentheses string is based on the following rule:
+// "()" has score 1.
+// AB has score A + B, where A and B are balanced parentheses strings.
+// (A) has score 2 * A, where A is a balanced parentheses string.
+
+// ❗ CONSTRAINTS:
+// 2 <= s.length <= 50
+// s consists of only '(' and ')'.
+// s is a balanced parentheses string.
+
+// ✅ SOLUTION:
 /**
  * @param {string} s
  * @return {number}
@@ -23,6 +36,7 @@ const scoreOfParentheses = function(s) {
     return stack.reduce((acc, curr) => acc + curr, 0);
 };
 
+// 📌 TESTCASE:
 console.log(scoreOfParentheses("()")) // 1
 console.log(scoreOfParentheses("(())")) // 2
 console.log(scoreOfParentheses("()()")) // 2
