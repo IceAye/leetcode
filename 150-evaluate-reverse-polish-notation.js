b6d07ee5f9f59a47862550e3f69114d70a20447f
Irene Volodko <ice7aye@gmail.com>
2025-09-26 14:33:17 +0300

    description: 150 - evaluate reverse polish notation

--- 150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation.js
@@ -1,8 +1,25 @@
+// ❓ DESCRIPTION:
+// You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+// Evaluate the expression. Return an integer that represents the value of the expression.
+// Note that:
+// The valid operators are '+', '-', '*', and '/'.
+// Each operand may be an integer or another expression.
+// The division between two integers always truncates toward zero.
+// There will not be any division by zero.
+// The input represents a valid arithmetic expression in a reverse polish notation.
+// The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+// ❗ CONSTRAINTS:
+// 1 <= tokens.length <= 10^4
+// tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+// ✅ SOLUTION (50.60% runtime, 48.24% memory):
 /**
  * @param {string[]} tokens
  * @return {number}
  */
-const evalRPN = function(tokens) {
+
+const evalRPN = function (tokens) {
     const stack = [];
 
     for (let i = 0; i < tokens.length; i++) {
@@ -33,6 +50,7 @@ const evalRPN = function(tokens) {
     return stack[0];
 };
 
+// 📌 TESTCASE:
 console.log(evalRPN(['2' , '1' , '+' , '3' , '*'])); // 9
 console.log(evalRPN(['4' , '13' , '5' , '/' , '+'])); // 6
 console.log(evalRPN(['10' , '6' , '9' , '3' , '+' , '-11' , '*' , '/' , '*' , '17' , '+' , '5' , '+'])); // 22
