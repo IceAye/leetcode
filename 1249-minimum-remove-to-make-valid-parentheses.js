845e7f06909936c2a9702115c69f412aedfaa936
Irene Volodko <ice7aye@gmail.com>
2025-10-02 16:00:49 +0300

    add solution 2: 1249 - minimum remove to make valid parentheses

--- 1249-minimum-remove-to-make-valid-parentheses.js
+++ 1249-minimum-remove-to-make-valid-parentheses.js
@@ -20,6 +20,33 @@ const minRemoveToMakeValid = function(s) {
     return array.filter((item, index) => typeof item !== 'number' && !stack.includes(index)).join('');
 };
 
+const minRemoveToMakeValid = function(s) {
+    const toRemove = new Set();
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (char === '(') {
+            stack.push(i);
+        } else if (char === ')') {
+            if (stack.length) {
+                stack.pop();
+            } else {
+                toRemove.add(i);
+            }
+        }
+    }
+
+    for (const item of stack) toRemove.add(item);
+
+    let result = '';
+    for (let i = 0; i < s.length; i++) {
+        if (!toRemove.has(i)) result += s[i];
+    }
+
+    return result;
+};
+
 console.log(minRemoveToMakeValid("lee(t(c)o)de)")) // "lee(t(c)o)de"
 console.log(minRemoveToMakeValid("a)b(c)d")) // "ab(c)d"
 console.log(minRemoveToMakeValid("))((")) // ""
