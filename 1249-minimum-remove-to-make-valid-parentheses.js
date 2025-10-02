8cd36683567e3a0535d82547de02243399dc22b0
Irene Volodko <ice7aye@gmail.com>
2025-10-02 16:07:52 +0300

    description: 1249 - minimum remove to make valid parentheses

--- 1249-minimum-remove-to-make-valid-parentheses.js
+++ 1249-minimum-remove-to-make-valid-parentheses.js
@@ -1,7 +1,22 @@
+// ❓ DESCRIPTION:
+// Given a string s of '(' , ')' and lowercase English characters.
+// Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+// Formally, a parentheses string is valid if and only if:
+// It is the empty string, contains only lowercase characters, or
+// It can be written as AB (A concatenated with B), where A and B are valid strings, or
+// It can be written as (A), where A is a valid string.
+
+// ❗ CONSTRAINTS:
+// 1 <= s.length <= 10^5
+// s[i] is either '(' , ')', or lowercase English letter.
+
+
 /**
  * @param {string} s
  * @return {string}
  */
+
+// ✅ SOLUTION 1 (6.36% runtime, 14.97% memory):
 const minRemoveToMakeValid = function(s) {
     let array = [...s];
     const stack = [];
@@ -20,6 +35,7 @@ const minRemoveToMakeValid = function(s) {
     return array.filter((item, index) => typeof item !== 'number' && !stack.includes(index)).join('');
 };
 
+// ✅ SOLUTION 2 (45.48% runtime, 67.94% memory):
 const minRemoveToMakeValid = function(s) {
     const toRemove = new Set();
     const stack = [];
@@ -47,6 +63,7 @@ const minRemoveToMakeValid = function(s) {
     return result;
 };
 
+// 📌 TESTCASE:
 console.log(minRemoveToMakeValid("lee(t(c)o)de)")) // "lee(t(c)o)de"
 console.log(minRemoveToMakeValid("a)b(c)d")) // "ab(c)d"
 console.log(minRemoveToMakeValid("))((")) // ""
