35511fa78747bdda8dc0a317cc23e4676b4f77ea
Irene Volodko <ice7aye@gmail.com>
2025-10-07 17:47:56 +0300

    description: 209 - minimum size subarray sum

--- 209-minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum.js
@@ -1,3 +1,12 @@
+// ❓ DESCRIPTION:
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// ❗ CONSTRAINTS:
+// 1 <= target <= 10^9
+// 1 <= nums.length <= 10^5
+// 1 <= nums[i] <= 10^4
+
+// ✅ SOLUTION:
 /**
  * @param {number} target
  * @param {number[]} nums
@@ -25,6 +34,7 @@ const minSubArrayLen = function(target , nums) {
     return min | 0;
 };
 
+// 📌 TESTCASE:
 console.log(minSubArrayLen(7 , [2 , 3 , 1 , 2 , 4 , 3])); // 2
 console.log(minSubArrayLen(4 , [1 , 4 , 4])); // 1
 console.log(minSubArrayLen(11 , [1 , 1 , 1 , 1 , 1 , 1 , 1 , 1])); // 0
