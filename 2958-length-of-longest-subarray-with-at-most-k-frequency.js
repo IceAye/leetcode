d412b015271ebdec3d68d0e8bdd13d06dedade34
Irene Volodko <ice7aye@gmail.com>
2025-10-08 21:30:07 +0300

    description: 2958 - length of longest subarray with at most k frequency

--- 2958-length-of-longest-subarray-with-at-most-k-frequency.js
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency.js
@@ -1,3 +1,16 @@
+// ❓ DESCRIPTION:
+// You are given an integer array nums and an integer k.
+// The frequency of an element x is the number of times it occurs in an array.
+// An array is called good if the frequency of each element in this array is less than or equal to k.
+// Return the length of the longest good subarray of nums.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// ❗ CONSTRAINTS:
+// 1 <= nums.length <= 10^5
+// 1 <= nums[i] <= 10^9
+// 1 <= k <= nums.length
+
+// ✅ SOLUTION (51.85% runtime, 9.26% memory):
 /**
  * @param {number[]} nums
  * @param {number} k
@@ -26,6 +39,7 @@ const maxSubarrayLength = function(nums , k) {
     return max;
 };
 
+// 📌 TESTCASE:
 console.log(maxSubarrayLength([1 , 2 , 3 , 1 , 2 , 3 , 1 , 2] , k = 2)); // 6
 console.log(maxSubarrayLength([1 , 2 , 1 , 2 , 1 , 2 , 1 , 2] , k = 1)); // 2
 console.log(maxSubarrayLength([5 , 5 , 5 , 5 , 5 , 5 , 5] , k = 4)); // 4
