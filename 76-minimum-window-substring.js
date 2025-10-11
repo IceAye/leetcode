a8ef21db014e79ba166c44f0ad0614d70fe5f328
Irene Volodko <ice7aye@gmail.com>
2025-10-11 12:01:39 +0300

    add solution: 76 - minimum window substring

--- 76-minimum-window-substring.js
+++ 76-minimum-window-substring.js
@@ -1,14 +1,3 @@
-// ❓ DESCRIPTION:
-// Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
-// The testcases will be generated such that the answer is unique.
-
-// ❗ CONSTRAINTS:
-// m == s.length
-// n == t.length
-// 1 <= m, n <= 10^5
-// s and t consist of uppercase and lowercase English letters.
-
-// ✅ SOLUTION (78.44% runtime, 55.38% memory):
 /**
  * @param {string} s
  * @param {string} t
@@ -58,7 +47,6 @@ const minWindow = function(s , t) {
     return minWord;
 };
 
-// 📌 TESTCASE:
 console.log(minWindow('ADOBECODEBANC' , 'ABC')); // "BANC"
 console.log(minWindow('a' , 'a')); // "a"
 console.log(minWindow('a' , 'aa')); // ""
