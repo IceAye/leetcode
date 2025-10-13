3d3273face05a39287594b47fc7ee56cb8a23ca6
Irene Volodko <ice7aye@gmail.com>
2025-10-13 21:31:30 +0300

    description: 2273 - find resultant array after removing anagrams

--- 2273-find-resultant-array-after-removing-anagrams.js
+++ 2273-find-resultant-array-after-removing-anagrams.js
@@ -1,3 +1,18 @@
+// ❓ DESCRIPTION:
+// You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+// In one operation, select any index i such that 0 < i < words.length and words[i - 1] and words[i] are anagrams, and
+// delete words[i] from words. Keep performing this operation as long as you can select an index that satisfies the conditions.
+// Return words after performing all operations. It can be shown that selecting the indices for each operation in any
+// arbitrary order will lead to the same result.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase using all the original
+// letters exactly once. For example, "dacb" is an anagram of "abdc".
+
+// ❗ CONSTRAINTS:
+// 1 <= words.length <= 100
+// 1 <= words[i].length <= 10
+// words[i] consists of lowercase English letters.
+
+// ✅ SOLUTION:
 /**
  * @param {string[]} words
  * @return {string[]}
@@ -27,6 +42,7 @@ const removeAnagrams = function (words) {
     return array
 };
 
+// 📌 TESTCASE:
 console.log(removeAnagrams(["abba","baba","bbaa","cd","cd"]))
 console.log(removeAnagrams(["a","b","c","d","e"]))
 console.log(removeAnagrams(["a","b","a"]))
