d7f58b39c310851f866c2002d04e97c7d125efe1
Irene Volodko <ice7aye@gmail.com>
2025-10-14 11:21:39 +0300

    description: 3349 - adjacent increasing subarrays detection 1

--- 3349-adjacent-increasing-subarrays-detection-1.js
+++ 3349-adjacent-increasing-subarrays-detection-1.js
@@ -1,3 +1,15 @@
+// ❓ DESCRIPTION:
+// Given an array nums of n integers and an integer k, determine whether there exist two adjacent subarrays of length k such that both subarrays are strictly increasing. Specifically, check if there are two subarrays starting at indices a and b (a < b), where:
+// Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
+// The subarrays must be adjacent, meaning b = a + k.
+// Return true if it is possible to find two such subarrays, and false otherwise.
+
+// ❗ CONSTRAINTS:
+// 2 <= nums.length <= 100
+// 1 < 2 * k <= nums.length
+// -1000 <= nums[i] <= 1000
+
+// ✅ SOLUTION (89.29% runtime, 76.79% memory):
 /**
  * @param {number[]} nums
  * @param {number} k
@@ -18,6 +30,7 @@ const hasIncreasingSubarrays = function(nums, k) {
     return false;
 };
 
+// 📌 TESTCASE:
 console.log(hasIncreasingSubarrays([2,5,7,8,9,2,3,4,3,1], 3)) // true
 console.log(hasIncreasingSubarrays([1,2,3,4,4,4,4,5,6,7], 5)) // false
 console.log(hasIncreasingSubarrays([-15,19], 1)) // true
