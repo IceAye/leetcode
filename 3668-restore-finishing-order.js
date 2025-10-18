69645a20a01f9c0ca5a37c5b7a7f0e04a9349170
Irene Volodko <ice7aye@gmail.com>
2025-10-18 23:19:38 +0300

    description: 3668 - restore finishing order

--- 3668-restore-finishing-order.js
+++ 3668-restore-finishing-order.js
@@ -1,3 +1,17 @@
+// ❓DESCRIPTION:
+// You are given an integer array order of length n and an integer array friends.
+// order contains every integer from 1 to n exactly once, representing the IDs of the participants of a race in their finishing order.
+// friends contains the IDs of your friends in the race sorted in strictly increasing order. Each ID in friends is guaranteed to appear in the order array.
+// Return an array containing your friends' IDs in their finishing order.
+
+// ❗ CONSTRAINTS:
+// 1 <= n == order.length <= 100
+// order contains every integer from 1 to n exactly once
+// 1 <= friends.length <= min(8, n)
+// 1 <= friends[i] <= n
+// friends is strictly increasing
+
+// ✅ SOLUTION (85.94% runtime, 34.34% memory):
 /**
  * @param {number[]} order
  * @param {number[]} friends
@@ -7,5 +21,6 @@ const recoverOrder = function(order, friends) {
     return friends.toSorted((a, b) => order.indexOf(a) - order.indexOf(b));
 };
 
+// 📌 TESTCASE:
 console.log(recoverOrder([3,1,2,5,4], [1,3,4])) // [3,1,4]
 console.log(recoverOrder([1,4,5,3,2], [2,5])) // [5,2]
