177eeb1a1b808e54a16694d5a807a6ad131c7c90
Irene Volodko <ice7aye@gmail.com>
2025-10-31 16:48:13 +0300

    description: 3461 - Check If Digits Are Equal in String After Operations I

--- 3461-check-if-digits-are-equal-in-string-after-operations-1.js
+++ 3461-check-if-digits-are-equal-in-string-after-operations-1.js
@@ -1,3 +1,14 @@
+// ❓ DESCRIPTION:
+// You are given a string s consisting of digits. Perform the following operation repeatedly until the string has exactly two digits:
+// For each pair of consecutive digits in s, starting from the first digit, calculate a new digit as the sum of the two digits modulo 10.
+// Replace s with the sequence of newly calculated digits, maintaining the order in which they are computed.
+// Return true if the final two digits in s are the same; otherwise, return false.
+
+// ❗ CONSTRAINTS:
+// 3 <= s.length <= 100
+// s consists of only digits.
+
+// ✅ SOLUTION:
 /**
  * @param {string} s
  * @return {boolean}
@@ -14,5 +25,6 @@ const hasSameDigits = function(s) {
   return array[0] === array[1];
 };
 
+// 📌 TESTCASE:
 console.log(hasSameDigits('3902')); // true
 console.log(hasSameDigits('34789')); // false
