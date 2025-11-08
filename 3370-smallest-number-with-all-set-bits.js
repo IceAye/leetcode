d0debede352c7ad5ea46b5206316ace95dbaf87a
Irene Volodko <ice7aye@gmail.com>
2025-11-08 15:35:51 +0300

    description: 3370 - smallest number with all set bits

--- 3370-smallest-number-with-all-set-bits.js
+++ 3370-smallest-number-with-all-set-bits.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// You are given a positive number n.
+// Return the smallest number x greater than or equal to n, such that the binary representation of x contains only set bits
+
+// ❗ CONSTRAINTS:
+// 1 <= n <= 1000
+
+// ✅ SOLUTION:
 /**
  * @param {number} n
  * @return {number}
@@ -10,6 +18,7 @@ const smallestNumber = function(n) {
     return i;
 };
 
+// 📌 TESTCASE:
 console.log(smallestNumber(5)) // 7
 console.log(smallestNumber(10)) // 15
 console.log(smallestNumber(3)) // 3
