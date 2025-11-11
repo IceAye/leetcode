39fd3bdaa1ee4ec250734dd21197adfca24bbf84
Irene Volodko <ice7aye@gmail.com>
2025-11-11 14:45:25 +0300

    description: 414 - third maximum number

--- 414-third-maximum-number.js
+++ 414-third-maximum-number.js
@@ -1,3 +1,11 @@
+// ❓ DESCRIPTION:
+// Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+// ❗ CONSTRAINTS:
+// 1 <= nums.length <= 10^4
+// -2^31 <= nums[i] <= 2^31 - 1
+
+// ✅ SOLUTION:
 /**
  * @param {number[]} nums
  * @return {number}
@@ -7,6 +15,7 @@ const thirdMax = function(nums) {
     return Number.isInteger(sortedArr[2]) ? sortedArr[2] : sortedArr[0];
 };
 
+// 📌 TESTCASE:
 console.log(thirdMax([3,2,1])) // 1
 console.log(thirdMax([1,2])) // 2
 console.log(thirdMax([2,2,3,1])) // 1
