10f57253e9bd12bfcd05b8b10fe72bf4e0fd2640
Iryna.Valadzko <iryna.valodzka@corp.itibo.com>
2025-12-03 19:24:57 +0300

    description: 350 - intersection of two arrays 2

--- 350-intersection-of-two-arrays-2.js
+++ 350-intersection-of-two-arrays-2.js
@@ -1,9 +1,18 @@
+// ❓ DESCRIPTION:
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must
+// appear as many times as it shows in both arrays and you may return the result in any order.
+
+// ❗ CONSTRAINTS:
+// 1 <= nums1.length, nums2.length <= 1000
+// 0 <= nums1[i], nums2[i] <= 1000
+
+// ✅ SOLUTION:
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
-const intersect = function(nums1, nums2) {
+const intersect = function (nums1, nums2) {
     const hash1 = countNumber(nums1);
     const hash2 = countNumber(nums2);
 
@@ -30,6 +39,7 @@ const intersect = function(nums1, nums2) {
 
 };
 
-console.log(intersect([1,2,2,1], [2,2])) // [2,2]
-console.log(intersect([4,9,5], [9,4,9,8,4])) // [4,9]
-console.log(intersect([1,2,3,4], [3,5,4,])) // [3,4]
+// 📌 TESTCASE:
+console.log(intersect([1, 2, 2, 1], [2, 2])); // [2,2]
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4])); // [4,9]
+console.log(intersect([1, 2, 3, 4], [3, 5, 4])); // [3,4]
