bf81c5b1141da6bd53cbb07ea019a5bff3198e74
Iryna.Valadzko <ice7aye@gmail.com>
2026-01-22 10:36:28 +0300

    description: 1637 - Widest Vertical Area Between Two Points Containing No Points

--- 1637-widest-vertical-area-between-two-points-containing-no-points.js
+++ 1637-widest-vertical-area-between-two-points-containing-no-points.js
@@ -1,7 +1,20 @@
+// ❓ DESCRIPTION:
+// Given n points on a 2D plane where points[i] = [xi, yi], Return the widest vertical area between two points such that no points are inside the area.
+// A vertical area is an area of fixed-width extending infinitely along the y-axis (i.e., infinite height). The widest vertical area is the one with the maximum width.
+// Note that points on the edge of a vertical area are not considered included in the area.
+
+// ❗ CONSTRAINTS:
+// n == points.length
+// 2 <= n <= 10^5
+// points[i].length == 2
+// 0 <= x^i, y^i <= 109
+
+// ✅ SOLUTION:
 /**
  * @param {number[][]} points
  * @return {number}
  */
+
 const maxWidthOfVerticalArea = function(points) {
     let max = -Infinity;
     points.sort((a, b) => a[0] - b[0]);
@@ -13,5 +26,6 @@ const maxWidthOfVerticalArea = function(points) {
     return max;
 };
 
+// 📌 TESTCASE:
 console.log(maxWidthOfVerticalArea([[8,7],[9,9],[7,4],[9,7]])) // 1
 console.log(maxWidthOfVerticalArea([[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]])) // 3
